8ffa93780db6b70db7c137e09d3986ead970ee86
Deluan <dquintao@adparlor.com>
2016-02-27 19:28:29 -0500

    Better test messaging

--- controllers/main_test.go
+++ controllers/main_test.go
@@ -11,17 +11,17 @@ import (
 	_ "github.com/deluan/gosonic/routers"
 )
 
-func TestMainController(t *testing.T) {
+func TestErrorHandler(t *testing.T) {
 	tests.Init(t, false)
 
 	r, _ := http.NewRequest("GET", "/INVALID_PATH", nil)
 	w := httptest.NewRecorder()
 	beego.BeeApp.Handlers.ServeHTTP(w, r)
 
-	beego.Debug("testing", "TestMainController", fmt.Sprintf("\nUrl: %s\nStatus Code: [%d]\n%s", r.URL, w.Code, w.Body.String()))
+	beego.Debug("testing", "TestErrorHandler", fmt.Sprintf("\nUrl: %s\nStatus Code: [%d]\n%s", r.URL, w.Code, w.Body.String()))
 
-	Convey("Subject: Error404\n", t, func() {
-		Convey("Status code should be 404", func() {
+	Convey("When requesting an non-existing URL\n", t, func() {
+		Convey("Then the status code should be 404", func() {
 			So(w.Code, ShouldEqual, 404)
 		})
 	})
