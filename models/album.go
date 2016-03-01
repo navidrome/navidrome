78805ac46519cc19fef1b040a7af5e24c3c2af97
Deluan <dquintao@adparlor.com>
2016-03-01 18:59:12 -0500

    Adding info for retrieving artwork from iTunes library

--- models/album.go
+++ models/album.go
@@ -4,7 +4,7 @@ type Album struct {
 	Id           string
 	Name         string
 	ArtistId     string `parent:"artist"`
-	CoverArtPath string
+	CoverArtPath string // TODO http://stackoverflow.com/questions/13795842/linking-itunes-itc2-files-and-ituneslibrary-xml
 	Year         int
 	Compilation  bool
 	Rating       int
