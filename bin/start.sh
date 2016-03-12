4b09eeccc293ca26ddbf62e6a2caf42066c69edd
Deluan <github@deluan.com>
2016-03-11 21:14:32 -0500

    Configuring "prod"

---
+++ bin/start.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+BEEGO_RUNMODE=prod ./gosonic
--- main.go
+++ main.go
@@ -1,16 +1,18 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/astaxie/beego"
 	_ "github.com/deluan/gosonic/conf"
 	_ "github.com/deluan/gosonic/tasks"
 )
 
 func main() {
-	//beego.BConfig.Log.FileLineNum = false
-	if beego.BConfig.RunMode == "dev" {
-		beego.BConfig.WebConfig.DirectoryIndex = true
-		beego.BConfig.WebConfig.StaticDir["/swagger"] = "swagger"
+	fmt.Printf("\nGoSonic v%s (%s mode)\n\n", "0.1", beego.BConfig.RunMode)
+	if beego.BConfig.RunMode == "prod" {
+		beego.SetLevel(beego.LevelInformational)
 	}
+	beego.BConfig.Log.FileLineNum = false
 	beego.Run()
 }
