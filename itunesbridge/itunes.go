85882f6a40b1a88232b2322f887816165708bb2f
Deluan <github@deluan.com>
2016-03-23 16:05:41 -0400

    Using persistent ids to talk to iTunes
    Also added methods to set loved and rating

--- itunesbridge/itunes.go
+++ itunesbridge/itunes.go
@@ -6,8 +6,12 @@ import (
 )
 
 type ItunesControl interface {
-	MarkAsPlayed(id string, playDate time.Time) error
-	MarkAsSkipped(id string, skipDate time.Time) error
+	MarkAsPlayed(trackId string, playDate time.Time) error
+	MarkAsSkipped(trackId string, skipDate time.Time) error
+	SetTrackLoved(trackId string, loved bool) error
+	SetAlbumLoved(trackId string, loved bool) error
+	SetTrackRating(trackId string, rating int) error
+	SetAlbumRating(trackId string, rating int) error
 }
 
 func NewItunesControl() ItunesControl {
@@ -16,9 +20,9 @@ func NewItunesControl() ItunesControl {
 
 type itunesControl struct{}
 
-func (c *itunesControl) MarkAsPlayed(id string, playDate time.Time) error {
+func (c *itunesControl) MarkAsPlayed(trackId string, playDate time.Time) error {
 	script := Script{fmt.Sprintf(
-		`set theTrack to the first item of (every track whose database ID is equal to "%s")`, id),
+		`set theTrack to the first item of (every track whose persistent ID is equal to "%s")`, trackId),
 		`set c to (get played count of theTrack)`,
 		`tell theTrack`,
 		`set played count to c + 1`,
@@ -27,13 +31,49 @@ func (c *itunesControl) MarkAsPlayed(id string, playDate time.Time) error {
 	return script.Run()
 }
 
-func (c *itunesControl) MarkAsSkipped(id string, skipDate time.Time) error {
+func (c *itunesControl) MarkAsSkipped(trackId string, skipDate time.Time) error {
 	script := Script{fmt.Sprintf(
-		`set theTrack to the first item of (every track whose database ID is equal to "%s")`, id),
+		`set theTrack to the first item of (every track whose persistent ID is equal to "%s")`, trackId),
 		`set c to (get skipped count of theTrack)`,
 		`tell theTrack`,
 		`set skipped count to c + 1`,
 		fmt.Sprintf(`set skipped date to date("%s")`, c.formatDateTime(skipDate)),
+		`end tell`}
+	return script.Run()
+}
+
+func (c *itunesControl) SetTrackLoved(trackId string, loved bool) error {
+	script := Script{fmt.Sprintf(
+		`set theTrack to the first item of (every track whose persistent ID is equal to "%s")`, trackId),
+		`tell theTrack`,
+		fmt.Sprintf(`set loved to %v`, loved),
+		`end tell`}
+	return script.Run()
+}
+
+func (c *itunesControl) SetAlbumLoved(trackId string, loved bool) error {
+	script := Script{fmt.Sprintf(
+		`set theTrack to the first item of (every track whose persistent ID is equal to "%s")`, trackId),
+		`tell theTrack`,
+		fmt.Sprintf(`set album loved to %v`, loved),
+		`end tell`}
+	return script.Run()
+}
+
+func (c *itunesControl) SetTrackRating(trackId string, rating int) error {
+	script := Script{fmt.Sprintf(
+		`set theTrack to the first item of (every track whose persistent ID is equal to "%s")`, trackId),
+		`tell theTrack`,
+		fmt.Sprintf(`set rating to %d`, rating),
+		`end tell`}
+	return script.Run()
+}
+
+func (c *itunesControl) SetAlbumRating(trackId string, rating int) error {
+	script := Script{fmt.Sprintf(
+		`set theTrack to the first item of (every track whose persistent ID is equal to "%s")`, trackId),
+		`tell theTrack`,
+		fmt.Sprintf(`set album rating to %d`, rating),
 		`end tell`}
 	return script.Run()
 }
