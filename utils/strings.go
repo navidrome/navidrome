5b2ecc39cadc78679c5147627a4e0ed2ec5fd8a1
Deluan <github@deluan.com>
2016-03-24 09:51:50 -0400

    Longest Common Prefix

--- utils/strings.go
+++ utils/strings.go
@@ -1,8 +1,9 @@
 package utils
 
 import (
-	"github.com/astaxie/beego"
 	"strings"
+
+	"github.com/astaxie/beego"
 )
 
 func NoArticle(name string) string {
@@ -14,4 +15,20 @@ func NoArticle(name string) string {
 		}
 	}
 	return name
+}
+
+func LongestCommonPrefix(list []string) string {
+	if len(list) == 0 {
+		return ""
+	}
+
+	for l := 0; l < len(list[0]); l++ {
+		c := list[0][l]
+		for i := 1; i < len(list); i++ {
+			if l >= len(list[i]) || list[i][l] != c {
+				return list[i][0:l]
+			}
+		}
+	}
+	return list[0]
 }
