d4231ca2a415d922467a3b3b5100af78ba2626df
Deluan <github@deluan.com>
2016-10-10 21:03:21 -0400

    Removed unused scripts (moved to Makefile)

--- bin/cloc.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-cloc --exclude-dir=devDb,.idea,wiki,static --exclude-ext=iml,xml .
--- bin/goconvey.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-NOLOG=1 goconvey --port 9090 -excludedDirs vendor,static,devDb,wiki,bin,tests
