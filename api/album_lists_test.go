a8d7bb7510fc2c6fd4817c19c173784e529ca97f
Deluan <deluan@deluan.com>
2020-01-09 22:39:42 -0500

    Restore AlbumListController tests

--- api/album_lists_test.go
+++ api/album_lists_test.go
@@ -1,68 +1,103 @@
-package api_test
-//
-//import (
-//	"testing"
-//
-//	"github.com/cloudsonic/sonic-server/api/responses"
-//	"github.com/cloudsonic/sonic-server/domain"
-//	"github.com/cloudsonic/sonic-server/engine"
-//	"github.com/cloudsonic/sonic-server/persistence"
-//	. "github.com/cloudsonic/sonic-server/tests"
-//	"github.com/cloudsonic/sonic-server/utils"
-//	. "github.com/smartystreets/goconvey/convey"
-//)
-//
-//func TestGetAlbumList(t *testing.T) {
-//	Init(t, false)
-//
-//	mockAlbumRepo := persistence.CreateMockAlbumRepo()
-//	utils.DefineSingleton(new(domain.AlbumRepository), func() domain.AlbumRepository {
-//		return mockAlbumRepo
-//	})
-//
-//	mockNowPlayingRepo := engine.CreateMockNowPlayingRepo()
-//	utils.DefineSingleton(new(engine.NowPlayingRepository), func() engine.NowPlayingRepository {
-//		return mockNowPlayingRepo
-//	})
-//
-//	Convey("Subject: GetAlbumList Endpoint", t, func() {
-//		mockAlbumRepo.SetData(`[
-//				{"Id":"A","Name":"Vagarosa","ArtistId":"2"},
-//				{"Id":"C","Name":"Liberation: The Island Anthology","ArtistId":"3"},
-//				{"Id":"B","Name":"Planet Rock","ArtistId":"1"}]`, 1)
-//
-//		Convey("Should fail if missing 'type' parameter", func() {
-//			_, w := Get(AddParams("/rest/getAlbumList.view"), "TestGetAlbumList")
-//
-//			So(w.Body, ShouldReceiveError, responses.ErrorMissingParameter)
-//		})
-//		Convey("Return fail on Album Table error", func() {
-//			mockAlbumRepo.SetError(true)
-//			_, w := Get(AddParams("/rest/getAlbumList.view", "type=newest"), "TestGetAlbumList")
-//
-//			So(w.Body, ShouldReceiveError, responses.ErrorGeneric)
-//		})
-//		Convey("Type is invalid", func() {
-//			_, w := Get(AddParams("/rest/getAlbumList.view", "type=not_implemented"), "TestGetAlbumList")
-//
-//			So(w.Body, ShouldReceiveError, responses.ErrorGeneric)
-//		})
-//		Convey("Max size = 500", func() {
-//			_, w := Get(AddParams("/rest/getAlbumList.view", "type=newest", "size=501"), "TestGetAlbumList")
-//			So(w.Body, ShouldBeAValid, responses.AlbumList{})
-//			So(mockAlbumRepo.Options.Size, ShouldEqual, 500)
-//			So(mockAlbumRepo.Options.Alpha, ShouldBeTrue)
-//		})
-//		Convey("Type == newest", func() {
-//			_, w := Get(AddParams("/rest/getAlbumList.view", "type=newest"), "TestGetAlbumList")
-//			So(w.Body, ShouldBeAValid, responses.AlbumList{})
-//			So(mockAlbumRepo.Options.SortBy, ShouldEqual, "CreatedAt")
-//			So(mockAlbumRepo.Options.Desc, ShouldBeTrue)
-//			So(mockAlbumRepo.Options.Alpha, ShouldBeTrue)
-//		})
-//		Reset(func() {
-//			mockAlbumRepo.SetData("[]", 0)
-//			mockAlbumRepo.SetError(false)
-//		})
-//	})
-//}
+package api
+
+import (
+	"errors"
+	"net/http/httptest"
+
+	"github.com/cloudsonic/sonic-server/engine"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+type fakeListGen struct {
+	engine.ListGenerator
+	data       engine.Entries
+	err        error
+	recvOffset int
+	recvSize   int
+}
+
+func (lg *fakeListGen) GetNewest(offset int, size int) (engine.Entries, error) {
+	if lg.err != nil {
+		return nil, lg.err
+	}
+	lg.recvOffset = offset
+	lg.recvSize = size
+	return lg.data, nil
+}
+
+var _ = Describe("AlbumListController", func() {
+	var controller *AlbumListController
+	var listGen *fakeListGen
+	var w *httptest.ResponseRecorder
+
+	BeforeEach(func() {
+		listGen = &fakeListGen{}
+		controller = NewAlbumListController(listGen)
+		w = httptest.NewRecorder()
+	})
+
+	Describe("GetAlbumList", func() {
+		It("should return list of the type specified", func() {
+			r := newTestRequest("type=newest", "offset=10", "size=20")
+			listGen.data = engine.Entries{
+				{Id: "1"}, {Id: "2"},
+			}
+			resp, err := controller.GetAlbumList(w, r)
+
+			Expect(err).To(BeNil())
+			Expect(resp.AlbumList.Album[0].Id).To(Equal("1"))
+			Expect(resp.AlbumList.Album[1].Id).To(Equal("2"))
+			Expect(listGen.recvOffset).To(Equal(10))
+			Expect(listGen.recvSize).To(Equal(20))
+		})
+
+		It("should fail if missing type parameter", func() {
+			r := newTestRequest()
+			_, err := controller.GetAlbumList(w, r)
+
+			Expect(err).To(MatchError("Required string parameter 'type' is not present"))
+		})
+
+		It("should return error if call fails", func() {
+			listGen.err = errors.New("some issue")
+			r := newTestRequest("type=newest")
+
+			_, err := controller.GetAlbumList(w, r)
+
+			Expect(err).To(MatchError("Internal Error"))
+		})
+	})
+
+	Describe("GetAlbumList2", func() {
+		It("should return list of the type specified", func() {
+			r := newTestRequest("type=newest", "offset=10", "size=20")
+			listGen.data = engine.Entries{
+				{Id: "1"}, {Id: "2"},
+			}
+			resp, err := controller.GetAlbumList2(w, r)
+
+			Expect(err).To(BeNil())
+			Expect(resp.AlbumList2.Album[0].Id).To(Equal("1"))
+			Expect(resp.AlbumList2.Album[1].Id).To(Equal("2"))
+			Expect(listGen.recvOffset).To(Equal(10))
+			Expect(listGen.recvSize).To(Equal(20))
+		})
+
+		It("should fail if missing type parameter", func() {
+			r := newTestRequest()
+			_, err := controller.GetAlbumList2(w, r)
+
+			Expect(err).To(MatchError("Required string parameter 'type' is not present"))
+		})
+
+		It("should return error if call fails", func() {
+			listGen.err = errors.New("some issue")
+			r := newTestRequest("type=newest")
+
+			_, err := controller.GetAlbumList2(w, r)
+
+			Expect(err).To(MatchError("Internal Error"))
+		})
+	})
+})
