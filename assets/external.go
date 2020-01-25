f63b1703e61f236fd05b374c921ea3b3463bdeb0
Deluan <deluan@deluan.com>
2020-01-24 20:35:20 -0500

    Add warning when using external assets

--- assets/external.go
+++ assets/external.go
@@ -4,10 +4,17 @@ package assets
 
 import (
 	"net/http"
+	"sync"
 
 	"github.com/deluan/navidrome/consts"
+	"github.com/deluan/navidrome/log"
 )
 
+var once sync.Once
+
 func AssetFile() http.FileSystem {
+	once.Do(func() {
+		log.Warn("Using external assets from " + consts.UIAssetsLocalPath)
+	})
 	return http.Dir(consts.UIAssetsLocalPath)
 }
