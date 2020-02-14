b7f1fc0374ac112344f4d21d36b45d010cc39354
Deluan <deluan@deluan.com>
2020-02-14 09:16:59 -0500

    refactor: remove unused import

--- ui/src/song/SongBulkActions.js
+++ ui/src/song/SongBulkActions.js
@@ -1,4 +1,4 @@
-import React, { Fragment, useEffect } from 'react'
+import React, { Fragment } from 'react'
 import AddToQueueButton from './AddToQueueButton'
 
 export const SongBulkActions = (props) => {
