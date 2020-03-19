cbbebb3264cb15b98b4c492ca4dfcdf8f80aad66
Deluan <deluan@deluan.com>
2020-03-18 23:21:01 -0400

    fix: version position under banner

--- consts/banner.go
+++ consts/banner.go
@@ -3,17 +3,18 @@ package consts
 import (
 	"fmt"
 	"strings"
+	"unicode"
 
 	"github.com/deluan/navidrome/static"
 )
 
 func getBanner() string {
 	data, _ := static.Asset("banner.txt")
-	return strings.TrimSuffix(string(data), "\n")
+	return strings.TrimRightFunc(string(data), unicode.IsSpace)
 }
 
 func Banner() string {
 	version := "Version: " + Version()
 	padding := strings.Repeat(" ", 52-len(version))
-	return fmt.Sprintf("%s%s%s\n", getBanner(), padding, version)
+	return fmt.Sprintf("%s\n%s%s\n", getBanner(), padding, version)
 }
