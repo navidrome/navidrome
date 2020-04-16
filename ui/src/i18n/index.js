4b6004d9e6156b71e9c395dc54569c554b3406e5
jvoisin <julien.voisin@dustri.org>
2020-04-16 13:59:51 +0200

    Add French localization

--- ui/src/i18n/index.js
+++ ui/src/i18n/index.js
@@ -1,5 +1,6 @@
 import deepmerge from 'deepmerge'
 import en from './en'
+import fr from './fr'
 import it from './it'
 import pt from './pt'
 import cn from './cn'
@@ -10,7 +11,7 @@ const addLanguages = (lang) => {
 const languages = { en }
 
 // Add new languages to the object bellow
-addLanguages({ cn, it, pt })
+addLanguages({ cn, fr, it, pt })
 
 // "Hack" to make "albumSongs" resource use the same translations as "song"
 Object.keys(languages).forEach(
