9238193c9615e229aaf1791da87176f495f0dd11
Srihari Chandana <schandan@MacBook-Pro.localdomain>
2020-04-26 11:56:35 -0400

    added grid play button

---
+++ ui/src/album/GridButton.js
@@ -0,0 +1,43 @@
+import React, { useCallback } from 'react'
+import { useDispatch } from 'react-redux'
+import { playAlbum } from '../audioplayer'
+import { Loading, useGetList} from 'react-admin'
+import IconButton from '@material-ui/core/IconButton'
+import PlayIcon from '@material-ui/icons/PlayCircleFilled'
+
+const GridButton = (props) => {
+const dispatch = useDispatch()
+const { ids, data, loading, error } = useGetList(
+	'albumSong', 
+	{  }, 
+	{ field: 'trackNumber', order: 'ASC' }, 
+	{ album_id: props.id},
+	)
+
+	if (loading) {
+	    return (
+	      <IconButton>
+	        <PlayIcon/>
+	      </IconButton>
+	    )
+	}
+
+	if (error) {
+	    return (
+	      <IconButton>
+	        <PlayIcon/>
+	      </IconButton>
+	    )
+	}
+
+    return (
+      <IconButton onClick={(e) => {
+		  e.preventDefault()
+		  e.stopPropagation()
+		  dispatch(playAlbum(ids[0], data))
+	  }}>
+        <PlayIcon/>
+      </IconButton>
+    )
+}
+export default GridButton
