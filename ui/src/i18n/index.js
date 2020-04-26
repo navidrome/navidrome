97ed213413b31b7e8cfdc8ff9170b8fdf0883428
Kevin Morssink <k.morssink@gmail.com>
2020-04-26 15:57:09 +0200

    Add Dutch language

--- ui/src/i18n/index.js
+++ ui/src/i18n/index.js
@@ -1,9 +1,10 @@
 import deepmerge from 'deepmerge'
 import en from './en'
+import cn from './cn'
 import fr from './fr'
 import it from './it'
+import nl from './nl'
 import pt from './pt'
-import cn from './cn'
 
 const addLanguages = (lang) => {
   Object.keys(lang).forEach((l) => (languages[l] = deepmerge(en, lang[l])))
@@ -11,7 +12,7 @@ const addLanguages = (lang) => {
 const languages = { en }
 
 // Add new languages to the object bellow (please keep alphabetic sort)
-addLanguages({ cn, fr, it, pt })
+addLanguages({ cn, fr, it, nl, pt })
 
 // "Hack" to make "albumSongs" resource use the same translations as "song"
 Object.keys(languages).forEach(
