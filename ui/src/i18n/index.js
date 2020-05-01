e341df1e268a9235188c0c00611b9309871ae78d
Deluan <deluan@deluan.com>
2020-05-01 10:43:49 -0400

    Rename Chinese translation file to `zh`

--- ui/src/i18n/index.js
+++ ui/src/i18n/index.js
@@ -1,6 +1,6 @@
 import deepmerge from 'deepmerge'
 import en from './en'
-import cn from './cn'
+import zh from './zh'
 import fr from './fr'
 import it from './it'
 import nl from './nl'
@@ -12,7 +12,7 @@ const addLanguages = (lang) => {
 const languages = { en }
 
 // Add new languages to the object below (please keep alphabetic sort)
-addLanguages({ cn, fr, it, nl, pt })
+addLanguages({ zh, fr, it, nl, pt })
 
 // "Hack" to make "albumSongs" resource use the same translations as "song"
 Object.keys(languages).forEach(
