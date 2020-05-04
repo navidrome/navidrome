ffe94195d24fe44ea51663aa7bd536d332aff930
Deluan <deluan@deluan.com>
2020-05-04 11:47:03 -0400

    Add pr-# tag to Docker image

--- .github/workflows/docker-tags.sh
+++ .github/workflows/docker-tags.sh
@@ -3,10 +3,15 @@
 GIT_TAG="${GITHUB_REF##refs/tags/}"
 GIT_BRANCH="${GITHUB_REF##refs/heads/}"
 GIT_SHA=$(git rev-parse --short HEAD)
+PR_NUM=$(jq --raw-output .pull_request.number "$GITHUB_EVENT_PATH")
 
 DOCKER_IMAGE_TAG="--tag ${DOCKER_IMAGE}:sha-${GIT_SHA}"
 
-if [[ $GITHUB_REF != $GIT_TAG ]]; then
+if [[ $PR_NUM != "null" ]]; then
+  DOCKER_IMAGE_TAG="${DOCKER_IMAGE_TAG} --tag ${DOCKER_IMAGE}:pr-${PR_NUM}"
+fi
+
+if [[ $GITHUB_REF != "$GIT_TAG" ]]; then
     DOCKER_IMAGE_TAG="${DOCKER_IMAGE_TAG} --tag ${DOCKER_IMAGE}:${GIT_TAG#v}  --tag ${DOCKER_IMAGE}:latest"
 elif [[ $GITHUB_REF == "refs/heads/master" ]]; then
     DOCKER_IMAGE_TAG="${DOCKER_IMAGE_TAG} --tag ${DOCKER_IMAGE}:develop"
