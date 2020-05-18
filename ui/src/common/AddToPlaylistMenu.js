84ea852339624225fb1a16199c1539338af8e7b3
Deluan <deluan@deluan.com>
2020-05-18 12:19:01 -0400

    Prettier

--- ui/src/common/AddToPlaylistMenu.js
+++ ui/src/common/AddToPlaylistMenu.js
@@ -1,9 +1,5 @@
 import React from 'react'
-import {
-  useDataProvider,
-  useGetList,
-  useNotify,
-} from 'react-admin'
+import { useDataProvider, useGetList, useNotify } from 'react-admin'
 import { MenuItem } from '@material-ui/core'
 import PropTypes from 'prop-types'
 
@@ -25,50 +21,52 @@ export const addAlbumToPlaylist = (dataProvider, albumId, playlistId) =>
     .then((response) => response.data.map((song) => song.id))
     .then((ids) => addTracksToPlaylist(dataProvider, ids, playlistId))
 
-const AddToPlaylistMenu = React.forwardRef(({ selectedIds, albumId, onClose }, ref) => {
-  const notify = useNotify()
-  const dataProvider = useDataProvider()
-  const { ids, data, loaded } = useGetList(
-    'playlist',
-    { page: 1, perPage: -1 },
-    { field: 'name', order: 'ASC' },
-    {}
-  )
+const AddToPlaylistMenu = React.forwardRef(
+  ({ selectedIds, albumId, onClose }, ref) => {
+    const notify = useNotify()
+    const dataProvider = useDataProvider()
+    const { ids, data, loaded } = useGetList(
+      'playlist',
+      { page: 1, perPage: -1 },
+      { field: 'name', order: 'ASC' },
+      {}
+    )
 
-  if (!loaded) {
-    return <MenuItem>Loading...</MenuItem>
-  }
-
-  const handleItemClick = (e) => {
-    e.preventDefault()
-    const value = e.target.getAttribute('value')
-    if (value !== '') {
-      const add = albumId
-        ? addAlbumToPlaylist(dataProvider, albumId, value)
-        : addTracksToPlaylist(dataProvider, selectedIds, value)
-
-      add
-        .then((len) => {
-          notify('message.songsAddedToPlaylist', 'info', {smart_count: len})
-        })
-        .catch(() => {
-          notify('ra.page.error', 'warning')
-        })
+    if (!loaded) {
+      return <MenuItem>Loading...</MenuItem>
     }
-    e.stopPropagation()
-    onClose && onClose()
-  }
 
-  return (
-    <>
-      {ids.map((id) => (
-        <MenuItem value={id} key={id} onClick={handleItemClick}>
-          {data[id].name}
-        </MenuItem>
-      ))}
-    </>
-  )
-})
+    const handleItemClick = (e) => {
+      e.preventDefault()
+      const value = e.target.getAttribute('value')
+      if (value !== '') {
+        const add = albumId
+          ? addAlbumToPlaylist(dataProvider, albumId, value)
+          : addTracksToPlaylist(dataProvider, selectedIds, value)
+
+        add
+          .then((len) => {
+            notify('message.songsAddedToPlaylist', 'info', { smart_count: len })
+          })
+          .catch(() => {
+            notify('ra.page.error', 'warning')
+          })
+      }
+      e.stopPropagation()
+      onClose && onClose()
+    }
+
+    return (
+      <>
+        {ids.map((id) => (
+          <MenuItem value={id} key={id} onClick={handleItemClick}>
+            {data[id].name}
+          </MenuItem>
+        ))}
+      </>
+    )
+  }
+)
 
 AddToPlaylistMenu.propTypes = {
   selectedIds: PropTypes.arrayOf(PropTypes.any).isRequired,
