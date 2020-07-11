35114be5f741fe920ff438889710208c2ffe68e9
Deluan <deluan@navidrome.org>
2020-07-10 21:44:40 -0400

    Add path to playlist

---
+++ db/migration/20200710211442_add_playlist_path.go
@@ -0,0 +1,27 @@
+package migration
+
+import (
+	"database/sql"
+
+	"github.com/pressly/goose"
+)
+
+func init() {
+	goose.AddMigration(upAddPlaylistPath, downAddPlaylistPath)
+}
+
+func upAddPlaylistPath(tx *sql.Tx) error {
+	_, err := tx.Exec(`
+alter table playlist
+	add path string default '' not null;
+
+alter table playlist
+	add sync bool default false not null;
+`)
+
+	return err
+}
+
+func downAddPlaylistPath(tx *sql.Tx) error {
+	return nil
+}
