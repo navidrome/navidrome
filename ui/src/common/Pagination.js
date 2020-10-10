31568aff4d12fa1f7976b5dd7361aed85baac01f
zoidy <7082025+zoidy@users.noreply.github.com>
2020-10-09 23:00:37 -0700

    fix js formatting

--- ui/src/common/Pagination.js
+++ ui/src/common/Pagination.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import { Pagination as RAPagination } from 'react-admin'
 
-const perPage = [15,25,50];
-var perPageCustom=parseInt(localStorage.rowsPerPageOther);
+const perPage = [15, 25, 50]
+var perPageCustom = parseInt(localStorage.rowsPerPageOther)
 
-if(perPageCustom>0){
-  perPage.push(perPageCustom);
-  perPage.sort(function(a, b){return a-b});
+if (perPageCustom > 0) {
+  perPage.push(perPageCustom)
+  perPage.sort(function (a, b) {
+    return a - b
+  })
 }
 
 const Pagination = (props) => (
