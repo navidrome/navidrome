19ead8f7e80b8962b1f08150b24bd5a0ff929e00
Deluan <deluan@navidrome.org>
2020-10-18 13:23:02 -0400

    Add initial spotify client implementation

---
+++ core/spotify/spotify_suite_test.go
@@ -0,0 +1,17 @@
+package spotify
+
+import (
+	"testing"
+
+	"github.com/deluan/navidrome/log"
+	"github.com/deluan/navidrome/tests"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+func TestSpotify(t *testing.T) {
+	tests.Init(t, false)
+	log.SetLevel(log.LevelCritical)
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "Spotify Test Suite")
+}
