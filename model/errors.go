a257891b46ffb69487a56f24a6b95c7c1eace6ba
Deluan <deluan@navidrome.org>
2020-10-20 15:31:49 -0400

    Get better artist images results

--- model/errors.go
+++ model/errors.go
@@ -6,4 +6,5 @@ var (
 	ErrNotFound      = errors.New("data not found")
 	ErrInvalidAuth   = errors.New("invalid authentication")
 	ErrNotAuthorized = errors.New("not authorized")
+	ErrNotAvailable  = errors.New("functionality not available")
 )
