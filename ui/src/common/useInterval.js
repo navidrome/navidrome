f2e55be47eecde6c0b4fc5306677782708cd2d0a
Deluan <deluan@navidrome.org>
2020-11-12 16:12:31 -0500

    Fine tune scan status behaviour

---
+++ ui/src/common/useInterval.js
@@ -0,0 +1,23 @@
+// From https://overreacted.io/making-setinterval-declarative-with-react-hooks/
+
+import { useEffect, useRef } from 'react'
+
+export const useInterval = (callback, delay) => {
+  const savedCallback = useRef()
+
+  // Remember the latest callback.
+  useEffect(() => {
+    savedCallback.current = callback
+  }, [callback])
+
+  // Set up the interval.
+  useEffect(() => {
+    function tick() {
+      savedCallback.current()
+    }
+    if (delay !== null) {
+      let id = setInterval(tick, delay)
+      return () => clearInterval(id)
+    }
+  }, [delay])
+}
