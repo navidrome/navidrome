15545411683edbfe4c28737c15df57f1cf8921e4
Deluan <deluan@navidrome.org>
2020-11-13 13:44:38 -0500

    Enable activity panel by default

--- conf/configuration.go
+++ conf/configuration.go
@@ -162,7 +162,7 @@ func init() {
 	viper.SetDefault("devprecachealbumartwork", false)
 	viper.SetDefault("devoldcachelayout", false)
 	viper.SetDefault("devdisabletrackcoverart", false)
-	viper.SetDefault("devactivitymenu", false)
+	viper.SetDefault("devactivitymenu", true)
 }
 
 func InitConfig(cfgFile string) {
