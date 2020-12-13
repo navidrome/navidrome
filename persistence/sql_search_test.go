4f90fa9924630a9dc6b279eaf8b0165c1452923b
Deluan <deluan@navidrome.org>
2020-12-13 14:05:48 -0500

    Add denormalized list of artist_ids to album, to speed-up artist's albums queries
    This will be removed once we have a proper many-to-many relationship between album and artist

--- persistence/sql_search_test.go
+++ persistence/sql_search_test.go
@@ -7,28 +7,8 @@ import (
 
 var _ = Describe("sqlRepository", func() {
 	Describe("getFullText", func() {
-		It("returns all lowercase chars", func() {
-			Expect(getFullText("Some Text")).To(Equal(" some text"))
-		})
-
-		It("removes accents", func() {
-			Expect(getFullText("Quintão")).To(Equal(" quintao"))
-		})
-
-		It("remove extra spaces", func() {
-			Expect(getFullText(" some  text  ")).To(Equal(" some text"))
-		})
-
-		It("remove duplicated words", func() {
-			Expect(getFullText("legião urbana urbana legiÃo")).To(Equal(" legiao urbana"))
-		})
-
-		It("remove symbols", func() {
-			Expect(getFullText("Tom’s Diner ' “40” ‘A’")).To(Equal(" 40 a diner toms"))
-		})
-
-		It("remove opening brackets", func() {
-			Expect(getFullText("[Five Years]")).To(Equal(" five years"))
+		It("prefixes with a space", func() {
+			Expect(getFullText("legiao urbana")).To(Equal(" legiao urbana"))
 		})
 	})
 })
