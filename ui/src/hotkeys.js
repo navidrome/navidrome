bc609be3c979ed3c07661feb52e28cd41247a730
Deluan <deluan@navidrome.org>
2021-02-05 13:10:58 -0500

    Fix space hotkey

--- ui/src/hotkeys.js
+++ ui/src/hotkeys.js
@@ -1,7 +1,7 @@
 const keyMap = {
   SHOW_HELP: { name: 'show_help', sequence: 'shift+?', group: 'Global' },
   TOGGLE_MENU: { name: 'toggle_menu', sequence: 'm', group: 'Global' },
-  TOGGLE_PLAY: { name: 'toggle_play', sequence: 'p', group: 'Player' },
+  TOGGLE_PLAY: { name: 'toggle_play', sequence: 'space', group: 'Player' },
   PREV_SONG: { name: 'prev_song', sequence: 'left', group: 'Player' },
   NEXT_SONG: { name: 'next_song', sequence: 'right', group: 'Player' },
   VOL_UP: { name: 'vol_up', sequence: '=', group: 'Player' },
