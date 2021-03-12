5a259ef3ff08b12c04c3ac2b122edb00812e8c0a
rochakjain361 <57208018+rochakjain361@users.noreply.github.com>
2021-03-12 20:56:41 +0530

    Make the version number in the about dialog clickable (#817)
    * Make the version number in the about dialog clickable
    
    * Fix prettier errors
    
    * Fix build errors

--- ui/src/dialogs/AboutDialog.js
+++ ui/src/dialogs/AboutDialog.js
@@ -45,7 +45,22 @@ const AboutDialog = ({ open, onClose }) => {
                 <TableCell align="right" component="th" scope="row">
                   {translate('menu.version')}:
                 </TableCell>
-                <TableCell align="left">{config.version}</TableCell>
+                {config.version === 'dev' ? (
+                  <TableCell align="left"> {config.version} </TableCell>
+                ) : (
+                  <TableCell align="left">
+                    <Link
+                      href={`https://github.com/navidrome/navidrome/releases/tag/v${
+                        config.version.split(' ')[0]
+                      }`}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      {config.version.split(' ')[0]}
+                    </Link>
+                    {' ' + config.version.split(' ')[1]}
+                  </TableCell>
+                )}
               </TableRow>
               {Object.keys(links).map((key) => {
                 return (
