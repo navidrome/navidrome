d74f49372519dbb6ce6ba56d3de7746a4e997ce6
RitikPandey1 <rp18.pandey@gmail.com>
2021-03-13 13:31:01 +0530

    duplicate_song_warning added

--- ui/src/dialogs/AddToPlaylistDialog.js
+++ ui/src/dialogs/AddToPlaylistDialog.js
@@ -13,11 +13,18 @@ import {
   DialogContent,
   DialogTitle,
 } from '@material-ui/core'
-import { closeAddToPlaylist } from '../actions'
+import {
+  closeAddToPlaylist,
+  closeDuplicateSongDialog,
+  openDuplicateSongWarning,
+} from '../actions'
 import { SelectPlaylistInput } from './SelectPlaylistInput'
+import { httpClient } from '../dataProvider'
+import { REST_URL } from '../consts'
+import DuplicateSongDialog from './DuplicateSongDialog'
 
 export const AddToPlaylistDialog = () => {
-  const { open, selectedIds, onSuccess } = useSelector(
+  const { open, selectedIds, onSuccess, duplicateSong } = useSelector(
     (state) => state.addToPlaylistDialog
   )
   const dispatch = useDispatch()
@@ -53,6 +60,18 @@ export const AddToPlaylistDialog = () => {
       })
   }
 
+  const checkDuplicateSong = (playlistId) => {
+    httpClient(`${REST_URL}/playlist/${playlistId}`)
+      .then((res) => {
+        const { tracks } = JSON.parse(res.body)
+        const dupSng = tracks.filter((song) => song.id === selectedIds[0])
+        if (dupSng.length) dispatch(openDuplicateSongWarning())
+      })
+      .catch(() => {
+        notify('ra.page.error', 'warning')
+      })
+  }
+
   const handleSubmit = (e) => {
     if (value.id) {
       addToPlaylist(value.id)
@@ -69,32 +88,52 @@ export const AddToPlaylistDialog = () => {
   }
 
   const handleChange = (pls) => {
+     if (pls.id && selectedIds.length === 1) {
+       checkDuplicateSong(pls.id)
+     }
     setValue(pls)
   }
 
+  const handleDuplicateClose = () => {
+    setValue({});
+    dispatch(closeDuplicateSongDialog())
+  }
+  const handleDuplicateSubmit = () => {
+    addToPlaylist(value.id)
+    dispatch(closeDuplicateSongDialog())
+    dispatch(closeAddToPlaylist())
+  }
+
   return (
-    <Dialog
-      open={open}
-      onClose={handleClickClose}
-      onBackdropClick={handleClickClose}
-      aria-labelledby="form-dialog-new-playlist"
-      fullWidth={true}
-      maxWidth={'sm'}
-    >
-      <DialogTitle id="form-dialog-new-playlist">
-        {translate('resources.playlist.actions.selectPlaylist')}
-      </DialogTitle>
-      <DialogContent>
-        <SelectPlaylistInput onChange={handleChange} />
-      </DialogContent>
-      <DialogActions>
-        <Button onClick={handleClickClose} color="primary">
-          {translate('ra.action.cancel')}
-        </Button>
-        <Button onClick={handleSubmit} color="primary" disabled={!value.name}>
-          {translate('ra.action.add')}
-        </Button>
-      </DialogActions>
-    </Dialog>
+    <>
+      <Dialog
+        open={open}
+        onClose={handleClickClose}
+        onBackdropClick={handleClickClose}
+        aria-labelledby="form-dialog-new-playlist"
+        fullWidth={true}
+        maxWidth={'sm'}
+      >
+        <DialogTitle id="form-dialog-new-playlist">
+          {translate('resources.playlist.actions.selectPlaylist')}
+        </DialogTitle>
+        <DialogContent>
+          <SelectPlaylistInput onChange={handleChange} />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClickClose} color="primary">
+            {translate('ra.action.cancel')}
+          </Button>
+          <Button onClick={handleSubmit} color="primary" disabled={!value.name}>
+            {translate('ra.action.add')}
+          </Button>
+        </DialogActions>
+      </Dialog>
+      <DuplicateSongDialog
+        open={duplicateSong}
+        handleClickClose={handleDuplicateClose}
+        handleSubmit={handleDuplicateSubmit}
+      />
+    </>
   )
 }
--- ui/src/reducers/dialogReducer.js
+++ ui/src/reducers/dialogReducer.js
@@ -1,8 +1,14 @@
-import { ADD_TO_PLAYLIST_CLOSE, ADD_TO_PLAYLIST_OPEN } from '../actions'
+import {
+  ADD_TO_PLAYLIST_CLOSE,
+  ADD_TO_PLAYLIST_OPEN,
+  DUPLICATE_SONG_WARNING_OPEN,
+  DUPLICATE_SONG_WARNING_CLOSE,
+} from '../actions'
 
 export const addToPlaylistDialogReducer = (
   previousState = {
     open: false,
+    duplicateSong: false,
   },
   payload
 ) => {
@@ -17,6 +23,10 @@ export const addToPlaylistDialogReducer = (
       }
     case ADD_TO_PLAYLIST_CLOSE:
       return { ...previousState, open: false, onSuccess: undefined }
+    case DUPLICATE_SONG_WARNING_OPEN:
+      return { ...previousState, duplicateSong: true }
+    case DUPLICATE_SONG_WARNING_CLOSE:
+      return { ...previousState, duplicateSong: false }
     default:
       return previousState
   }
