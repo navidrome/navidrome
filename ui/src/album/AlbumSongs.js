794e1d87ae1272af55744da3b39b4cbf92c4015e
aniketbiswas21 <aniket.biswas75@gmail.com>
2021-03-13 19:03:13 +0530

    made the requested ui changes

--- ui/src/album/AlbumSongs.js
+++ ui/src/album/AlbumSongs.js
@@ -1,16 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {
   BulkActionsToolbar,
   ListToolbar,
   TextField,
   useVersion,
   useListContext,
+  FunctionField,
 } from 'react-admin'
 import clsx from 'clsx'
-import { useDispatch } from 'react-redux'
-import { Card, useMediaQuery } from '@material-ui/core'
+import { useDispatch, useSelector } from 'react-redux'
+import { Card, useMediaQuery, Typography } from '@material-ui/core'
+import { useTheme } from '@material-ui/core/styles'
 import { makeStyles } from '@material-ui/core/styles'
 import StarBorderIcon from '@material-ui/icons/StarBorder'
+import get from 'lodash.get'
 import { playTracks } from '../actions'
 import {
   DurationField,
@@ -22,6 +25,10 @@ import {
 } from '../common'
 import { AddToPlaylistDialog } from '../dialogs'
 import SongPlayIcon from '../common/SongPlayIcon'
+import PlayingLight from '../icons/playing-light.gif'
+import PlayingDark from '../icons/playing-dark.gif'
+import PausedLight from '../icons/paused-light.png'
+import PausedDark from '../icons/paused-dark.png'
 
 const useStyles = makeStyles(
   (theme) => ({
@@ -67,17 +74,107 @@ const useStyles = makeStyles(
     contextMenu: {
       visibility: (props) => (props.isDesktop ? 'hidden' : 'visible'),
     },
+    trackNoText: {
+      width: '24px',
+    },
+    icon: {
+      width: '32px',
+      height: '32px',
+      verticalAlign: 'text-top',
+      marginLeft: '-8px',
+      marginTop: '-7px',
+      paddingRight: '3px',
+    },
   }),
   { name: 'RaList' }
 )
 
 const AlbumSongs = (props) => {
   const { data, ids } = props
+  const theme = useTheme()
   const isXsmall = useMediaQuery((theme) => theme.breakpoints.down('xs'))
   const isDesktop = useMediaQuery((theme) => theme.breakpoints.up('md'))
   const classes = useStyles({ isDesktop })
   const dispatch = useDispatch()
   const version = useVersion()
+  const [isHoveredOn, setIsHoveredOn] = useState(false)
+  const [currentHoveredId, setCurrentHoveredId] = useState(null)
+  const currentTrack = useSelector((state) => get(state, 'queue.current', {}))
+  const currentId = currentTrack.trackId
+  const paused = currentTrack.paused
+
+  const handleHover = (id) => {
+    setIsHoveredOn(true)
+    setCurrentHoveredId(id)
+  }
+
+  const handleHoverRemove = () => {
+    setIsHoveredOn(false)
+    setCurrentHoveredId(null)
+  }
+
+  const Icon = () => {
+    let icon
+    if (paused) {
+      icon = theme.palette.type === 'light' ? PausedLight : PausedDark
+    } else {
+      icon = theme.palette.type === 'light' ? PlayingLight : PlayingDark
+    }
+    return (
+      <img
+        src={icon}
+        className={classes.icon}
+        alt={paused ? 'paused' : 'playing'}
+      />
+    )
+  }
+
+  const handletrackNumber = (record) => {
+    const isCurrent =
+      currentId && (currentId === record.id || currentId === record.mediaFileId)
+    if (
+      record.id !== currentId &&
+      !isHoveredOn &&
+      record.id !== currentHoveredId
+    ) {
+      return (
+        <Typography className={classes.trackNoText} variant="subtitle2">
+          {record.trackNumber}
+        </Typography>
+      )
+    } else if (
+      record.id === currentId &&
+      isHoveredOn &&
+      record.id === currentHoveredId
+    ) {
+      return (
+        <SongPlayIcon
+          record={record}
+          onClick={(id) => dispatch(playTracks(data, ids, id))}
+        />
+      )
+    } else if (
+      record.id !== currentId &&
+      isHoveredOn &&
+      record.id === currentHoveredId
+    ) {
+      return (
+        <SongPlayIcon
+          record={record}
+          onClick={(id) => dispatch(playTracks(data, ids, id))}
+        />
+      )
+    } else if (isCurrent) {
+      return <Icon />
+    } else {
+      return (
+        <Typography className={classes.trackNoText} variant="subtitle2">
+          {record.trackNumber}
+        </Typography>
+      )
+    }
+  }
+
   return (
     <>
       <ListToolbar
@@ -103,18 +200,16 @@ const AlbumSongs = (props) => {
             showDiscSubtitles={true}
             contextAlwaysVisible={!isDesktop}
             classes={{ row: classes.row }}
+            hoverAction={handleHover}
+            removeHoverAction={handleHoverRemove}
           >
             {isDesktop && (
-              <TextField
+              <FunctionField
                 source="trackNumber"
                 sortBy="discNumber asc, trackNumber asc"
                 label="#"
                 sortable={false}
-              />
-            )}
-            {isDesktop && (
-              <SongPlayIcon
-                onClick={(id) => dispatch(playTracks(data, ids, id))}
+                render={handletrackNumber}
               />
             )}
             <SongTitleField
