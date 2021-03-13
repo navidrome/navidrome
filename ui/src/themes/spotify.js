dc04323a2b140072226ccaee7ae27c01a289a893
dpirad007 <dionpinto000@gmail.com>
2021-03-13 13:05:36 +0530

    prettier used

--- ui/src/themes/spotify.js
+++ ui/src/themes/spotify.js
@@ -1,24 +1,24 @@
 export default {
-    themeName: 'Spotify',
-    palette: {
-        primary: {
-            main: '#ffffff'
-        },
-        secondary: {
-            light: '#4ac776',
-            dark: '#14813a',
-            main: '#1DB954',
-        },
-        type: 'dark',
+  themeName: 'Spotify',
+  palette: {
+    primary: {
+      main: '#ffffff',
     },
-    overrides: {
-        MuiFormGroup: {
-            root: {
-                color: 'white',
-            },
-        },
-        player: {
-            theme: 'dark',
-        },
-    }
+    secondary: {
+      light: '#4ac776',
+      dark: '#14813a',
+      main: '#1DB954',
+    },
+    type: 'dark',
+  },
+  overrides: {
+    MuiFormGroup: {
+      root: {
+        color: 'white',
+      },
+    },
+    player: {
+      theme: 'dark',
+    },
+  },
 }
