9e780a648a774f4799c088236b97ffdeeef9d6fd
dpirad007 <dionpinto000@gmail.com>
2021-03-13 11:41:20 +0530

    Spotify theme added

---
+++ ui/src/themes/spotify.js
@@ -0,0 +1,24 @@
+export default {
+    themeName: 'Spotify',
+    palette: {
+        primary: {
+            main: '#ffffff'
+        },
+        secondary: {
+            light: '#4ac776',
+            dark: '#14813a',
+            main: '#1DB954',
+        },
+        type: 'dark',
+    },
+    overrides: {
+        MuiFormGroup: {
+            root: {
+                color: 'white',
+            },
+        },
+        player: {
+            theme: 'dark',
+        },
+    }
+}
