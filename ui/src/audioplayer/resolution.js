466b79e10c3d41d9ee040ea97b284747773bd1a3
the-pro <rockingayas13@gmail.com>
2021-03-16 12:52:10 +0530

    Changed the resolutions

--- ui/src/audioplayer/resolution.js
+++ ui/src/audioplayer/resolution.js
@@ -1,6 +1,6 @@
 const MobileResolution = { value: '600px (Mobile Devices)' }
-const DesktopResolutionSm = { value: '1200px (Small Screens)' }
-const TabletResolution = { value: '1500px (Tablets)' }
+const DesktopResolutionSm = { value: '1500px (Small Screens)' }
+const TabletResolution = { value: '1200px (Tablets)' }
 const DesktopResolutionBi = { value: '4000px (Large Screens)' }
 
 export default {
