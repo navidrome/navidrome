c0b3f15ce4327237264f26d9c02a3f61d95a23ed
YashJipkate <yashjipkate@gmail.com>
2021-03-17 11:47:06 +0530

    Liberalise theme setting

--- ui/src/themes/useCurrentTheme.js
+++ ui/src/themes/useCurrentTheme.js
@@ -1,15 +1,14 @@
 import { useSelector } from 'react-redux'
 import useMediaQuery from '@material-ui/core/useMediaQuery'
-import LightTheme from './light'
-import DarkTheme from './dark'
+import themes from './index'
 import { AUTO_THEME_ID } from '../consts'
 
 export default () => {
   const prefersLightMode = useMediaQuery('(prefers-color-scheme: light)')
   return useSelector((state) => {
     if (state.theme === AUTO_THEME_ID) {
-      return prefersLightMode ? LightTheme : DarkTheme
+      return prefersLightMode ? themes.LightTheme : themes.DarkTheme
     }
-    return state.theme === 'LightTheme' ? LightTheme : DarkTheme
+    return themes[state.theme] || themes.DarkTheme
   })
 }
