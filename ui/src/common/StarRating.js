9dcb9dc9c359f6faa2f0c3d9f1acd2f116214d26
Neil Chauhan <neilchauhan2@gmail.com>
2021-03-19 14:01:02 +0530

    Improved indentation of the newly added files

--- ui/src/common/StarRating.js
+++ ui/src/common/StarRating.js
@@ -1,60 +1,65 @@
 import React, { useCallback } from 'react'
 import StarIcon from '@material-ui/icons/Star'
 import { makeStyles } from '@material-ui/core/styles'
-import { useStarRating } from "./useStarRating";
+import { useStarRating } from './useStarRating'
 
 const useStyles = makeStyles({
-    rated: {
-        color : '#ffc107'
-    },
-    unrated: {
-        color : '#e4e5e9'
-    },
-    radioBtn: {
-        opacity: 0,
-        width: 0,
-        height: 0,
-    },
-    star : {
-        cursor: 'pointer',
-        transition: 'color 200ms' 
-    }
+  rated: {
+    color: '#ffc107',
+  },
+  unrated: {
+    color: '#e4e5e9',
+  },
+  radioBtn: {
+    opacity: 0,
+    width: 0,
+    height: 0,
+  },
+  star: {
+    cursor: 'pointer',
+    transition: 'color 200ms',
+  },
 })
 
+export const StarRating = ({ record = {}, source, resource }) => {
+  const [rate, hoverRating, hover] = useStarRating(resource, record, source)
+  const classes = useStyles()
 
-export const StarRating = ({record = {}, source, resource}) => {
-    const [ rate, hoverRating, hover] = useStarRating(resource, record, source)
-    const classes = useStyles()
+  const handleRating = useCallback(
+    (e) => {
+      e.preventDefault()
+      rate(e.target.value)
+      e.stopPropagation()
+    },
+    [rate]
+  )
 
-    const handleRating = useCallback((e) => {
-            e.preventDefault()
-            rate(e.target.value)
-            e.stopPropagation()
-        }, [rate])
+  return (
+    <div>
+      {[...Array(5)].map((star, i) => {
+        const ratingVal = i + 1
 
-    return (
-        <div>
-            {[...Array(5)].map((star, i) => {
-                const ratingVal = i + 1
-
-                return (
-                    <label key={i}>
-                        <input 
-                            className={classes.radioBtn}
-                            type="radio" 
-                            name="rating" 
-                            value={ratingVal}
-                            onClick={handleRating} 
-                        />
-                        <StarIcon 
-                            className={ratingVal <= (hover || record[source]) ? classes.rated : classes.unrated + " " + classes.star }
-                            onMouseEnter={() => hoverRating(ratingVal)}
-                            onMouseLeave={() => hoverRating(null)} 
-                        />
-                    </label>
-
-                )
-            })}
-        </div>
-    )
+        return (
+          <label key={i}>
+            <input
+              className={classes.radioBtn}
+              type="radio"
+              name="rating"
+              value={ratingVal}
+              onClick={handleRating}
+            />
+            <StarIcon
+              className={
+                ratingVal <= (hover || record[source])
+                  ? classes.rated
+                  : classes.unrated + ' ' + classes.star
+              }
+              onMouseEnter={() => hoverRating(ratingVal)}
+              onMouseLeave={() => hoverRating(null)}
+            />
+          </label>
+        )
+      })}
+    </div>
+  )
 }
--- ui/src/common/useStarRating.js
+++ ui/src/common/useStarRating.js
@@ -1,48 +1,46 @@
 import { useState, useCallback, useEffect, useRef } from 'react'
 import { useDataProvider, useNotify } from 'react-admin'
-import subsonic from "../subsonic"
+import subsonic from '../subsonic'
 
 export const useStarRating = (resource, record = {}, source) => {
-    const [hover, setHover] = useState(null)
-    const [loading, setLoading] = useState(false)
-    const notify = useNotify()
-    const dataProvider = useDataProvider()
-    const mountedRef = useRef(false)
+  const [hover, setHover] = useState(null)
+  const [loading, setLoading] = useState(false)
+  const notify = useNotify()
+  const dataProvider = useDataProvider()
+  const mountedRef = useRef(false)
 
-    useEffect(() => {
-        mountedRef.current = true
-        return () => {
-            mountedRef.current = false
+  useEffect(() => {
+    mountedRef.current = true
+    return () => {
+      mountedRef.current = false
+    }
+  }, [])
+
+  const refreshRating = useCallback(() => {
+    dataProvider.getOne(resource, { id: record.id }).then(() => {
+      if (mountedRef.current) {
+        setLoading(false)
+      }
+    })
+  }, [dataProvider, record.id, resource])
+
+  const rate = (val) => {
+    setLoading(true)
+    subsonic
+      .setRating(record.id, val)
+      .then(refreshRating)
+      .catch((e) => {
+        console.log('Error setting star rating: ', e)
+        notify('ra.page.error', 'warning')
+        if (mountedRef.current) {
+          setLoading(false)
         }
-    }, [])
+      })
+  }
 
-    const refreshRating = useCallback(() => {
-        dataProvider.getOne(resource, {id: record.id})
-        .then(() => {
-            if(mountedRef.current) {
-                setLoading(false)
-            }
-        })
-    }, [dataProvider, record.id, resource])
+  const hoverRating = (val) => {
+    setHover(val)
+  }
 
-
-    const rate = (val) => {
-        setLoading(true)
-        subsonic.setRating(record.id, val)
-        .then(refreshRating)
-        .catch((e) => {
-            console.log('Error setting star rating: ', e)
-            notify('ra.page.error', 'warning')
-            if(mountedRef.current) {
-                setLoading(false)
-            }  
-        })
-    }
-
-    const hoverRating = (val) => {
-        setHover(val)
-    }
-
-    return [rate, hoverRating, hover]
+  return [rate, hoverRating, hover]
 }
-
