2c6c76fc549d997a3c2e588999db648560bd7671
Samarjeet <samarsault@gmail.com>
2021-03-27 10:30:42 +0530

    Make spotify-ish more spotify-ish

--- ui/src/themes/spotify.js
+++ ui/src/themes/spotify.js
@@ -1,22 +1,315 @@
-import green from '@material-ui/core/colors/green'
+const spotifyGreen = {
+  300: '#62ec83',
+  500: '#1db954',
+  900: '#008827',
+}
+
+// For Album, Playlist
+const musicListActions = {
+  padding: '1rem 0',
+  alignItems: 'center',
+  '@global': {
+    button: {
+      margin: 5,
+      border: '1px solid transparent',
+      backgroundColor: 'inherit',
+      color: '#b3b3b3',
+      '&:hover': {
+        border: '1px solid #b3b3b3',
+        backgroundColor: 'inherit !important',
+      },
+    },
+    'button:first-child': {
+      transform: 'scale(2)',
+      margin: '1.5rem',
+      minWidth: 0,
+      transition: 'transform .3s ease',
+      background: spotifyGreen['500'],
+      color: '#fff',
+      borderRadius: 500,
+      border: 0,
+      '&:hover': {
+        transform: 'scale(2.1)',
+        backgroundColor: `${spotifyGreen['500']} !important`,
+        border: 0,
+      },
+    },
+    'button:first-child>span:first-child': {
+      padding: 0,
+    },
+    'button:first-child>span:first-child>span': {
+      display: 'none',
+    },
+    'button>span:first-child>span, button:not(:first-child)>span:first-child>svg': {
+      color: '#b3b3b3',
+    },
+  },
+}
 
 export default {
   themeName: 'Spotify-ish',
+  typography: {
+    fontFamily: "system-ui, 'Helvetica Neue', Helvetica, Arial",
+    h6: {
+      fontSize: '1rem', // AppBar title
+    },
+  },
   palette: {
     primary: {
-      light: green['300'],
-      main: green['500'],
+      light: spotifyGreen['300'],
+      main: spotifyGreen['500'],
     },
     secondary: {
-      main: green['900'],
+      main: '#000',
       contrastText: '#fff',
+    },
+    background: {
+      default: '#121212',
+      paper: 'inherit',
     },
     type: 'dark',
   },
   overrides: {
+    MuiPopover: {
+      paper: {
+        backgroundColor: '#121212',
+      },
+    },
+    MuiDialog: {
+      paper: {
+        backgroundColor: '#121212',
+      },
+    },
     MuiFormGroup: {
       root: {
-        color: 'white',
+        color: spotifyGreen['500'],
+      },
+    },
+    MuiMenuItem: {
+      root: {
+        fontSize: '0.875rem',
+      },
+    },
+    MuiDivider: {
+      root: {
+        margin: '.75rem 0',
+      },
+    },
+    MuiButton: {
+      root: {
+        background: spotifyGreen['500'],
+        color: '#fff',
+        // margin: '5px',
+        borderRadius: 500,
+        '&:hover': {
+          background: `${spotifyGreen['900']} !important`,
+        },
+      },
+      textSecondary: {
+        border: '1px solid #b3b3b3',
+        background: '#000',
+        '&:hover': {
+          border: '1px solid #fff !important',
+          background: '#000 !important',
+        },
+      },
+      label: {
+        color: '#fff',
+        paddingRight: '1rem',
+        paddingLeft: '0.7rem',
+      },
+    },
+    MuiDrawer: {
+      root: {
+        background: '#000',
+        paddingTop: '10px',
+      },
+    },
+    MuiTableRow: {
+      root: {
+        padding: '10px 0',
+        transition: 'background-color .3s ease',
+        '&:hover': {
+          backgroundColor: '#1d1d1d !important',
+        },
+        '@global': {
+          'td:nth-child(4)': {
+            color: '#fff !important',
+          },
+        },
+      },
+    },
+    MuiTableCell: {
+      root: {
+        borderBottom: '1px solid #1d1d1d',
+        padding: '10px !important',
+        color: '#b3b3b3 !important',
+      },
+      head: {
+        borderBottom: '1px solid #282828',
+        fontSize: '0.75rem',
+        textTransform: 'uppercase',
+        letterSpacing: 1.2,
+      },
+    },
+    MuiAppBar: {
+      positionFixed: {
+        backgroundColor: '#101420',
+        boxShadow: 'none',
+      },
+    },
+    NDAlbumGridView: {
+      albumName: {
+        fontSize: '1rem',
+        marginTop: '0.5rem',
+        marginBottom: '0.25rem',
+        fontWeight: 700,
+        lineHeight: '1.5rem',
+        textTransform: 'none',
+        color: '#fff',
+      },
+      albumSubtitle: {
+        color: '#b3b3b3',
+        fontSize: '0.875rem',
+      },
+      albumContainer: {
+        backgroundColor: '#181818',
+        borderRadius: '.5rem',
+        padding: '1rem',
+        transition: 'background-color .3s ease',
+        '&:hover': {
+          backgroundColor: '#282828',
+        },
+      },
+      albumPlayButton: {
+        backgroundColor: spotifyGreen['500'],
+        borderRadius: '50%',
+        boxShadow: '0 8px 8px rgb(0 0 0 / 30%)',
+        padding: '0.35rem',
+        transition: 'padding .3s ease',
+        '&:hover': {
+          background: `${spotifyGreen['500']} !important`,
+          padding: '0.45rem',
+        },
+      },
+    },
+    NDPlaylistDetails: {
+      container: {
+        background: 'linear-gradient(#1d1d1d, transparent)',
+        borderRadius: 0,
+        paddingTop: '2.5rem !important',
+        boxShadow: 'none',
+      },
+      title: {
+        fontSize: 'calc(1.5rem + 1.5vw);',
+        fontWeight: 700,
+        color: '#fff',
+      },
+      details: {
+        fontSize: '.875rem',
+        color: 'rgba(255,255,255, 0.8)',
+      },
+    },
+    NDAlbumDetails: {
+      root: {
+        background: 'linear-gradient(#1d1d1d, transparent)',
+        borderRadius: 0,
+        paddingTop: '3.5rem !important',
+        boxShadow: 'none',
+      },
+      recordName: {
+        fontSize: 'calc(1.5rem + 1.5vw);',
+        fontWeight: 700,
+      },
+      recordArtist: {
+        fontSize: '.875rem',
+        fontWeight: 700,
+      },
+      recordMeta: {
+        fontSize: '.875rem',
+        color: 'rgba(255,255,255, 0.8)',
+      },
+      commentBlock: {
+        fontSize: '.875rem',
+        color: 'rgba(255,255,255, 0.8)',
+      },
+    },
+    NDAlbumShow: {
+      albumActions: musicListActions,
+    },
+    NDPlaylistShow: {
+      playlistActions: musicListActions,
+    },
+    NDAudioPlayer: {
+      audioTitle: {
+        color: '#fff',
+        fontSize: '0.875rem',
+        '&.songTitle': {
+          fontWeight: 400,
+        },
+        '&.songInfo': {
+          fontSize: '0.675rem',
+          color: '#b3b3b3',
+        },
+      },
+      player: {
+        border: '10px solid blue',
+      },
+    },
+    RaLayout: {
+      appFrame: {
+        '@global': {
+          '.panel-content': {
+            display: 'none',
+          },
+        },
+      },
+      content: {
+        padding: '0 !important',
+        background: 'linear-gradient(#171717, #121212)',
+      },
+    },
+    RaListToolbar: {
+      toolbar: {
+        padding: '0 .55rem !important',
+      },
+    },
+    RaSearchInput: {
+      input: {
+        borderRadius: 500,
+        width: '20rem',
+        paddingLeft: '.9rem',
+        border: 0,
+        height: '2.5rem',
+      },
+    },
+    RaFilterButton: {
+      root: {
+        marginRight: '1rem',
+      },
+    },
+    RaPaginationActions: {
+      button: {
+        backgroundColor: 'inherit',
+        minWidth: 48,
+        margin: '0 4px',
+        border: '1px solid #282828',
+        '@global': {
+          '> .MuiButton-label': {
+            padding: 0,
+          },
+        },
+      },
+      actions: {
+        '@global': {
+          '.next-page': {
+            marginLeft: 8,
+            marginRight: 8,
+          },
+          '.previous-page': {
+            marginRight: 8,
+          },
+        },
       },
     },
     NDLogin: {
