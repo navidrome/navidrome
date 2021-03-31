188163248c5378c60ae8a79291706cca0f2c532d
himanshunitrr <himanshumauryacrz@gmail.com>
2021-04-01 00:54:14 +0530

    npm run prettier

--- ui/src/common/QualityInfo.js
+++ ui/src/common/QualityInfo.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Chip from '@material-ui/core/Chip'
 import { LOSSLESS_FORMATS } from '../consts'
 
-export const QualityInfo = ({record, ...rest}) => {
+export const QualityInfo = ({ record, ...rest }) => {
   let { suffix, bitRate } = record
   suffix = suffix.toUpperCase()
   let info = suffix
--- ui/src/consts.js
+++ ui/src/consts.js
@@ -4,4 +4,12 @@ export const M3U_MIME_TYPE = 'audio/x-mpegurl'
 
 export const AUTO_THEME_ID = 'AUTO_THEME_ID'
 
-export const LOSSLESS_FORMATS = ['FLAC', 'WAV', 'DSF', 'ALAC', 'TTA', 'ATRAC', 'SHN']
+export const LOSSLESS_FORMATS = [
+  'FLAC',
+  'WAV',
+  'DSF',
+  'ALAC',
+  'TTA',
+  'ATRAC',
+  'SHN',
+]
