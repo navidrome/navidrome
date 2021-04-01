3320042170676b24e4d75a1ec00dde35930457aa
himanshunitrr <himanshumauryacrz@gmail.com>
2021-04-01 22:48:26 +0530

    added size

--- ui/src/common/QualityInfo.js
+++ ui/src/common/QualityInfo.js
@@ -1,14 +1,24 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import Chip from '@material-ui/core/Chip'
 import { LOSSLESS_FORMATS } from '../consts'
 
-export const QualityInfo = ({ record, ...rest }) => {
+export const QualityInfo = ({ record, size, ...rest }) => {
   let { suffix, bitRate } = record
   suffix = suffix.toUpperCase()
   let info = suffix
   if (!LOSSLESS_FORMATS.includes(suffix)) {
     info += ' ' + bitRate
   }
+  return <Chip size={size} variant="outlined" label={info} />
+}
 
-  return <Chip size="small" variant="outlined" label={info} />
+QualityInfo.propTypes = {
+  record: PropTypes.object,
+  color: PropTypes.string,
+  size: PropTypes.string,
+}
+
+QualityInfo.defaultProps = {
+  size: 'small',
 }
