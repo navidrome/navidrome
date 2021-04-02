50d064f05a5b308efc2faeacd7924cbba31fd180
certuna <62144283+certuna@users.noreply.github.com>
2021-04-02 17:58:45 +0200

    New service worker (#952)
    * Add files via upload
    
    * Update serviceWorker.js

--- ui/src/serviceWorker.js
+++ ui/src/serviceWorker.js
@@ -32,7 +32,7 @@ export function register(config) {
     }
 
     window.addEventListener('load', () => {
-      const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`
+      const swUrl = `${process.env.PUBLIC_URL}/navidrome-service-worker.js`
 
       if (isLocalhost) {
         // This is running on localhost. Let's check if a service worker still exists or not.
