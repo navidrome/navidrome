216abc48c9ba3dc640f8b476efdac862c80c84e1
sobhanbera <50291544+SobhanBera@users.noreply.github.com>
2021-04-04 21:00:41 +0530

    trying something

--- ui/src/themes/index.js
+++ ui/src/themes/index.js
@@ -1,7 +1,7 @@
 import LightTheme from './light'
 import DarkTheme from './dark'
 import ExtraDarkTheme from './extradark'
-import GreenTheme from './green'
 import SpotifyTheme from './spotify'
+import GreenTheme from './green'
 
 export default { LightTheme, DarkTheme, ExtraDarkTheme, GreenTheme, SpotifyTheme }
