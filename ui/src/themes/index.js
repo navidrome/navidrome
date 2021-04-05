ca51372d8d2f60e708a248549d158753c4035fe0
sobhanbera <50291544+SobhanBera@users.noreply.github.com>
2021-04-05 06:55:54 +0530

    Add Extra Dark theme (#955)
    * added new theme - night
    
    * removed a unused field
    
    * fixed a typo from previous change
    
    * night theme in login window
    
    * changed name
    
    changed the theme name from "Night" to "Extra Dark"
    
    * changed the theme name
    
    * Update index.js
    
    * Rename night.js to extradark.js
    
    * trying something
    
    * formatted
    
    the JS build was failing because I haven't formatted the index.js file with prettier. I got to know about this now.
    I think now it will be resolved.

--- ui/src/themes/index.js
+++ ui/src/themes/index.js
@@ -1,6 +1,13 @@
 import LightTheme from './light'
 import DarkTheme from './dark'
+import ExtraDarkTheme from './extradark'
 import GreenTheme from './green'
 import SpotifyTheme from './spotify'
 
-export default { LightTheme, DarkTheme, GreenTheme, SpotifyTheme }
+export default {
+  LightTheme,
+  DarkTheme,
+  ExtraDarkTheme,
+  GreenTheme,
+  SpotifyTheme,
+}
