11c0c724bc9d57d42f1ea290808e68c50e6a7e25
Dnouv <sdevanshu90@yahoo.com>
2021-04-07 20:23:54 +0530

    Theme color change

--- ui/src/themes/light.js
+++ ui/src/themes/light.js
@@ -1,6 +1,6 @@
 const tLight = {
-  300: '#31b1c2',
-  500: '#04a7ff',
+  300: '#0054df',
+  500: '#ffffff',
   900: '#007588',
 }
 const musicListActions = {
@@ -13,7 +13,7 @@ const musicListActions = {
       backgroundColor: '#fff',
       color: '#b3b3b3',
       '&:hover': {
-        border: '1px solid #b3b3b3',
+        border: '1px solid #224bff',
         backgroundColor: 'inherit !important',
       },
     },
@@ -38,17 +38,18 @@ const musicListActions = {
         transform: 'scale(2.1)',
         backgroundColor: `${tLight['500']} !important`,
         border: 0,
+        boxShadow: '0px 0px 4px 0px #5656567d',
       },
     },
     'button:first-child>span:first-child': {
       padding: 0,
-      color: '#fff',
+      color: tLight['300'],
     },
     'button:first-child>span:first-child>span': {
       display: 'none',
     },
     'button>span:first-child>span, button:not(:first-child)>span:first-child>svg': {
-      color: '#b3b3b3',
+      color: '#656565',
     },
   },
 }
@@ -58,18 +59,18 @@ export default {
   palette: {
     primary: {
       light: tLight['300'],
-      main: tLight['500'],
+      main: '#464646',
     },
     secondary: {
       main: '#000',
       contrastText: '#fff',
     },
     background: {
-      default: '#fff',
+      default: '#f0f2f5',
       paper: 'inherit',
     },
     text: {
-      secondary: '#fff',
+      secondary: '#232323',
     },
   },
   typography: {
@@ -79,12 +80,24 @@ export default {
     },
   },
   overrides: {
+    MuiCard: {
+      root: {
+        marginLeft: '1%',
+        marginRight: '1%',
+        background: '#ffffff',
+      },
+    },
     MuiPopover: {
       paper: {
         backgroundColor: tLight['500'],
         '& .MuiListItemIcon-root': {
-          color: '#fff',
+          color: '#656565',
         },
+      },
+    },
+    MuiTypography: {
+      colorTextSecondary: {
+        color: '#0a0a0a',
       },
     },
     MuiDialog: {
@@ -112,31 +125,40 @@ export default {
         color: '#91b1b0',
       },
     },
+    MuiCheckbox: {
+      root: {
+        color: '#616161',
+      },
+    },
     MuiIconButton: {
       label: {},
     },
     MuiButton: {
       root: {
         background: '#fff',
-        color: '#fff',
+        color: '#000',
         border: '1px solid transparent',
         borderRadius: 500,
         '&:hover': {
-          background: `${tLight['500']} !important`,
+          background: '#0054df !important',
+          color: '#fff',
         },
       },
       containedPrimary: {
         backgroundColor: '#fff',
       },
       textPrimary: {
-        backgroundColor: tLight['500'],
+        backgroundColor: '#0054df',
+        '& span': {
+          color: '#fff',
+        },
       },
       textSecondary: {
         border: '1px solid #b3b3b3',
         background: '#fff',
         '&:hover': {
           border: '1px solid #fff !important',
-          background: `${tLight['500']} !important`,
+          background: '#b3b3b3 !important',
         },
       },
       label: {
@@ -144,14 +166,15 @@ export default {
         paddingRight: '1rem',
         paddingLeft: '0.7rem',
         '&:hover': {
-          color: '#fff',
+          // color: '#fff',
         },
       },
     },
     MuiDrawer: {
       root: {
-        background: tLight['500'],
+        background: '#ffffff',
         paddingTop: '10px',
+        boxShadow: '-14px -7px 20px black',
       },
       '&:hover': {
         backgroundColor: '#000',
@@ -162,23 +185,23 @@ export default {
         padding: '10px 0',
         transition: 'background-color .3s ease',
         '&:hover': {
-          backgroundColor: `${tLight['500']} !important`,
+          backgroundColor: '#e4e4e4 !important',
         },
         '@global': {
           'td:nth-child(4)': {
-            color: '#5f5f5f !important',
+            color: '#3c3c3c !important',
           },
         },
       },
       head: {
-        backgroundColor: '#cdf4ff',
+        backgroundColor: '#e0efff',
       },
     },
     MuiTableCell: {
       root: {
         borderBottom: '1px solid #1d1d1d',
         padding: '10px !important',
-        color: '#000000b0 !important',
+        color: '#656565 !important',
       },
       head: {
         borderBottom: '1px solid #282828',
@@ -190,7 +213,10 @@ export default {
     MuiAppBar: {
       positionFixed: {
         background: `${tLight['500']} !important`,
-        boxShadow: 'none',
+        boxShadow: '13px -12px 20px 0px #000',
+      },
+      colorSecondary: {
+        color: '#0054df',
       },
     },
     NDAppBar: {
@@ -210,12 +236,12 @@ export default {
         display: 'block',
       },
       albumContainer: {
-        backgroundColor: '#02e8ff14',
+        backgroundColor: '#e0efff7d',
         borderRadius: '.5rem',
         padding: '.75rem',
         transition: 'background-color .3s ease',
         '&:hover': {
-          backgroundColor: '#02c5ff66',
+          backgroundColor: '#c6dbff',
         },
       },
       albumPlayButton: {
@@ -224,17 +250,17 @@ export default {
         boxShadow: '0 8px 8px rgb(0 0 0 / 30%)',
         padding: '0.35rem',
         transition: 'padding .3s ease',
-        color: '#fff',
+        color: '#0054df',
         '&:hover': {
-          background: `${tLight['500']} !important`,
+          background: '#0054df !important',
           padding: '0.45rem',
-          color: '#fff',
+          color: tLight['500'],
         },
       },
     },
     NDPlaylistDetails: {
       container: {
-        background: 'linear-gradient(#edfbff, transparent)',
+        // background: 'linear-gradient(#edfbff, transparent)',
         borderRadius: 0,
         paddingTop: '2.5rem !important',
         boxShadow: 'none',
@@ -251,7 +277,7 @@ export default {
     },
     NDAlbumDetails: {
       root: {
-        background: 'linear-gradient(#f8feff, #b2f1ff)',
+        // background: 'linear-gradient(#f8feff, #b2f1ff)',
         borderRadius: 0,
         boxShadow: 'none',
       },
@@ -284,7 +310,7 @@ export default {
     },
     NDSubMenu: {
       icon: {
-        color: '#fff',
+        color: '#656565',
       },
     },
     NDAudioPlayer: {
@@ -335,7 +361,7 @@ export default {
     RaLayout: {
       content: {
         padding: '0 !important',
-        background: 'linear-gradient(#f8feff, #b2f1ff)',
+        // background: 'linear-gradient(#f8feff, #b2f1ff)',
       },
     },
     RaListToolbar: {
@@ -358,7 +384,14 @@ export default {
       root: {
         marginRight: '1rem',
         '& button': {
+          color: '#0f0f0f',
           backgroundColor: '#fff',
+          '& span': {
+            color: '#101010',
+            '&:hover': {
+              color: '#ffffff',
+            },
+          },
         },
       },
     },
@@ -369,7 +402,7 @@ export default {
     },
     RaLink: {
       link: {
-        color: '#717171',
+        color: '#287eff',
       },
     },
     RaLogout: {
@@ -379,23 +412,23 @@ export default {
     },
     RaMenuItemLink: {
       root: {
-        color: '#fff',
+        color: '#232323 !important',
         '& .MuiListItemIcon-root': {
-          color: '#fff',
+          color: '#656565',
         },
       },
       active: {
-        backgroundColor: '#b6faff',
-        color: '#000000b0 !important',
+        backgroundColor: '#44a0ff1f',
+        color: '#232323 !important',
         '& .MuiListItemIcon-root': {
-          color: '#000000b0',
+          color: '#0066ff',
         },
       },
     },
     RaSidebar: {
       drawerPaper: {
         '@media (min-width: 0px) and (max-width: 599.95px)': {
-          backgroundColor: '#128099!important',
+          backgroundColor: `tLight['500'] !important`,
         },
       },
     },
