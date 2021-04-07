9399873f4c60725b07af306454ad4d4fc4ce595c
Himanshu maurya <himanshumauryacrz@gmail.com>
2021-04-07 07:00:17 +0530

    Added quality info (#918)
    * added quality info
    
    * fixed formatting
    
    * implemented various suggestions
    
    * npm run prettier
    
    * applied suggestions
    
    * npm run prettier
    
    * corrected lossless formats and other suggestions
    
    * moved losslessformats into consts.js
    
    * added some test
    
    * typo while resolving conflicts
    
    * fetch
    
    * removed a bug causing component (as suggested)
    
    * Update PlayerToolbar.js
    
    * implemented suggestions
    
    * added few more tests
    
    * npm run prettier
    
    * added size
    
    * updated qualityInfo
    
    * implemented suggestions
    
    * added test for when no record is recieved
    
    * Update QualityInfo.js

---
+++ ui/src/common/QualityInfo.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import Chip from '@material-ui/core/Chip'
+import { LOSSLESS_FORMATS } from '../consts'
+
+export const QualityInfo = ({ record, size, ...rest }) => {
+  let { suffix = 'NO_SUFFIX', bitRate = 'NO_BITRATE' } = record
+  suffix = suffix.toUpperCase()
+  let info = suffix
+  if (!LOSSLESS_FORMATS.includes(suffix)) {
+    info += ' ' + bitRate
+  }
+  return <Chip {...rest} size={size} variant="outlined" label={info} />
+}
+
+QualityInfo.propTypes = {
+  record: PropTypes.object.isRequired,
+  size: PropTypes.string,
+}
+
+QualityInfo.defaultProps = {
+  size: 'small',
+  record: {},
+}
--- ui/src/consts.js
+++ ui/src/consts.js
@@ -3,3 +3,13 @@ export const REST_URL = '/app/api'
 export const M3U_MIME_TYPE = 'audio/x-mpegurl'
 
 export const AUTO_THEME_ID = 'AUTO_THEME_ID'
+
+export const LOSSLESS_FORMATS = [
+  'FLAC',
+  'WAV',
+  'DSF',
+  'ALAC',
+  'TTA',
+  'ATRAC',
+  'SHN',
+]
