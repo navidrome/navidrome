b441260186b3b88069fcb4d8c84ef06e1a6a4dbb
Ruchi Kushwaha <rk07ruchi@gmail.com>
2021-04-17 10:10:07 +0530

    Change icon on active menu item (#903)
    * add icons
    
    * add logic to change the icon
    
    * make the active menu bold
    
    * Encapsulate the dynamic icon behaviour into a self-contained component
    
    Co-authored-by: Deluan <deluan@navidrome.org>

--- ui/src/artist/index.js
+++ ui/src/artist/index.js
@@ -1,7 +1,16 @@
-import MicIcon from '@material-ui/icons/Mic'
+import React from 'react'
 import ArtistList from './ArtistList'
+import DynamicMenuIcon from '../layout/DynamicMenuIcon'
+import MicNoneOutlinedIcon from '@material-ui/icons/MicNoneOutlined'
+import MicIcon from '@material-ui/icons/Mic'
 
 export default {
   list: ArtistList,
-  icon: MicIcon,
+  icon: (
+    <DynamicMenuIcon
+      path={'artist'}
+      icon={MicNoneOutlinedIcon}
+      activeIcon={MicIcon}
+    />
+  ),
 }
