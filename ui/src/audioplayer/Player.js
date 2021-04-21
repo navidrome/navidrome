0395a73b8522691ceb2621588092e120d8eebcf5
Shishir <shishir.srik@gmail.com>
2021-04-21 14:33:57 +0530

    Removed unused useState

--- ui/src/audioplayer/Player.js
+++ ui/src/audioplayer/Player.js
@@ -1,4 +1,4 @@
-import React, { useCallback, useMemo, useState } from 'react'
+import React, { useCallback, useMemo } from 'react'
 import ReactGA from 'react-ga'
 import { useDispatch, useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
