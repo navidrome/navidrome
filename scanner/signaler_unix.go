d5c7a8188853776a86e313098230db9182affebb
Deluan <deluan@navidrome.org>
2021-04-23 21:22:04 -0400

    Disable SIGUSR1 handler for Windows (not available)

---
+++ scanner/signaler_unix.go
@@ -0,0 +1,16 @@
+// +build !windows !plan9
+
+package scanner
+
+import (
+	"os"
+	"os/signal"
+	"syscall"
+)
+
+func init() {
+	signals := []os.Signal{
+		syscall.SIGUSR1,
+	}
+	signal.Notify(sigChan, signals...)
+}
