504b2221678ec813e3ea5c4a9587c46966d80ef1
Deluan <deluan@navidrome.org>
2021-05-02 16:49:20 -0400

    Fix `EnableLogRedacting` case

--- conf/configuration.go
+++ conf/configuration.go
@@ -161,7 +161,7 @@ func init() {
 	viper.SetDefault("enablestarrating", true)
 	viper.SetDefault("defaulttheme", "Dark")
 	viper.SetDefault("gatrackingid", "")
-	viper.SetDefault("EnableLogRedacting", true)
+	viper.SetDefault("enablelogredacting", true)
 	viper.SetDefault("authrequestlimit", 5)
 	viper.SetDefault("authwindowlength", 20*time.Second)
 
