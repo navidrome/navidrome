5f014920c251d4571382ebeb814995ed3fcf9424
Dnouv <61188295+Dnouv@users.noreply.github.com>
2021-05-11 20:59:55 +0530

    Replace Feature Policy with  Permissions Policy (#1112)
    * Add Permissions Policy
    
    * Remove Display capture option

--- server/middlewares.go
+++ server/middlewares.go
@@ -74,7 +74,7 @@ func secureMiddleware() func(h http.Handler) http.Handler {
 		ContentTypeNosniff: true,
 		FrameDeny:          true,
 		ReferrerPolicy:     "same-origin",
-		FeaturePolicy:      "autoplay 'none'; camera: 'none'; display-capture 'none'; microphone: 'none'; usb: 'none'",
+		PermissionsPolicy:  "autoplay=(), camera=(), microphone=(), usb=()",
 		//ContentSecurityPolicy: "script-src 'self' 'unsafe-inline'",
 	})
 	return sec.Handler
