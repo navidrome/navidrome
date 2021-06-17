3511f64c60757dc846bca77bcf71f2880b6535dd
Steve Richter <steve@nerdbra.in>
2021-06-17 02:05:52 -0400

    Initial Last.fm UI implementation

--- ui/src/personal/ScrobbleToggle.js
+++ ui/src/personal/ScrobbleToggle.js
@@ -1,24 +1,63 @@
+import { useState } from 'react'
 import { useNotify, useTranslate } from 'react-admin'
-import { useDispatch, useSelector } from 'react-redux'
-import { setNotificationsState } from '../actions'
 import {
   FormControl,
   FormControlLabel,
   LinearProgress,
   Switch,
 } from '@material-ui/core'
-import { useState } from 'react'
-import { openInNewTab } from '../utils'
+import { useInterval } from '../common'
+import { baseUrl } from '../utils'
+
+const Progress = (props) => {
+  const { setLinked, setCheckingLink } = props
+  const translate = useTranslate()
+  const notify = useNotify()
+  let linkCheckDelay = 2000
+  let linkChecks = 5
+  // openInNewTab(
+  //   '/api/lastfm/link'
+  // )
+
+  const endChecking = (success) => {
+    linkCheckDelay = null
+    setCheckingLink(false)
+    if (success) {
+      notify(translate('Last.fm successfully linked!'), 'success')
+    } else {
+      notify(translate('Last.fm could not be linked'), 'warning')
+    }
+    setLinked(success)
+  }
+
+  useInterval(() => {
+    fetch(baseUrl(`/api/lastfm/link/status?c=${linkChecks}`))
+      .then((response) => response.text())
+      .then((response) => {
+        if (response === 'true') {
+          endChecking(true)
+        }
+      })
+      .catch((error) => {
+        endChecking(false)
+        throw new Error(error)
+      })
+
+    if (--linkChecks === 0) {
+      endChecking(false)
+    }
+  }, linkCheckDelay)
+
+  return linkChecks > 0 && <LinearProgress />
+}
 
 export const ScrobbleToggle = (props) => {
   const translate = useTranslate()
   const [linked, setLinked] = useState(false)
-
-  const toggleScrobble = (event) => {
+  const [checkingLink, setCheckingLink] = useState(false)
+  const toggleScrobble = () => {
     if (!linked) {
-      openInNewTab(
-        'https://www.last.fm/api/auth/?api_key=c2918986bf01b6ba353c0bc1bdd27bea'
-      )
+      return setCheckingLink(true)
     }
     setLinked(!linked)
   }
@@ -30,14 +69,16 @@ export const ScrobbleToggle = (props) => {
           <Switch
             id={'notifications'}
             color="primary"
-            checked={linked}
-            disabled={linked}
+            checked={linked || checkingLink}
+            disabled={checkingLink}
             onChange={toggleScrobble}
           />
         }
         label={<span>{translate('Scrobble to Last.FM')}</span>}
       />
-      {linked && <LinearProgress />}
+      {checkingLink && (
+        <Progress setLinked={setLinked} setCheckingLink={setCheckingLink} />
+      )}
     </FormControl>
   )
 }
