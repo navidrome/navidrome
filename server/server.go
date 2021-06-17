fb53bba5aba74829ed00b271b58a77c40535f880
Steve Richter <steve@nerdbra.in>
2021-06-17 02:05:52 -0400

    Initial Last.fm UI implementation

--- server/server.go
+++ server/server.go
@@ -15,6 +15,7 @@ import (
 	"github.com/navidrome/navidrome/log"
 	"github.com/navidrome/navidrome/model"
 	"github.com/navidrome/navidrome/ui"
+	"github.com/navidrome/navidrome/utils"
 )
 
 type Server struct {
@@ -82,6 +83,15 @@ func (s *Server) initRoutes() {
 			r.Post("/login", login(s.ds))
 		}
 		r.Post("/createAdmin", createAdmin(s.ds))
+	})
+
+	r.Get("/api/lastfm/link/status", func(w http.ResponseWriter, r *http.Request) {
+		rs := "false"
+		c := utils.ParamInt(r, "c", 0)
+		if (c == 4) {
+			rs = "true"
+		}
+		_, _ = w.Write([]byte(rs))
 	})
 
 	// Redirect root to UI URL
