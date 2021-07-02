5f9c7d1507a2128f425f95b9cdc8649cf6599dab
Deluan <deluan@navidrome.org>
2021-07-02 09:54:09 -0400

    Add feature flag for buffered scrobbling

--- conf/configuration.go
+++ conf/configuration.go
@@ -71,6 +71,7 @@ type configOptions struct {
 	DevOldCacheLayout          bool
 	DevActivityPanel           bool
 	DevEnableShare             bool
+	DevEnableBufferedScrobble  bool
 }
 
 type scannerOptions struct {
@@ -248,6 +249,7 @@ func init() {
 	viper.SetDefault("devfastaccesscoverart", false)
 	viper.SetDefault("devactivitypanel", true)
 	viper.SetDefault("devenableshare", false)
+	viper.SetDefault("devenablebufferedscrobble", false)
 }
 
 func InitConfig(cfgFile string) {
