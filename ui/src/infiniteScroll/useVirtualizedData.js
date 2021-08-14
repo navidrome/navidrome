cbc4752f72821263cb09bd7c61251ece074f4f0f
Samarjeet <samarsault@gmail.com>
2021-08-14 19:56:25 +0530

    Optimize data fetch hook network calls

--- ui/src/infiniteScroll/useVirtualizedData.js
+++ ui/src/infiniteScroll/useVirtualizedData.js
@@ -4,67 +4,52 @@
  * within a list
  */
 
-import { useCallback, useEffect, useRef } from 'react'
-import { useSelector, useDispatch } from 'react-redux'
-import { useListContext, crudGetList } from 'ra-core'
+import { useEffect, useRef } from 'react'
+import { useSelector } from 'react-redux'
+import { useListContext } from 'ra-core'
+import { useDataProvider } from 'react-admin'
 
 export default function useVirtualizedData() {
   const { resource, perPage, currentSort, filterValues } = useListContext()
 
-  const loadPromiseResolver = useRef(null)
   const loadedIds = useRef({})
-  const lastFetchPosition = useRef({
-    startIndex: 0,
-    stopIndex: perPage,
-  })
 
   const { data, ids, total } = useSelector((state) => ({
     ids: state.admin.resources[resource].list.ids,
     data: state.admin.resources[resource].data,
     total: state.admin.resources[resource].list.total,
-    loadedOnce: state.admin.resources[resource].list.loadedOnce,
   }))
 
-  const dispatch = useDispatch()
+  const dataProvider = useDataProvider()
 
-  const getList = useCallback(
-    (...args) => dispatch(crudGetList(...args)),
-    [dispatch]
-  )
-
+  // ids change only on first mount, or an external trigger
+  // like currentSort, filterValue, delete etc
   useEffect(() => {
     loadedIds.current = {}
-    lastFetchPosition.current = { startIndex: 0, stopIndex: perPage }
-    getList(resource, { page: 1, perPage }, currentSort, filterValues)
-    return () => {
-      loadedIds.current = {}
-      lastFetchPosition.current = { startIndex: 0, stopIndex: perPage }
+    for (let i = 0; i < ids.length; i++) {
+      loadedIds.current[i] = ids[i]
     }
-  }, [currentSort, filterValues, getList, resource, perPage])
-
-  useEffect(() => {
-    const { startIndex, stopIndex } = lastFetchPosition.current
-    for (let i = startIndex; i <= stopIndex; i++) {
-      loadedIds.current[i] = ids[i - startIndex]
-    }
-
-    lastFetchPosition.current = { startIndex, stopIndex }
-
-    if (loadPromiseResolver.current) {
-      loadPromiseResolver.current()
-      loadPromiseResolver.current = null
-    }
-  }, [ids, perPage])
+  }, [ids])
 
   const handleLoadMore = ({ startIndex, stopIndex }) => {
-    const page = Math.floor(startIndex / perPage) + 1
-    const newStopIndex = Math.min(total, startIndex + perPage - 1)
+    // React Admin always fetches the first page for us, so we
+    // don't need to fetch it again
+    if (startIndex < perPage) return
 
-    return new Promise((resolve) => {
-      loadPromiseResolver.current = resolve
-      lastFetchPosition.current = { startIndex, stopIndex: newStopIndex }
-      getList(resource, { page: page, perPage }, currentSort, filterValues)
-    })
+    const page = Math.floor(startIndex / perPage) + 1
+
+    return dataProvider
+      .getList(resource, {
+        pagination: { page, perPage },
+        sort: currentSort,
+        filter: filterValues,
+      })
+      .then(({ data, total }) => {
+        const newStopIndex = Math.min(total - 1, startIndex + perPage - 1)
+        for (let i = startIndex; i <= newStopIndex; i++) {
+          loadedIds.current[i] = data[i - startIndex].id
+        }
+      })
   }
 
   return { data, loadedIds: loadedIds.current, total, handleLoadMore }
