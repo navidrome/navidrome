865dce8faf59dd0349b22c0b6f53a28b68190888
Dnouv <sdevanshu90@yahoo.com>
2021-08-15 13:52:28 +0530

    Remove errors

--- ui/src/common/ArtistLinkField.js
+++ ui/src/common/ArtistLinkField.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { Link } from 'react-admin'
-import { useAlbumsPerPage } from './index'
+// import { useAlbumsPerPage } from './index'
 import { withWidth } from '@material-ui/core'
 import ArtistView from './ArtistDetail'
 
