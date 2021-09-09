9fbd6792ec9faa1518a4a98ccecd9f6f996bb901
Salman Inayat <45660519+Salman-Inayat@users.noreply.github.com>
2021-09-09 20:56:48 +0500

    Album size overflow fixed (#1071)
    * Added back button
    
    * Added back button
    
    * Added back button
    
    * Fixed Album size overflow
    
    * Fixed Album size overflow
    
    * Fixed album size overflowing
    
    * Fixed album size overflowing
    
    * Fixed album size overflowing
    
    * Fixed album size overflow on small screen
    
    * Changes reverted in PlayerEdit.js
    
    * prettier formatting issue resolved
    
    Co-authored-by: Deluan <deluan@navidrome.org>

--- ui/src/common/SizeField.js
+++ ui/src/common/SizeField.js
@@ -2,10 +2,18 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { formatBytes } from '../utils'
 import { useRecordContext } from 'react-admin'
+import { makeStyles } from '@material-ui/core'
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'inline-block',
+  },
+}))
 
 export const SizeField = ({ source, ...rest }) => {
+  const classes = useStyles()
   const record = useRecordContext(rest)
-  return <span>{formatBytes(record[source])}</span>
+  return <span className={classes.root}>{formatBytes(record[source])}</span>
 }
 
 SizeField.propTypes = {
