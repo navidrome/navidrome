7b391370f4c6e511cc1352a8c4a9bb1192958602
Zane van Iperen <zane@zanevaniperen.com>
2021-09-30 11:23:44 +1000

    model/properties: add DeletePrefixed()

--- model/properties.go
+++ model/properties.go
@@ -8,5 +8,6 @@ type PropertyRepository interface {
 	Put(id string, value string) error
 	Get(id string) (string, error)
 	Delete(id string) error
+	DeletePrefixed(prefix string) error
 	DefaultGet(id string, defaultValue string) (string, error)
 }
