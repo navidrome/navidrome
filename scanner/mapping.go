1dd430f2d8fd16f0588028b5f02d0dc6f2f71343
Dheeraj Lalwani <lalwanidheeraj1234@gmail.com>
2021-10-08 19:00:51 +0000

    Add function 'isSynced' that identifies if lyrics are synced or not and add tests for the same

--- scanner/mapping.go
+++ scanner/mapping.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
+	"regexp"
 	"strings"
 	"time"
 
@@ -164,4 +165,13 @@ func (s mediaFileMapper) mapGenres(genres []string) (string, model.Genres) {
 		return "", nil
 	}
 	return result[0].Name, result
+}
+
+func isSynced(rawLyrics string) bool {
+	// regex for matching timestamp
+	r := regexp.MustCompile(`(\[([0-9]{1,2}:)?([0-9]{1,2}:)([0-9]{1,2})(\.[0-9]{1,2})?\])(.*)$`)
+	// Eg: [04:02:50.85]
+	// [02:50.85]
+	// [02:50]
+	return r.MatchString(rawLyrics)
 }
