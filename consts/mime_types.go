fc3028d7447566a4dad90e29a695e56e01d1510b
Deluan <deluan@navidrome.org>
2021-10-27 15:00:32 -0400

    Add `.mka` file format. Only works with `ffmpeg` extractor

--- consts/mime_types.go
+++ consts/mime_types.go
@@ -33,6 +33,7 @@ var audioFormats = map[string]format{
 	".dsf":  {typ: "audio/dsd", lossless: true},
 	".wv":   {typ: "audio/x-wavpack", lossless: true},
 	".wvp":  {typ: "audio/x-wavpack", lossless: true},
+	".mka":  {typ: "audio/x-matroska"},
 }
 var imageFormats = map[string]string{
 	".gif":  "image/gif",
