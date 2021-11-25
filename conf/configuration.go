7be85ed032f640b03acc7d4450eadc8e5e56302a
Deluan <deluan@navidrome.org>
2021-11-25 15:48:32 -0500

    Better logging for agents configuration

--- conf/configuration.go
+++ conf/configuration.go
@@ -169,6 +169,7 @@ func disableExternalServices() {
 	Server.LastFM.Enabled = false
 	Server.Spotify.ID = ""
 	Server.ListenBrainz.Enabled = false
+	Server.Agents = ""
 	if Server.UILoginBackgroundURL == consts.DefaultUILoginBackgroundURL {
 		Server.UILoginBackgroundURL = consts.DefaultUILoginBackgroundURLOffline
 	}
