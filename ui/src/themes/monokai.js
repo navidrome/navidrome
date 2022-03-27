e7cf74d863569911ee99c7835b5f86e736a6514c
Matt Doyle <1092786+chief8192@users.noreply.github.com>
2022-03-26 21:14:13 -0400

    Adds a Monokai theme (#1669)
    * Adds a new Monokai theme
    
    * Deletes a commented-out line

---
+++ ui/src/themes/monokai.js
@@ -0,0 +1,100 @@
+export default {
+  themeName: 'Monokai',
+  palette: {
+    primary: {
+      main: '#66d9ef',
+    },
+    secondary: {
+      main: '#49483e',
+      contrastText: '#f8f8f2',
+    },
+    type: 'dark',
+    background: {
+      default: '#272822',
+    },
+  },
+  overrides: {
+    MuiPaper: {
+      root: {
+        color: '#f8f8f2',
+        backgroundColor: '#3b3a32',
+      },
+    },
+    MuiButton: {
+      textPrimary: {
+        color: '#66d9ef',
+      },
+      textSecondary: {
+        color: '#f8f8f2',
+      },
+    },
+    MuiChip: {
+      clickable: {
+        background: '#49483e',
+      },
+    },
+    MuiFormGroup: {
+      root: {
+        color: '#f8f8f2',
+      },
+    },
+    MuiFormHelperText: {
+      root: {
+        Mui: {
+          error: {
+            color: '#f92672',
+          },
+        },
+      },
+    },
+    MuiTableHead: {
+      root: {
+        color: '#f8f8f2',
+        background: '#3b3a32 !important',
+      },
+    },
+    MuiTableCell: {
+      root: {
+        color: '#f8f8f2',
+        background: '#3b3a32 !important',
+      },
+      head: {
+        color: '#f8f8f2',
+        background: '#3b3a32 !important',
+      },
+    },
+    MuiSnackbarContent: {
+      message: {
+        color: '#f8f8f2',
+        backgroundColor: '#f92672',
+      },
+    },
+    NDLogin: {
+      systemNameLink: {
+        color: '#66d9ef',
+      },
+      icon: {},
+      welcome: {
+        color: '#f8f8f2',
+      },
+      card: {
+        minWidth: 300,
+        background: '#3b3a32',
+      },
+      avatar: {},
+      button: {
+        boxShadow: '3px 3px 5px #272822',
+      },
+    },
+    NDMobileArtistDetails: {
+      bgContainer: {
+        background:
+          'linear-gradient(to bottom, rgba(52 52 52 / 72%), rgb(48 48 48))!important',
+      },
+    },
+  },
+  player: {
+    theme: 'monokai',
+    stylesheet: require('./monokai.css.js'),
+  },
+}
