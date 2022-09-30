d65d48b490e517376298a46573e4bb5473dda8ff
William Lohan <william.g.lohan@gmail.com>
2022-09-30 16:54:00 -0700

    Add theme Electric Purple (#1889)
    * add theme file
    
    add theme file electricPurple.js
    
    * import theme file 
    
    import theme file  electricPurple
    
    * add electricPurple.css.js

---
+++ ui/src/themes/electricPurple.css.js
@@ -0,0 +1,39 @@
+module.exports = `
+
+.react-jinke-music-player-main svg:active, .react-jinke-music-player-main svg:hover {
+    color: #bd4aff;
+}
+
+.react-jinke-music-player-main .music-player-panel .panel-content .rc-slider-handle, .react-jinke-music-player-main .music-player-panel .panel-content .rc-slider-track {
+    background-color: #8800cb;
+}
+
+.react-jinke-music-player-main ::-webkit-scrollbar-thumb {
+    background-color: #8800cb;
+}
+
+.react-jinke-music-player-main .music-player-panel .panel-content .rc-slider-handle:active {
+    box-shadow: 0 0 2px #8800cb;
+}
+
+.react-jinke-music-player-main .audio-item.playing svg {
+    color: #8800cb;
+}
+
+.react-jinke-music-player-main .audio-item.playing .player-singer {
+    color: #8800cb !important;
+}
+
+.audio-lists-panel-content .audio-item.playing, .audio-lists-panel-content .audio-item.playing svg {
+    color: #8800cb;
+}
+.audio-lists-panel-content .audio-item:active .group:not([class=".player-delete"]) svg, .audio-lists-panel-content .audio-item:hover .group:not([class=".player-delete"]) svg {
+    color: #8800cb;
+}
+
+.react-jinke-music-player-mobile-progress .rc-slider-handle, .react-jinke-music-player-mobile-progress .rc-slider-track {
+    background-color: #8800cb;
+}
+
+
+`
---
+++ ui/src/themes/electricPurple.js
@@ -0,0 +1,55 @@
+export default {
+  themeName: 'Electric Purple',
+  palette: {
+    primary: {
+      light: '#f757ff',
+      dark: '#8800cb',
+      main: '#bf00ff',
+      contrastText: '#fff',
+    },
+    secondary: {
+      light: '#bd4aff',
+      dark: '#530099',
+      main: '#8800cb',
+      contrastText: '#fff',
+    },
+    warn: {
+      light: '#ffff82',
+      dark: '#c9bf07',
+      main: '#fff14e',
+      contrastText: '#000',
+    },
+    error: {
+      light: '#ff763a',
+      dark: '#c30000',
+      main: '#ff3f00',
+      contrastText: '#000',
+    },
+    type: 'dark',
+  },
+  overrides: {
+    MuiFormGroup: {
+      root: {
+        color: 'white',
+      },
+    },
+    NDLogin: {
+      systemNameLink: {
+        color: '#fff',
+      },
+      welcome: {
+        color: '#eee',
+      },
+    },
+    NDMobileArtistDetails: {
+      bgContainer: {
+        background:
+          'linear-gradient(to bottom, rgba(52 52 52 / 72%), rgb(48 48 48))!important',
+      },
+    },
+  },
+  player: {
+    theme: 'dark',
+    stylesheet: require('./electricPurple.css.js'),
+  },
+}
