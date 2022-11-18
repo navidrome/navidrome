cad0523aa0fad08a3b19420e68fa0a69d0fff852
Aleksey Lobanov <i@likemath.ru>
2022-11-18 23:59:03 +0300

    refactor: Add todos for useful for metrics methods

--- model/properties.go
+++ model/properties.go
@@ -10,4 +10,5 @@ type PropertyRepository interface {
 	Get(id string) (string, error)
 	Delete(id string) error
 	DefaultGet(id string, defaultValue string) (string, error)
+	// TODO: GetAll() (map[string]string, error) may be useful
 }
