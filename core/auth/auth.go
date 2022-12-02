5e1ed19bf887b8bddcfe5e4242024500f7bd30d3
Deluan <deluan@navidrome.org>
2022-12-02 17:58:38 -0500

    Bump github.com/go-chi/jwtauth/v5 from 5.0.2 to 5.1.0

--- core/auth/auth.go
+++ core/auth/auth.go
@@ -6,7 +6,7 @@ import (
 	"time"
 
 	"github.com/go-chi/jwtauth/v5"
-	"github.com/lestrrat-go/jwx/jwt"
+	"github.com/lestrrat-go/jwx/v2/jwt"
 	"github.com/navidrome/navidrome/conf"
 	"github.com/navidrome/navidrome/consts"
 	"github.com/navidrome/navidrome/log"
