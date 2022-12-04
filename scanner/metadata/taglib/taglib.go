4489c3475716e0c70d6ebacc3b82bae7563093ab
Reo <112817228+reopt999@users.noreply.github.com>
2022-12-05 00:48:21 +0700

    Fix Misleading Error Message on unreadable Media due to Permission (#1873)
    * fix(taglib): Fix misleading error message on unreadable media - #1576
    
    Signed-off-by: reo <reo_999@proton.me>
    
    * fix(taglib): Add unit test and exclude scan for only unreadable file - #1576
    
    Signed-off-by: reo <reo_999@proton.me>
    
    * fix(taglib): Add unit test and exclude scan for only unreadable file - #1576
    
    Signed-off-by: reo <reo_999@proton.me>
    
    * fix(taglib): Add unit test and exclude scan for only unreadable file - #1576
    
    Signed-off-by: reo <reo_999@proton.me>
    
    * fix(taglib): Add unit test and exclude scan for only unreadable file - #1576
    
    Signed-off-by: reo <reo_999@proton.me>
    
    * fix(taglib): Add unit test and exclude scan for only unreadable file - #1576
    
    Signed-off-by: reo <reo_999@proton.me>
    
    * Fix test and simplify code a bit
    
    We don't need to expose the type of error: `taglib.Parse()` always return nil
    
    * Fix comment
    
    Signed-off-by: reo <reo_999@proton.me>
    Co-authored-by: Deluan <deluan@navidrome.org>

--- scanner/metadata/taglib/taglib.go
+++ scanner/metadata/taglib/taglib.go
@@ -1,6 +1,8 @@
 package taglib
 
 import (
+	"errors"
+	"os"
 	"strconv"
 
 	"github.com/navidrome/navidrome/log"
@@ -13,16 +15,19 @@ type parsedTags = map[string][]string
 func (e *Parser) Parse(paths ...string) (map[string]parsedTags, error) {
 	fileTags := map[string]parsedTags{}
 	for _, path := range paths {
-		fileTags[path] = e.extractMetadata(path)
+		tags, err := e.extractMetadata(path)
+		if !errors.Is(err, os.ErrPermission) {
+			fileTags[path] = tags
+		}
 	}
 	return fileTags, nil
 }
 
-func (e *Parser) extractMetadata(filePath string) parsedTags {
+func (e *Parser) extractMetadata(filePath string) (parsedTags, error) {
 	tags, err := Read(filePath)
 	if err != nil {
-		log.Warn("Error reading metadata from file. Skipping", "filePath", filePath, err)
-		return nil
+		log.Warn("TagLib: Error reading metadata from file. Skipping", "filePath", filePath, err)
+		return nil, err
 	}
 
 	alternativeTags := map[string][]string{
@@ -46,5 +51,5 @@ func (e *Parser) extractMetadata(filePath string) parsedTags {
 			}
 		}
 	}
-	return tags
+	return tags, nil
 }
