71c9048029520a4af99275515d1903e2f026ae2f
Deluan <deluan@navidrome.org>
2022-12-14 11:50:16 -0500

    Fix overriding previous logger in context

--- server/subsonic/middlewares.go
+++ server/subsonic/middlewares.go
@@ -100,7 +100,6 @@ func authenticate(ds model.DataStore) func(next http.Handler) http.Handler {
 
 			ctx = log.NewContext(r.Context(), "username", username)
 			ctx = request.WithUser(ctx, *usr)
-			ctx = request.WithUsername(ctx, usr.UserName)
 			r = r.WithContext(ctx)
 
 			next.ServeHTTP(w, r)
