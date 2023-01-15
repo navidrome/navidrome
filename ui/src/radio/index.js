e99f832e599e721d3dc574c4f1f22557a2a9b7be
Deluan <deluan@navidrome.org>
2023-01-14 19:22:47 -0500

    Add dynamic sidebar icon to Radios

--- ui/src/radio/index.js
+++ ui/src/radio/index.js
@@ -1,12 +1,21 @@
-import RadioIcon from '@material-ui/icons/Radio'
 import RadioCreate from './RadioCreate'
 import RadioEdit from './RadioEdit'
 import RadioList from './RadioList'
 import RadioShow from './RadioShow'
+import DynamicMenuIcon from '../layout/DynamicMenuIcon'
+import RadioIcon from '@material-ui/icons/Radio'
+import RadioOutlinedIcon from '@material-ui/icons/RadioOutlined'
+import React from 'react'
 
 const all = {
   list: RadioList,
-  icon: <RadioIcon />,
+  icon: (
+    <DynamicMenuIcon
+      path={'radio'}
+      icon={RadioOutlinedIcon}
+      activeIcon={RadioIcon}
+    />
+  ),
   show: RadioShow,
 }
 
