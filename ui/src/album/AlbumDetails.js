cf05f0de3d5046350872d173a5f4f36528e32e7e
certuna <62144283+certuna@users.noreply.github.com>
2023-01-17 00:52:54 +0100

    Update AlbumDetails.js

--- ui/src/album/AlbumDetails.js
+++ ui/src/album/AlbumDetails.js
@@ -193,15 +193,13 @@ const Details = (props) => {
   const yearsEqual = (releaseYear == year)
   year && addDetail(
     <>
-      {(!yearsEqual && (
-      !isXsmall ? "Original " : "♫ ")) +
-      year
+      {[!yearsEqual && (!isXsmall ? "Original " : "♫ "),year].join()
       }
     </>
   )
   !yearsEqual && addDetail(
     <>
-      {(!isXsmall ? "Released " : "☼ ") + releaseYear
+      {[(!isXsmall ? "Released " : "◘ "),releaseYear].join()
       }
     </>
  )
