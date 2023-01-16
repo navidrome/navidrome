f6e4e491dd80bf7e678cecb442b67259dbec1e95
certuna <62144283+certuna@users.noreply.github.com>
2023-01-17 00:13:52 +0100

    Update RangeFieldDouble.js

--- ui/src/common/RangeFieldDouble.js
+++ ui/src/common/RangeFieldDouble.js
@@ -1,16 +1,29 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { useRecordContext } from 'react-admin'
-import RangeField from '../common'
+
+export const formatRange2 = (record, source) => {
+  const nameCapitalized = source.charAt(0).toUpperCase() + source.slice(1)
+  const min = record[`min${nameCapitalized}`]
+  const max = record[`max${nameCapitalized}`]
+  let range = []
+  if (min) {
+    range.push(min)
+  }
+  if (max && max !== min) {
+    range.push(max)
+  }
+  return range.join('-')
+}
 
 export const RangeFieldDouble = ({ className, source1, source2, ...rest }) => {
   const record = useRecordContext(rest)
-  if (formatRange(record, source1) == formatRange(record, source2)) {
-      return <span className={className}>{formatRange(record, source1)}</span>
+  if (formatRange2(record, source1) == formatRange2(record, source2)) {
+      return <span className={className}>{formatRange2(record, source1)}</span>
   } else {
   return <span className={className}>{"♫ " +
-    formatRange(record, source1) + " · □ " +
-    formatRange(record, source2)}</span>
+    formatRange2(record, source1) + " · □ " +
+    formatRange2(record, source2)}</span>
   }
 }
 
