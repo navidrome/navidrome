5e0b6387ef45789490d182c0c541dd7c13550328
certuna <62144283+certuna@users.noreply.github.com>
2023-01-16 18:19:17 +0100

    Update mapping.go

--- scanner/mapping.go
+++ scanner/mapping.go
@@ -129,8 +129,9 @@ func (s mediaFileMapper) trackID(md metadata.Tags) string {
 
 func (s mediaFileMapper) albumID(md metadata.Tags) string {
 	albumPath := strings.ToLower(fmt.Sprintf("%s\\%s", s.mapAlbumArtistName(md), s.mapAlbumName(md)))
+	
 	if conf.Server.DevUseOriginalDate && !conf.Server.DevGroupAlbumEditions && md.ReleaseYear() > 0 {
-		albumPath := fmt.Sprintf("%d\\%d", albumPath, string(md.ReleaseYear()))
+		albumPath := albumPath + string(md.ReleaseYear())
 	}
 	return fmt.Sprintf("%x", md5.Sum([]byte(albumPath)))
 }
