6bb5328ad5218dd93511acf7f12e92dd4c82ea65
certuna <62144283+certuna@users.noreply.github.com>
2023-01-17 08:32:39 +0100

    Update RangeFieldDouble.js

--- ui/src/common/RangeFieldDouble.js
+++ ui/src/common/RangeFieldDouble.js
@@ -3,13 +3,13 @@ import PropTypes from 'prop-types'
 import { useRecordContext } from 'react-admin'
 import { formatRange } from '../common'
 
-export const RangeFieldDouble = ({ className, source1, source2, ...rest }) => {
+export const RangeFieldDouble = ({ className, source1, source2, symbol1, symbol2, divider, ...rest }) => {
   const record = useRecordContext(rest)
   if (formatRange(record, source1) == formatRange(record, source2)) {
       return <span className={className}>{formatRange(record, source1)}</span>
   } else {
-  return <span className={className}>{"♫ " +
-    formatRange(record, source1) + " · ○ " +
+  return <span className={className}>{symbol1 +
+    formatRange(record, source1) + divider + symbol2 +
     formatRange(record, source2)}</span>
   }
 }
