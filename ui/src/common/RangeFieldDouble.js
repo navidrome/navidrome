c0f30a9a39d7e1383b977476149a5474f84e13b4
certuna <62144283+certuna@users.noreply.github.com>
2023-01-18 17:01:30 +0100

    Update RangeFieldDouble.js

--- ui/src/common/RangeFieldDouble.js
+++ ui/src/common/RangeFieldDouble.js
@@ -5,13 +5,28 @@ import { formatRange } from '../common'
 
 export const RangeFieldDouble = ({ className, source1, source2, symbol1, symbol2, divider, ...rest }) => {
   const record = useRecordContext(rest)
+  if (formatRange(record,source2).includes('-')) {
+      return <span className={className}>{
+            formatRange(record, source1) +
+            divider +
+            "\(\( " +
+            symbol2 + 
+            "\)\)"
+            }</span>
+   }
   if (formatRange(record, source1) === formatRange(record, source2)) {
-      return <span className={className}>{formatRange(record, source1)}</span>
-  } else {
-  return <span className={className}>{symbol1 +
-    formatRange(record, source1) + divider + symbol2 +
-    formatRange(record, source2)}</span>
-  }
+          return <span className={className}>{
+            formatRange(record, source1)
+            }</span>
+        } else {
+          return <span className={className}>{
+            symbol1 +
+            formatRange(record, source1) +
+            divider +
+            symbol2 +
+            formatRange(record, source2)
+           }</span>
+        }
 }
 
 RangeFieldDouble.propTypes = {
