ff1b9d5160318d2bf4b0790631b80bf7cc8bea5e
Matthias Schmidt <matthias.schmidt@gmail.com>
2023-01-23 17:23:56 +0100

    First draft on parsing the action

--- server/subsonic/jukebox.go
+++ server/subsonic/jukebox.go
@@ -6,7 +6,63 @@ import (
 	"github.com/navidrome/navidrome/server/subsonic/responses"
 )
 
+type ActionType int
+
+const (
+	ActionUnknown ActionType = iota
+	ActionGet
+	ActionStatus
+	ActionSet
+	ActionStart
+	ActionStop
+	ActionSkip
+	ActionAdd
+	ActionClear
+	ActionRemove
+	ActionShuffle
+	ActionSetGain
+)
+
+func parseAction(action string) ActionType {
+	switch action {
+	case "get":
+		return ActionGet
+	case "status":
+		return ActionStatus
+	case "set":
+		return ActionSet
+	case "start":
+		return ActionStart
+	case "stop":
+		return ActionStop
+	case "skip":
+		return ActionSkip
+	case "add":
+		return ActionAdd
+	case "clear":
+		return ActionClear
+	case "remove":
+		return ActionRemove
+	case "shuffle":
+		return ActionShuffle
+	case "setGain":
+		return ActionSetGain
+	default:
+		return ActionUnknown
+	}
+}
+
 func (api *Router) JukeboxControl(r *http.Request) (*responses.Subsonic, error) {
+	actionString, err := requiredParamString(r, "action")
+	if err != nil {
+		return nil, err
+	}
+
+	action := parseAction(actionString)
+	if action == ActionUnknown {
+		return nil, newError(responses.ErrorMissingParameter, "Unknown action: %s", actionString)
+	}
+
 	response := newResponse()
 	response.JukeboxStatus = &responses.JukeboxStatus{}
 	response.JukeboxStatus.CurrentIndex = 0
