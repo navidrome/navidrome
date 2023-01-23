8c781116c9ea6023b61ea07717c81e3d356850fe
certuna <62144283+certuna@users.noreply.github.com>
2023-01-23 19:02:53 +0100

    Update album.go

--- model/album.go
+++ model/album.go
@@ -20,7 +20,7 @@ type Album struct {
 	AllArtistIDs         string    `structs:"all_artist_ids" json:"allArtistIds"  orm:"column(all_artist_ids)"`
 	MaxYear              int       `structs:"max_year" json:"maxYear"`
 	MinYear              int       `structs:"min_year" json:"minYear"`
-	Date				 time.Time `structs:"date" json:"date,omitempty"`
+	Date		     time.Time `structs:"date" json:"date,omitempty"`
 	MinReleaseYear       int       `structs:"min_release_year" json:"minReleaseYear,omitempty"`
 	MaxReleaseYear       int       `structs:"max_release_year" json:"maxReleaseYear,omitempty"`
 	ReleaseDate 	     time.Time `structs:"release_date" json:"releaseDate,omitempty"`
