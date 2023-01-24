17d9573f4defdc777afe6975a7f47e39b3d5cc56
Deluan <deluan@navidrome.org>
2023-01-24 11:53:42 -0500

    Refactor dialogs, make it simple to add a new dialog to all views

---
+++ ui/src/dialogs/Dialogs.js
@@ -0,0 +1,11 @@
+import { AddToPlaylistDialog } from './AddToPlaylistDialog'
+import DownloadMenuDialog from './DownloadMenuDialog'
+import { HelpDialog } from './HelpDialog'
+
+export const Dialogs = (props) => (
+  <>
+    <AddToPlaylistDialog />
+    <DownloadMenuDialog />
+    <HelpDialog />
+  </>
+)
