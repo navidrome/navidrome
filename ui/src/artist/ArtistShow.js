ebbbb0a3befe62f60467e1f6277b05c77e940486
certuna <62144283+certuna@users.noreply.github.com>
2023-01-24 14:01:37 +0100

    Update ArtistShow.js

--- ui/src/artist/ArtistShow.js
+++ ui/src/artist/ArtistShow.js
@@ -62,7 +62,7 @@ const AlbumShowLayout = (props) => {
           addLabel={false}
           reference="album"
           target="artist_id"
-          sort={{ field: 'max_year', order: 'ASC' }}
+          sort={{ field: 'max_year asc,date asc', order: 'ASC' }}
           filter={{ artist_id: record?.id }}
           perPage={0}
           pagination={null}
