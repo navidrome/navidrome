30bcf26c5e2a1f73929d372a1b14ca936c6ab8af
Matthias Schmidt <matthias.schmidt@gmail.com>
2023-02-13 10:14:33 +0100

    Merge jukebox_action.go into jukebox.go

--- server/subsonic/jukebox.go
+++ server/subsonic/jukebox.go
@@ -4,11 +4,60 @@ import (
 	"fmt"
 	"net/http"
 	"strconv"
+	"strings"
 
 	"github.com/navidrome/navidrome/core/playback"
 	"github.com/navidrome/navidrome/log"
 	"github.com/navidrome/navidrome/server/subsonic/responses"
 )
+
+type ActionType int
+
+const (
+	ActionUnknown ActionType = iota
+	ActionGet
+	ActionStatus
+	ActionSet
+	ActionStart
+	ActionStop
+	ActionSkip
+	ActionAdd
+	ActionClear
+	ActionRemove
+	ActionShuffle
+	ActionSetGain
+)
+
+var ACTION_MAP = map[ActionType]string{
+	ActionGet:     "get",
+	ActionStatus:  "status",
+	ActionSet:     "set",
+	ActionStart:   "start",
+	ActionStop:    "stop",
+	ActionSkip:    "skip",
+	ActionAdd:     "add",
+	ActionClear:   "clear",
+	ActionRemove:  "remove",
+	ActionShuffle: "shuffle",
+	ActionSetGain: "setGain",
+}
+
+func (action ActionType) String() string {
+	value, found := ACTION_MAP[action]
+	if found {
+		return strings.ToUpper(value)
+	}
+	return "Unknown"
+}
+
+func parseAction(actionStr string) ActionType {
+	for k, v := range ACTION_MAP {
+		if v == actionStr {
+			return k
+		}
+	}
+	return ActionUnknown
+}
 
 func (api *Router) JukeboxControl(r *http.Request) (*responses.Subsonic, error) {
 	user, err := requiredParamString(r, "u")
