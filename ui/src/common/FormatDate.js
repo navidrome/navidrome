b29f11c4d14acd0b9987e42901a269b43af0b02e
certuna <62144283+certuna@users.noreply.github.com>
2023-03-01 16:31:28 +0100

    Update FormatDate.js

--- ui/src/common/FormatDate.js
+++ ui/src/common/FormatDate.js
@@ -1,20 +1,20 @@
 export const FormatFullDate = (date) => {
-  const dashes = date.split("-").length - 1
+  const dashes = date.split('-').length - 1
   let options = {
-    year: "numeric"
+    year: 'numeric',
   }
-  switch(dashes) {
+  switch (dashes) {
     case 2:
       options = {
-        year: "numeric",
-        month: "long",
-        day: "numeric"
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric',
       }
       return new Date(date).toLocaleDateString(undefined, options)
     case 1:
       options = {
-        year: "numeric",
-        month: "long"
+        year: 'numeric',
+        month: 'long',
       }
       return new Date(date).toLocaleDateString(undefined, options)
     case 0:
