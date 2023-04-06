c2b763d2e05c07364a4eda186d7c7aeb91c7be47
Subhajit Ghosh <subhajitstd07@gmail.com>
2023-04-06 20:57:16 +0530

    fix(pipeline):  - #2174
    Signed-off-by: Subhajit Ghosh <subhajitstd07@gmail.com>

--- ui/.prettierrc.js
+++ ui/.prettierrc.js
@@ -1,5 +1,5 @@
 module.exports = {
   singleQuote: true,
   semi: false,
-  arrowParens: 'always'
+  arrowParens: 'always',
 }
--- ui/public/navidrome-service-worker.js
+++ ui/public/navidrome-service-worker.js
@@ -6,48 +6,46 @@ workbox.setConfig({
   debug: false,
 })
 
-workbox.loadModule('workbox-core');
-workbox.loadModule('workbox-strategies');
-workbox.loadModule('workbox-routing');
-workbox.loadModule('workbox-navigation-preload');
+workbox.loadModule('workbox-core')
+workbox.loadModule('workbox-strategies')
+workbox.loadModule('workbox-routing')
+workbox.loadModule('workbox-navigation-preload')
 
-workbox.core.clientsClaim();
-self.skipWaiting();
+workbox.core.clientsClaim()
+self.skipWaiting()
 
 addEventListener('message', (event) => {
   if (event.data && event.data.type === 'SKIP_WAITING') {
-    skipWaiting();
+    skipWaiting()
   }
-});
+})
 
-
-const CACHE_NAME = 'offline-html';
+const CACHE_NAME = 'offline-html'
 // This assumes /offline.html is a URL for your self-contained
 // (no external images or styles) offline page.
-const FALLBACK_HTML_URL = './offline.html';
+const FALLBACK_HTML_URL = './offline.html'
 // Populate the cache with the offline HTML page when the
 // service worker is installed.
 self.addEventListener('install', async (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then((cache) => cache.add(FALLBACK_HTML_URL))
-  );
-});
+    caches.open(CACHE_NAME).then((cache) => cache.add(FALLBACK_HTML_URL))
+  )
+})
 
-const networkOnly = new workbox.strategies.NetworkOnly();
+const networkOnly = new workbox.strategies.NetworkOnly()
 const navigationHandler = async (params) => {
   try {
     // Attempt a network request.
-    return await networkOnly.handle(params);
+    return await networkOnly.handle(params)
   } catch (error) {
     // If it fails, return the cached HTML.
     return caches.match(FALLBACK_HTML_URL, {
       cacheName: CACHE_NAME,
-    });
+    })
   }
-};
+}
 
 // Register this strategy to handle all navigations.
 workbox.routing.registerRoute(
   new workbox.routing.NavigationRoute(navigationHandler)
-);
+)
