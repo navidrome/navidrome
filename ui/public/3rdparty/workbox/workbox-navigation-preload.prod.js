796a30a636ed5bc16e1948a551e0bc1ed65a916b
Subhajit Ghosh <subhajitstd07@gmail.com>
2023-04-06 21:13:25 +0530

    fix #2174 issue
    Signed-off-by: Subhajit Ghosh <subhajitstd07@gmail.com>

--- ui/public/3rdparty/workbox/workbox-navigation-preload.prod.js
+++ ui/public/3rdparty/workbox/workbox-navigation-preload.prod.js
@@ -1,2 +1,33 @@
-this.workbox=this.workbox||{},this.workbox.navigationPreload=function(t){"use strict";try{self["workbox:navigation-preload:6.2.4"]&&_()}catch(t){}function e(){return Boolean(self.registration&&self.registration.navigationPreload)}return t.disable=function(){e()&&self.addEventListener("activate",(t=>{t.waitUntil(self.registration.navigationPreload.disable().then((()=>{})))}))},t.enable=function(t){e()&&self.addEventListener("activate",(e=>{e.waitUntil(self.registration.navigationPreload.enable().then((()=>{t&&self.registration.navigationPreload.setHeaderValue(t)})))}))},t.isSupported=e,t}({});
+;(this.workbox = this.workbox || {}),
+  (this.workbox.navigationPreload = (function (t) {
+    'use strict'
+    try {
+      self['workbox:navigation-preload:6.2.4'] && _()
+    } catch (t) {}
+    function e() {
+      return Boolean(self.registration && self.registration.navigationPreload)
+    }
+    return (
+      (t.disable = function () {
+        e() &&
+          self.addEventListener('activate', (t) => {
+            t.waitUntil(
+              self.registration.navigationPreload.disable().then(() => {})
+            )
+          })
+      }),
+      (t.enable = function (t) {
+        e() &&
+          self.addEventListener('activate', (e) => {
+            e.waitUntil(
+              self.registration.navigationPreload.enable().then(() => {
+                t && self.registration.navigationPreload.setHeaderValue(t)
+              })
+            )
+          })
+      }),
+      (t.isSupported = e),
+      t
+    )
+  })({}))
 //# sourceMappingURL=workbox-navigation-preload.prod.js.map
