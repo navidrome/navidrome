da21acba92fc8bb79a7b83383d50c0ad3d577897
Subhajit Ghosh <99127578+subhajit20@users.noreply.github.com>
2023-04-08 23:09:59 +0530

    Give page the right lang attribute (#2299)
    * Fixed issue no #2174
    
    Signed-off-by: Subhajit Ghosh <subhajitstd07@gmail.com>
    
    * Fixed issue no #2174
    
    ---------
    
    Signed-off-by: Subhajit Ghosh <subhajitstd07@gmail.com>

--- ui/src/App.js
+++ ui/src/App.js
@@ -142,6 +142,8 @@ const Admin = (props) => {
 }
 
 const AppWithHotkeys = () => {
+  let language = localStorage.getItem('locale') || 'en'
+  document.documentElement.lang = language
   if (config.enableSharing && shareInfo) {
     return <SharePlayer />
   }
