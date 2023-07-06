4f453f991890b30f0bb21d7dd396bf88ff745258
certuna <62144283+certuna@users.noreply.github.com>
2023-07-06 14:41:23 +0200

    RemoveDuplicateStr()
    Add function to remove duplicate values but keep the order intact

--- utils/slice/slice.go
+++ utils/slice/slice.go
@@ -54,3 +54,16 @@ func Move[T any](slice []T, srcIndex int, dstIndex int) []T {
 	value := slice[srcIndex]
 	return Insert(Remove(slice, srcIndex), value, dstIndex)
 }
+
+func RemoveDuplicateStr(strSlice []string) []string {
+	// removes duplicates but keeps the order intact
+	allKeys := make(map[string]bool)
+	list := []string{}
+	for _, item := range strSlice {
+		if _, value := allKeys[item]; !value {
+			allKeys[item] = true
+			list = append(list, item)
+		}
+	}
+	return list
+}
