4b9989bdb362454ce7ce1dfaed9ad3d21c4f9237
certuna <62144283+certuna@users.noreply.github.com>
2023-07-06 14:45:40 +0200

    Enhanced string sanitization
    - adds a function SanitizeChars that sanitizes hyphens + single/double quotes
    - extends SanitizeStrings to sanitize for more characters
    - fixes SanitizeStrings to remove duplicate words

--- utils/sanitize_strings.go
+++ utils/sanitize_strings.go
@@ -3,14 +3,29 @@ package utils
 import (
 	"html"
 	"regexp"
-	"sort"
 	"strings"
 
 	"github.com/deluan/sanitize"
 	"github.com/microcosm-cc/bluemonday"
+	"github.com/navidrome/navidrome/utils/slice"
 )
 
-var quotesRegex = regexp.MustCompile("[“”‘’'\"\\[\\(\\{\\]\\)\\}]")
+var hyphenRegex = regexp.MustCompile("[‐–—−―]")
+var singlequotesRegex = regexp.MustCompile("[‘’‛′]")
+var doublequotesRegex = regexp.MustCompile("[＂〃ˮײ᳓″‶˶ʺ“”˝‟]")
+
+func SanitizeChars(text []string) []string {
+	var fullText []string
+	for _, w := range text {
+		w = hyphenRegex.ReplaceAllString(w, "-")
+		w = singlequotesRegex.ReplaceAllString(w, "'")
+		w = doublequotesRegex.ReplaceAllString(w, "\"")
+		fullText = append(fullText, w)
+	}
+	return fullText
+}
+
+var quotesRegex = regexp.MustCompile("[“”‘’'·\"\\[\\(\\{\\]\\)\\}]")
 
 func SanitizeStrings(text ...string) string {
 	sanitizedText := strings.Builder{}
@@ -24,12 +39,14 @@ func SanitizeStrings(text ...string) string {
 	var fullText []string
 	for w := range words {
 		w = quotesRegex.ReplaceAllString(w, "")
+		w = hyphenRegex.ReplaceAllString(w, "-")
 		if w != "" {
 			fullText = append(fullText, w)
 		}
 	}
-	sort.Strings(fullText)
-	return strings.Join(fullText, " ")
+	concatenatedString := strings.Join(fullText, " ")
+	allWords := slice.RemoveDuplicateStr(strings.Split(concatenatedString, " "))
+	return strings.Join(allWords, " ")
 }
 
 var policy = bluemonday.UGCPolicy()
