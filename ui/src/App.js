af82962bde810cd94ad35a0f61c09729d185e571
egor.aristov <egor.aristov@vk.team>
2023-07-06 21:48:02 +0300

    feat(ui): Allow drag-and-drop song title from player to sidebar playlist
    Signed-off-by: egor.aristov <egor.aristov@vk.team>

--- ui/src/App.js
+++ ui/src/App.js
@@ -36,6 +36,8 @@ import config, { shareInfo } from './config'
 import { keyMap } from './hotkeys'
 import useChangeThemeColor from './useChangeThemeColor'
 import SharePlayer from './share/SharePlayer'
+import { HTML5Backend } from 'react-dnd-html5-backend'
+import { DndProvider } from 'react-dnd'
 
 const history = createHashHistory()
 
@@ -135,7 +137,9 @@ const AppWithHotkeys = () => {
   }
   return (
     <HotKeys keyMap={keyMap}>
-      <App />
+      <DndProvider backend={HTML5Backend}>
+        <App />
+      </DndProvider>
     </HotKeys>
   )
 }
