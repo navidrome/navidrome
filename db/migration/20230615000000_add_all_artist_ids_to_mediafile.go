4975032c4d450428583a173968106589494b546c
certuna <62144283+certuna@users.noreply.github.com>
2023-07-06 14:24:45 +0200

    Create 20230615000000_add_all_artist_ids_to_mediafile.go

---
+++ db/migration/20230615000000_add_all_artist_ids_to_mediafile.go
@@ -0,0 +1,32 @@
+package migrations
+
+import (
+	"database/sql"
+
+	"github.com/pressly/goose/v3"
+)
+
+func init() {
+	goose.AddMigration(Up20230615000000, Down20230615000000)
+}
+
+func Up20230615000000(tx *sql.Tx) error {
+	_, err := tx.Exec(`
+alter table media_file
+	add all_artist_ids varchar;
+
+create index if not exists mediafile_all_artist_ids
+	on media_file (all_artist_ids);
+
+`)
+	if err != nil {
+		return err
+	}
+
+	notice(tx, "A full rescan needs to be performed to import all artist IDs")
+	return forceFullRescan(tx)
+}
+
+func Down20230615000000(tx *sql.Tx) error {
+	return nil
+}
