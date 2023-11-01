c3a6229439ac3ed20e3ade2e2f96eaea93c8fd73
certuna <62144283+certuna@users.noreply.github.com>
2023-11-01 22:46:13 +0100

    Update mapping.go
    fix conflict

--- scanner/mapping.go
+++ scanner/mapping.go
@@ -76,7 +76,7 @@ func (s mediaFileMapper) toMediaFile(md metadata.Tags) model.MediaFile {
 	mf.Lyrics = utils.SanitizeText(md.Lyrics())
 	mf.Bpm = md.Bpm()
 	mf.AlbumID = s.albumID(md, mf.ReleaseDate, mf.MbzAlbumID)
-	mf.CreatedAt = time.Now()
+	mf.CreatedAt = md.BirthTime()
 	mf.UpdatedAt = md.ModificationTime()
 
 	return *mf
