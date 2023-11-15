abcb2a98ccb52a23994168af55c37614b76ccd53
Deluan <deluan@navidrome.org>
2023-11-14 19:46:59 -0500

    Convert individual specs to a table spec

--- scanner/metadata/taglib/taglib_test.go
+++ scanner/metadata/taglib/taglib_test.go
@@ -73,8 +73,8 @@ var _ = Describe("Extractor", func() {
 			Expect(m["bitrate"][0]).To(BeElementOf("18", "39", "40"))
 		})
 
-		Context("Format-Specific tests", func() {
-			testTags := func(file, duration, channels, albumGain, albumPeak, trackGain, trackPeak string) {
+		DescribeTable("Format-Specific tests",
+			func(file, duration, channels, albumGain, albumPeak, trackGain, trackPeak string) {
 				file = "tests/fixtures/" + file
 				mds, err := e.Parse(file)
 				Expect(err).NotTo(HaveOccurred())
@@ -109,44 +109,30 @@ var _ = Describe("Extractor", func() {
 				Expect(m).To(HaveKeyWithValue("bpm", []string{"123"}))
 
 				Expect(m).To(HaveKey("tracknumber"))
-				trackno := m["tracknumber"]
-				Expect(trackno).To(HaveLen(1))
-				Expect(trackno[0]).To(BeElementOf([]string{"3", "3/10"}))
-			}
+				trackNo := m["tracknumber"]
+				Expect(trackNo).To(HaveLen(1))
+				Expect(trackNo[0]).To(BeElementOf([]string{"3", "3/10"}))
+			},
 
-			It("correctly parses aiff tags", func() {
-				// ffmpeg -f lavfi -i "sine=frequency=1400:duration=1" test.aiff
-				testTags("test.aiff", "1.00", "1", "2.00 dB", "0.124972", "2.00 dB", "0.124972")
-			})
+			// ffmpeg -f lavfi -i "sine=frequency=1400:duration=1" test.aiff
+			Entry("correctly parses aiff tags", "test.aiff", "1.00", "1", "2.00 dB", "0.124972", "2.00 dB", "0.124972"),
 
-			It("correctly parses flac tags", func() {
-				// ffmpeg -f lavfi -i "sine=frequency=1200:duration=1" test.flac
-				testTags("test.flac", "1.00", "1", "+4.06 dB", "0.12496948", "+4.06 dB", "0.12496948")
-			})
+			// ffmpeg -f lavfi -i "sine=frequency=1200:duration=1" test.flac
+			Entry("correctly parses flac tags", "test.flac", "1.00", "1", "+4.06 dB", "0.12496948", "+4.06 dB", "0.12496948"),
 
-			It("Correctly parses m4a (aac) gain tags", func() {
-				testTags("01 Invisible (RED) Edit Version.m4a", "1.04", "2", "0.37", "0.48", "0.37", "0.48")
-			})
+			Entry("Correctly parses m4a (aac) gain tags", "01 Invisible (RED) Edit Version.m4a", "1.04", "2", "0.37", "0.48", "0.37", "0.48"),
 
-			It("correctly parses ogg (vorbis) tags", func() {
-				testTags("test.ogg", "1.04", "2", "+7.64 dB", "0.11772506", "+7.64 dB", "0.11772506")
-			})
+			Entry("correctly parses ogg (vorbis) tags", "test.ogg", "1.04", "2", "+7.64 dB", "0.11772506", "+7.64 dB", "0.11772506"),
 
-			It("correctly parses wav tags", func() {
-				// ffmpeg -f lavfi -i "sine=frequency=1000:duration=1" test.wav
-				testTags("test.wav", "1.00", "1", "3.06 dB", "0.125056", "3.06 dB", "0.125056")
-			})
+			// ffmpeg -f lavfi -i "sine=frequency=1000:duration=1" test.wav
+			Entry("correctly parses wav tags", "test.wav", "1.00", "1", "3.06 dB", "0.125056", "3.06 dB", "0.125056"),
 
-			It("correctly parses wma/asf tags", func() {
-				// ffmpeg -f lavfi -i "sine=frequency=900:duration=1" test.wma
-				testTags("test.wma", "1.02", "1", "3.27 dB", "0.132914", "3.27 dB", "0.132914")
-			})
+			// ffmpeg -f lavfi -i "sine=frequency=900:duration=1" test.wma
+			Entry("correctly parses wma/asf tags", "test.wma", "1.02", "1", "3.27 dB", "0.132914", "3.27 dB", "0.132914"),
 
-			It("correctly parses wv (wavpak) tags", func() {
-				// ffmpeg -f lavfi -i "sine=frequency=800:duration=1" test.wv
-				testTags("test.wv", "1.00", "1", "3.43 dB", "0.125061", "3.43 dB", "0.125061")
-			})
-		})
+			// ffmpeg -f lavfi -i "sine=frequency=800:duration=1" test.wv
+			Entry("correctly parses wv (wavpak) tags", "test.wv", "1.00", "1", "3.43 dB", "0.125061", "3.43 dB", "0.125061"),
+		)
 	})
 
 	Context("Error Checking", func() {
