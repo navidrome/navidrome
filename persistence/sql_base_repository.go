28dc98dec46a83d6163c5943d2522015b76969c4
Deluan <deluan@navidrome.org>
2023-11-25 23:08:20 -0500

    Revert "Replace `COUNT(DISTINCT primary_key)` with `COUNT(*)`"
    Genres are required as part of the count queries, so filter by genres work

--- persistence/sql_base_repository.go
+++ persistence/sql_base_repository.go
@@ -181,7 +181,8 @@ func (r sqlRepository) exists(existsQuery SelectBuilder) (bool, error) {
 
 func (r sqlRepository) count(countQuery SelectBuilder, options ...model.QueryOptions) (int64, error) {
 	countQuery = countQuery.
-		RemoveColumns().Columns("count(*) as count").From(r.tableName)
+		RemoveColumns().Columns("count(distinct " + r.tableName + ".id) as count").
+		From(r.tableName)
 	countQuery = r.applyFilters(countQuery, options...)
 	var res struct{ Count int64 }
 	err := r.queryOne(countQuery, &res)
