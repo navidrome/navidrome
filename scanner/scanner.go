d3f6b4692deaa7ff62b90f1c017f93a0911b735f
Deluan <deluan@navidrome.org>
2023-12-15 07:59:34 -0500

    Temporary fix for scan context cancellation for Go 1.20

--- scanner/scanner.go
+++ scanner/scanner.go
@@ -139,28 +139,6 @@ func (s *scanner) startProgressTracker(mediaFolder string) (chan uint32, context
 	return progress, cancel
 }
 
-func (s *scanner) RescanAll(ctx context.Context, fullRescan bool) error {
-	ctx = context.WithoutCancel(ctx)
-	if !isScanning.TryLock() {
-		log.Debug("Scanner already running, ignoring request for rescan.")
-		return ErrAlreadyScanning
-	}
-	defer isScanning.Unlock()
-
-	var hasError bool
-	for folder := range s.folders {
-		err := s.rescan(ctx, folder, fullRescan)
-		hasError = hasError || err != nil
-	}
-	if hasError {
-		log.Error("Errors while scanning media. Please check the logs")
-		core.WriteAfterScanMetrics(ctx, s.ds, false)
-		return ErrScanError
-	}
-	core.WriteAfterScanMetrics(ctx, s.ds, true)
-	return nil
-}
-
 func (s *scanner) getStatus(folder string) (scanStatus, bool) {
 	s.lock.RLock()
 	defer s.lock.RUnlock()
