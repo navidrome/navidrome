b4815ecee58ed6ee3814c6004d00d0f99c4a60f2
Andrew Katsikas <apkatsikas@gmail.com>
2023-12-26 18:39:15 -0500

    Add TAK support (#2745)
    * bug(consts/mime_types): tak-support - 2514
    
    Add tak to mime_types audioFormats
    
    Signed-off-by: apkatsikas <apkatsikas@gmail.com>
    
    * bug(scanner): tak-support - 2514
    
    Add tak test fixture file and add fixes for tag_scanner and walk_dir_tree tests
    
    Signed-off-by: apkatsikas <apkatsikas@gmail.com>
    
    * Remove comment
    
    ---------
    
    Signed-off-by: apkatsikas <apkatsikas@gmail.com>

--- scanner/tag_scanner_test.go
+++ scanner/tag_scanner_test.go
@@ -10,11 +10,12 @@ var _ = Describe("TagScanner", func() {
 		It("return all audio files from the folder", func() {
 			files, err := loadAllAudioFiles("tests/fixtures")
 			Expect(err).ToNot(HaveOccurred())
-			Expect(files).To(HaveLen(11))
+			Expect(files).To(HaveLen(12))
 			Expect(files).To(HaveKey("tests/fixtures/test.aiff"))
 			Expect(files).To(HaveKey("tests/fixtures/test.flac"))
 			Expect(files).To(HaveKey("tests/fixtures/test.m4a"))
 			Expect(files).To(HaveKey("tests/fixtures/test.mp3"))
+			Expect(files).To(HaveKey("tests/fixtures/test.tak"))
 			Expect(files).To(HaveKey("tests/fixtures/test.ogg"))
 			Expect(files).To(HaveKey("tests/fixtures/test.wav"))
 			Expect(files).To(HaveKey("tests/fixtures/test.wma"))
