0d8f8e3afd23b62db1eb85b8704af3032cc55e35
Deluan <deluan@navidrome.org>
2023-12-27 22:12:34 -0500

    Optimize Singleton (sometimes a simple lock is a better solution)

--- utils/singleton/singleton_test.go
+++ utils/singleton/singleton_test.go
@@ -19,15 +19,15 @@ func TestSingleton(t *testing.T) {
 
 var _ = Describe("GetInstance", func() {
 	type T struct{ id string }
-	var numInstances int
+	var numInstancesCreated int
 	constructor := func() *T {
-		numInstances++
+		numInstancesCreated++
 		return &T{id: uuid.NewString()}
 	}
 
 	It("calls the constructor to create a new instance", func() {
 		instance := singleton.GetInstance(constructor)
-		Expect(numInstances).To(Equal(1))
+		Expect(numInstancesCreated).To(Equal(1))
 		Expect(instance).To(BeAssignableToTypeOf(&T{}))
 	})
 
@@ -36,24 +36,24 @@ var _ = Describe("GetInstance", func() {
 		newInstance := singleton.GetInstance(constructor)
 
 		Expect(newInstance.id).To(Equal(instance.id))
-		Expect(numInstances).To(Equal(1))
+		Expect(numInstancesCreated).To(Equal(1))
 	})
 
 	It("makes a distinction between a type and its pointer", func() {
 		instance := singleton.GetInstance(constructor)
 		newInstance := singleton.GetInstance(func() T {
-			numInstances++
+			numInstancesCreated++
 			return T{id: uuid.NewString()}
 		})
 
 		Expect(instance).To(BeAssignableToTypeOf(&T{}))
 		Expect(newInstance).To(BeAssignableToTypeOf(T{}))
 		Expect(newInstance.id).ToNot(Equal(instance.id))
-		Expect(numInstances).To(Equal(2))
+		Expect(numInstancesCreated).To(Equal(2))
 	})
 
 	It("only calls the constructor once when called concurrently", func() {
-		const maxCalls = 8000
+		const maxCalls = 80000
 		var numCalls int32
 		start := sync.WaitGroup{}
 		start.Add(1)
@@ -61,12 +61,12 @@ var _ = Describe("GetInstance", func() {
 		prepare.Add(maxCalls)
 		done := sync.WaitGroup{}
 		done.Add(maxCalls)
-		numInstances = 0
+		numInstancesCreated = 0
 		for i := 0; i < maxCalls; i++ {
 			go func() {
 				start.Wait()
 				singleton.GetInstance(func() struct{ I int } {
-					numInstances++
+					numInstancesCreated++
 					return struct{ I int }{I: 1}
 				})
 				atomic.AddInt32(&numCalls, 1)
@@ -79,6 +79,6 @@ var _ = Describe("GetInstance", func() {
 		done.Wait()
 
 		Expect(numCalls).To(Equal(int32(maxCalls)))
-		Expect(numInstances).To(Equal(1))
+		Expect(numInstancesCreated).To(Equal(1))
 	})
 })
