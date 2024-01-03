a1002d0327d6ed135c61e3d8cff8639fd08d0b2f
Jack Kawell <jgkawell@outlook.com>
2024-01-03 17:33:41 -0600

    added missing driver

--- persistence/persistence_suite_test.go
+++ persistence/persistence_suite_test.go
@@ -23,6 +23,7 @@ func TestPersistence(t *testing.T) {
 	//os.Remove("./test-123.db")
 	//conf.Server.DbPath = "./test-123.db"
 	conf.Server.DbPath = "file::memory:?cache=shared"
+	conf.Server.DbDriver = "sqlite3"
 	db.Init()
 	log.SetLevel(log.LevelError)
 	RegisterFailHandler(Fail)
