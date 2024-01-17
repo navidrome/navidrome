c547c9fac5741d0a762bd6514ee47d1a50594cc5
caiocotts <caio@cotts.com.br>
2024-01-17 16:46:22 -0500

    Use the default import path for jest-dom.

--- ui/src/common/Linkify.test.js
+++ ui/src/common/Linkify.test.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { render, screen } from '@testing-library/react'
-import '@testing-library/jest-dom/extend-expect'
+import '@testing-library/jest-dom'
 import Linkify from './Linkify'
 
 const URL = 'http://www.example.com'
