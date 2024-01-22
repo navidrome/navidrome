50a18bad5a355f8dfc717511b303312753fe9c82
caiocotts <caio@cotts.com.br>
2024-01-22 17:34:33 -0500

    Fix album image_files being null.

---
+++ db/migration/20240122223340_fix_null_image_files.go
@@ -0,0 +1,28 @@
+package migrations
+
+import (
+	"context"
+	"database/sql"
+
+	"github.com/pressly/goose/v3"
+)
+
+func init() {
+	goose.AddMigrationContext(Up20240122223340, Down20240122223340)
+}
+
+func Up20240122223340(ctx context.Context, tx *sql.Tx) error {
+	// This code is executed when the migration is applied.
+	_, err := tx.Exec(`
+	alter table album add image_files_new varchar not null default '';
+	update album set image_files_new = image_files where image_files is not null;
+	alter table album drop image_files;
+	alter table album rename image_files_new to image_files;
+`)
+	return err
+}
+
+func Down20240122223340(ctx context.Context, tx *sql.Tx) error {
+	// This code is executed when the migration is rolled back.
+	return nil
+}
