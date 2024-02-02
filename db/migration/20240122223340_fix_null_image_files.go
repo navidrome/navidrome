265ea9c0f52f99ecf113048c4ddde6ce6d02bf50
Caio Cotts <caio@cotts.com.br>
2024-02-02 15:50:04 -0500

    Change more columns to not null and set default values.

--- db/migration/20240122223340_fix_null_image_files.go
+++ db/migration/20240122223340_fix_null_image_files.go
@@ -13,10 +13,579 @@ func init() {
 
 func Up20240122223340(ctx context.Context, tx *sql.Tx) error {
 	_, err := tx.ExecContext(ctx, `
-	alter table album add image_files_new varchar not null default '';
-	update album set image_files_new = image_files where image_files is not null;
-	alter table album drop image_files;
-	alter table album rename image_files_new to image_files;
+drop index if exists album_full_text;
+drop index if exists album_alphabetical_by_artist;
+drop index if exists album_order_album_name;
+drop index if exists album_order_album_artist_name;
+
+drop index if exists artist_full_text;
+drop index if exists artist_order_artist_name;
+
+drop index if exists media_file_full_text;
+drop index if exists media_file_order_album_name;
+drop index if exists media_file_order_artist_name;
+drop index if exists media_file_order_title;
+drop index if exists media_file_bpm;
+drop index if exists media_file_channels;
+
+drop index if exists playlist_evaluated_at;
+
+alter table album
+    add image_files_new varchar not null default '';
+update album
+set image_files_new = image_files
+where image_files is not null;
+alter table album
+    drop image_files;
+alter table album
+    rename image_files_new to image_files;
+
+alter table album
+    add full_text_new varchar not null default '';
+update album
+set full_text_new = full_text
+where full_text is not null;
+alter table album
+    drop full_text;
+alter table album
+    rename full_text_new to full_text;
+
+alter table album
+    add order_album_name_new varchar not null default '';
+update album
+set order_album_name_new = order_album_name
+where order_album_name is not null;
+alter table album
+    drop order_album_name;
+alter table album
+    rename order_album_name_new to order_album_name;
+
+alter table album
+    add order_album_artist_name_new varchar not null default '';
+update album
+set order_album_artist_name_new = order_album_artist_name
+where order_album_artist_name is not null;
+alter table album
+    drop order_album_artist_name;
+alter table album
+    rename order_album_artist_name_new to order_album_artist_name;
+
+alter table album
+    add sort_album_name_new varchar not null default '';
+update album
+set sort_album_name_new = sort_album_name
+where sort_album_name is not null;
+alter table album
+    drop sort_album_name;
+alter table album
+    rename sort_album_name_new to sort_album_name;
+
+alter table album
+    add sort_artist_name_new varchar not null default '';
+update album
+set sort_artist_name_new = sort_artist_name
+where sort_artist_name is not null;
+alter table album
+    drop sort_artist_name;
+alter table album
+    rename sort_artist_name_new to sort_artist_name;
+
+alter table album
+    add sort_album_artist_name_new varchar not null default '';
+update album
+set sort_album_artist_name_new = sort_album_artist_name
+where sort_album_artist_name is not null;
+alter table album
+    drop sort_album_artist_name;
+alter table album
+    rename sort_album_artist_name_new to sort_album_artist_name;
+
+alter table album
+    add catalog_num_new varchar not null default '';
+update album
+set catalog_num_new = catalog_num
+where catalog_num is not null;
+alter table album
+    drop catalog_num;
+alter table album
+    rename catalog_num_new to catalog_num;
+
+alter table album
+    add comment_new varchar not null default '';
+update album
+set comment_new = comment
+where comment is not null;
+alter table album
+    drop comment;
+alter table album
+    rename comment_new to comment;
+
+alter table album
+    add image_files_new varchar not null default '';
+update album
+set image_files_new = image_files
+where image_files is not null;
+alter table album
+    drop image_files;
+alter table album
+    rename image_files_new to image_files;
+
+alter table album
+    add paths_new varchar not null default '';
+update album
+set paths_new = paths
+where paths is not null;
+alter table album
+    drop paths;
+alter table album
+    rename paths_new to paths;
+
+alter table album
+    add external_info_updated_at_new datetime not null default '0000-00-00 00:00:00';
+update album
+set external_info_updated_at_new = external_info_updated_at
+where external_info_updated_at is not null;
+alter table album
+    drop external_info_updated_at;
+alter table album
+    rename external_info_updated_at_new to external_info_updated_at;
+
+alter table album
+    add discs_new jsonb not null default '{}';
+update album
+set discs_new = discs
+where discs is not null;
+alter table album
+    drop discs;
+alter table album
+    rename discs_new to discs;
+
+--  ARTIST
+alter table artist
+    add full_text_new varchar not null default '';
+update artist
+set full_text_new = full_text
+where full_text is not null;
+alter table artist
+    drop full_text;
+alter table artist
+    rename full_text_new to full_text;
+
+alter table artist
+    add order_artist_name_new varchar not null default '';
+update artist
+set order_artist_name_new = order_artist_name
+where order_artist_name is not null;
+alter table artist
+    drop order_artist_name;
+alter table artist
+    rename order_artist_name_new to order_artist_name;
+
+alter table artist
+    add sort_artist_name_new varchar not null default '';
+update artist
+set sort_artist_name_new = sort_artist_name
+where sort_artist_name is not null;
+alter table artist
+    drop sort_artist_name;
+alter table artist
+    rename sort_artist_name_new to sort_artist_name;
+
+alter table artist
+    add external_info_updated_at_new datetime not null default '0000-00-00 00:00:00';
+update artist
+set external_info_updated_at_new = external_info_updated_at
+where external_info_updated_at is not null;
+alter table artist
+    drop external_info_updated_at;
+alter table artist
+    rename external_info_updated_at_new to external_info_updated_at;
+
+--  MEDIA_FILE
+alter table media_file
+    add full_text_new varchar not null default '';
+update media_file
+set full_text_new = full_text
+where full_text is not null;
+alter table media_file
+    drop full_text;
+alter table media_file
+    rename full_text_new to full_text;
+
+alter table media_file
+    add order_album_name_new varchar not null default '';
+update media_file
+set order_album_name_new = order_album_name
+where order_album_name is not null;
+alter table media_file
+    drop order_album_name;
+alter table media_file
+    rename order_album_name_new to order_album_name;
+
+alter table media_file
+    add order_album_artist_name_new varchar not null default '';
+update media_file
+set order_album_artist_name_new = order_album_artist_name
+where order_album_artist_name is not null;
+alter table media_file
+    drop order_album_artist_name;
+alter table media_file
+    rename order_album_artist_name_new to order_album_artist_name;
+
+alter table media_file
+    add order_artist_name_new varchar not null default '';
+update media_file
+set order_artist_name_new = order_artist_name
+where order_artist_name is not null;
+alter table media_file
+    drop order_artist_name;
+alter table media_file
+    rename order_artist_name_new to order_artist_name;
+
+alter table media_file
+    add sort_album_name_new varchar not null default '';
+update media_file
+set sort_album_name_new = sort_album_name
+where sort_album_name is not null;
+alter table media_file
+    drop sort_album_name;
+alter table media_file
+    rename sort_album_name_new to sort_album_name;
+
+alter table media_file
+    add sort_artist_name_new varchar not null default '';
+update media_file
+set sort_artist_name_new = sort_artist_name
+where sort_artist_name is not null;
+alter table media_file
+    drop sort_artist_name;
+alter table media_file
+    rename sort_artist_name_new to sort_artist_name;
+
+alter table media_file
+    add sort_album_artist_name_new varchar not null default '';
+update media_file
+set sort_album_artist_name_new = sort_album_artist_name
+where sort_album_artist_name is not null;
+alter table media_file
+    drop sort_album_artist_name;
+alter table media_file
+    rename sort_album_artist_name_new to sort_album_artist_name;
+
+alter table media_file
+    add sort_title_new varchar not null default '';
+update media_file
+set sort_title_new = sort_title
+where sort_title is not null;
+alter table media_file
+    drop sort_title;
+alter table media_file
+    rename sort_title_new to sort_title;
+
+alter table media_file
+    add disc_subtitle_new varchar not null default '';
+update media_file
+set disc_subtitle_new = disc_subtitle
+where disc_subtitle is not null;
+alter table media_file
+    drop disc_subtitle;
+alter table media_file
+    rename disc_subtitle_new to disc_subtitle;
+
+alter table media_file
+    add catalog_num_new varchar not null default '';
+update media_file
+set catalog_num_new = catalog_num
+where catalog_num is not null;
+alter table media_file
+    drop catalog_num;
+alter table media_file
+    rename catalog_num_new to catalog_num;
+
+alter table media_file
+    add comment_new varchar not null default '';
+update media_file
+set comment_new = comment
+where comment is not null;
+alter table media_file
+    drop comment;
+alter table media_file
+    rename comment_new to comment;
+
+alter table media_file
+    add order_title_new varchar not null default '';
+update media_file
+set order_title_new = order_title
+where order_title is not null;
+alter table media_file
+    drop order_title;
+alter table media_file
+    rename order_title_new to order_title;
+
+alter table media_file
+    add bpm_new integer not null default 0;
+update media_file
+set bpm_new = bpm
+where bpm is not null;
+alter table media_file
+    drop bpm;
+alter table media_file
+    rename bpm_new to bpm;
+
+alter table media_file
+    add bpm_new integer not null default 0;
+update media_file
+set bpm_new = bpm
+where bpm is not null;
+alter table media_file
+    drop bpm;
+alter table media_file
+    rename bpm_new to bpm;
+
+alter table media_file
+    add channels_new integer not null default 0;
+update media_file
+set channels_new = channels
+where channels is not null;
+alter table media_file
+    drop channels;
+alter table media_file
+    rename channels_new to channels;
+
+alter table media_file
+    add rg_album_gain_new real not null default 0;
+update media_file
+set rg_album_gain_new = rg_album_gain
+where rg_album_gain is not null;
+alter table media_file
+    drop rg_album_gain;
+alter table media_file
+    rename rg_album_gain_new to rg_album_gain;
+
+alter table media_file
+    add rg_album_peak_new real not null default 0;
+update media_file
+set rg_album_peak_new = rg_album_peak
+where rg_album_peak is not null;
+alter table media_file
+    drop rg_album_peak;
+alter table media_file
+    rename rg_album_peak_new to rg_album_peak;
+
+alter table media_file
+    add rg_track_gain_new real not null default 0;
+update media_file
+set rg_track_gain_new = rg_track_gain
+where rg_track_gain is not null;
+alter table media_file
+    drop rg_track_gain;
+alter table media_file
+    rename rg_track_gain_new to rg_album_again;
+
+alter table media_file
+    add rg_track_peak_new real not null default 0;
+update media_file
+set rg_track_peak_new = rg_track_peak
+where rg_track_peak is not null;
+alter table media_file
+    drop rg_track_peak;
+alter table media_file
+    rename rg_track_peak_new to rg_track_peak;
+
+alter table media_file
+    add lyrics_new jsonb not null default '[]';
+update media_file
+set lyrics_new = lyrics
+where lyrics is not null;
+alter table media_file
+    drop lyrics;
+alter table media_file
+    rename lyrics_new to lyrics;
+
+-- RADIO
+alter table radio
+    add stream_url_new varchar not null default '';
+update radio
+set stream_url_new = stream_url
+where stream_url is not null;
+alter table radio
+    drop stream_url;
+alter table radio
+    rename stream_url_new to stream_url;
+
+alter table radio
+    add home_page_url_new varchar not null default '';
+update radio
+set home_page_url_new = home_page_url
+where home_page_url is not null;
+alter table radio
+    drop home_page_url;
+alter table radio
+    rename home_page_url_new to home_page_url;
+
+-- SHARE
+alter table share
+    add description_new varchar not null default '';
+update share
+set description_new = description
+where description is not null;
+alter table share
+    drop description;
+alter table share
+    rename description_new to description;
+
+alter table share
+    add resource_type_new varchar not null default '';
+update share
+set resource_type_new = resource_type
+where resource_type is not null;
+alter table share
+    drop resource_type;
+alter table share
+    rename resource_type_new to resource_type;
+
+alter table share
+    add contents_new varchar not null default '';
+update share
+set contents_new = contents
+where contents is not null;
+alter table share
+    drop contents;
+alter table share
+    rename contents_new to contents;
+
+alter table share
+    add format_new varchar not null default '';
+update share
+set format_new = format
+where format is not null;
+alter table share
+    drop format;
+alter table share
+    rename format_new to format;
+
+alter table share
+    add expires_at_new datetime not null default '0000-00-00 00:00:00';
+update share
+set expires_at_new = expires_at
+where expires_at is not null;
+alter table share
+    drop expires_at;
+alter table share
+    rename expires_at_new to expires_at;
+
+alter table share
+    add last_visited_at_new datetime not null default '0000-00-00 00:00:00';
+update share
+set last_visited_at_new = last_visited_at
+where last_visited_at is not null;
+alter table share
+    drop last_visited_at;
+alter table share
+    rename last_visited_at_new to last_visited_at;
+
+alter table share
+    add max_bit_rate_new integer not null default 0;
+update share
+set max_bit_rate_new = max_bit_rate
+where max_bit_rate is not null;
+alter table share
+    drop max_bit_rate;
+alter table share
+    rename max_bit_rate_new to max_bit_rate;
+
+alter table share
+    add visit_count_new integer not null default 0;
+update share
+set visit_count_new = visit_count
+where visit_count is not null;
+alter table share
+    drop visit_count;
+alter table share
+    rename visit_count_new to visit_count;
+
+-- PLAYLIST
+alter table playlist
+    add rules_new varchar not null default '';
+update playlist
+set rules_new = rules
+where rules is not null;
+alter table playlist
+    drop rules;
+alter table playlist
+    rename rules_new to rules;
+
+alter table playlist
+    add evaluated_at_new datetime not null default '0000-00-00 00:00:00';
+update playlist
+set evaluated_at_new = evaluated_at
+where evaluated_at is not null;
+alter table playlist
+    drop evaluated_at;
+alter table playlist
+    rename evaluated_at_new to evaluated_at;
+
+-- INDEX
+select full_text,
+       compilation,
+       order_album_artist_name,
+       order_album_name
+from album;
+
+create index album_full_text
+    on album (full_text);
+
+create index album_alphabetical_by_artist
+    on album (compilation, order_album_artist_name, order_album_name);
+
+create index album_order_album_name
+    on album (order_album_name);
+
+create index album_order_album_artist_name
+    on album (order_album_artist_name);
+
+select full_text,
+       order_artist_name
+from artist;
+
+create index artist_full_text
+    on artist (full_text);
+
+create index artist_order_artist_name
+    on artist (order_artist_name);
+
+select full_text,
+       order_album_name,
+       order_artist_name,
+       order_title,
+       bpm,
+       channels
+from media_file;
+
+create index media_file_full_text
+    on media_file (full_text);
+
+create index media_file_order_album_name
+    on media_file (order_album_name);
+
+create index media_file_order_artist_name
+    on media_file (order_artist_name);
+
+create index media_file_order_title
+    on media_file (order_title);
+
+create index media_file_bpm
+    on media_file (bpm);
+
+create index media_file_channels
+    on media_file (channels);
+
+select evaluated_at
+from playlist;
+
+create index playlist_evaluated_at
+    on playlist (evaluated_at)
+ 	 	
 `)
 	return err
 }
