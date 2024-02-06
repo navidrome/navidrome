1d17831e1be6043937101cc25039f504772db2d4
Deluan <deluan@navidrome.org>
2024-02-05 19:37:49 -0500

    Make ExternalInfoUpdatedAt nullable

--- utils/gg/gg.go
+++ utils/gg/gg.go
@@ -30,3 +30,17 @@ func FirstOr[T comparable](or T, values ...T) T {
 	// If all the input values are zero, return the default value.
 	return or
 }
+
+// P returns a pointer to the input value
+func P[T any](v T) *T {
+	return &v
+}
+
+// V returns the value of the input pointer, or a zero value if the input pointer is nil.
+func V[T any](p *T) T {
+	if p == nil {
+		var zero T
+		return zero
+	}
+	return *p
+}
