4294d9148032429c70f3c7a213eb2c9343f3ae4b
bornav <borna.vincek1@gmail.com>
2024-02-24 11:51:50 +0100

    Implemented web handeling of queue sync - #245 Added setting in user pannel for toggeling sync
    Addied temporary solution for web sync trigger.(on refresh)

--- ui/src/store/persistState.js
+++ ui/src/store/persistState.js
@@ -1,9 +1,13 @@
+import { GetTime } from '../common/UpdateQueueButton'
+
 export const loadState = () => {
   try {
     const serializedState = localStorage.getItem('state')
     if (serializedState === null) {
+      GetTime(undefined)
       return undefined
     }
+    GetTime(JSON.parse(serializedState))
     return JSON.parse(serializedState)
   } catch (err) {
     return undefined
