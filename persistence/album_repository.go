3ab66ae8ef3b93ae832c665e74b16bab23d5b18e
Deluan <deluan@navidrome.org>
2024-04-20 13:35:13 -0400

    Use constants for AlbumPlayCountMode values

--- persistence/album_repository.go
+++ persistence/album_repository.go
@@ -9,6 +9,7 @@ import (
 	. "github.com/Masterminds/squirrel"
 	"github.com/deluan/rest"
 	"github.com/navidrome/navidrome/conf"
+	"github.com/navidrome/navidrome/consts"
 	"github.com/navidrome/navidrome/log"
 	"github.com/navidrome/navidrome/model"
 	"github.com/pocketbase/dbx"
@@ -172,7 +173,7 @@ func (r *albumRepository) GetAll(options ...model.QueryOptions) (model.Albums, e
 func (r *albumRepository) toModels(dba []dbAlbum) model.Albums {
 	res := model.Albums{}
 	for i := range dba {
-		if conf.Server.AlbumPlaycountMode == "normalized" && dba[i].Album.SongCount != 0 {
+		if conf.Server.AlbumPlayCountMode == consts.AlbumPlayCountModeNormalized && dba[i].Album.SongCount != 0 {
 			dba[i].Album.PlayCount = (dba[i].Album.PlayCount + (int64(dba[i].Album.SongCount) - 1)) / int64(dba[i].Album.SongCount)
 		}
 		res = append(res, *dba[i].Album)
