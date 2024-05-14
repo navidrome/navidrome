d02e4fd4e61f4083e5cb8ae430a2b6deab3f60b6
Deluan <deluan@navidrome.org>
2024-05-14 19:00:11 -0400

    New tags (wip)
    # Conflicts:
    #	go.sum

---
+++ model/tag/tag_suite_test.go
@@ -0,0 +1,18 @@
+package tag_test
+
+import (
+	"testing"
+
+	_ "github.com/mattn/go-sqlite3"
+	"github.com/navidrome/navidrome/log"
+	"github.com/navidrome/navidrome/tests"
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+)
+
+func TestTag(t *testing.T) {
+	tests.Init(t, true)
+	log.SetLevel(log.LevelFatal)
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "Values Suite")
+}
---
+++ model/tag/tags.go
@@ -0,0 +1,86 @@
+package tag
+
+import (
+	"strings"
+	"sync"
+
+	"github.com/navidrome/navidrome/log"
+	"github.com/navidrome/navidrome/resources"
+	"gopkg.in/yaml.v3"
+)
+
+type Name string
+
+const (
+	Album         Name = "album"
+	Title         Name = "title"
+	TrackNumber   Name = "track"
+	DiscNumber    Name = "disc"
+	TotalTracks   Name = "tracktotal"
+	TotalDiscs    Name = "disctotal"
+	Genre         Name = "genre"
+	Comment       Name = "comment"
+	AlbumSort     Name = "albumsort"
+	TitleSort     Name = "titlesort"
+	Compilation   Name = "compilation"
+	Grouping      Name = "grouping"
+	Lyrics        Name = "lyrics"
+	CatalogNumber Name = "catalognumber"
+	BPM           Name = "bpm"
+
+	// Dates and years
+
+	OriginalYear Name = "originalyear"
+	OriginalDate Name = "originaldate"
+	ReleaseDate  Name = "releasedate"
+
+	// Artists and roles
+
+	AlbumArtist     Name = "albumartist"
+	AlbumArtistSort Name = "albumartistsort"
+	TrackArtist     Name = "artist"
+	TrackArtistSort Name = "artistsort"
+	Composer        Name = "composer"
+	ComposerSort    Name = "composersort"
+	Performer       Name = "performer"
+	Director        Name = "director"
+	Conductor       Name = "conductor"
+	Arranger        Name = "arranger"
+	Lyricist        Name = "lyricist"
+
+	// ReplayGain
+
+	ReplayGainAlbumGain Name = "replaygain_album_gain"
+	ReplayGainAlbumPeak Name = "replaygain_album_peak"
+	ReplayGainTrackGain Name = "replaygain_track_gain"
+	ReplayGainTrackPeak Name = "replaygain_track_peak"
+
+	// MusicBrainz
+
+	MusicBrainzArtistID      Name = "musicbrainz_artistid"
+	MusicBrainzDiscID        Name = "musicbrainz_discid"
+	MusicBrainzRecordingID   Name = "musicbrainz_recordingid"
+	MusicBrainzAlbumArtistID Name = "musicbrainz_albumartistid"
+	MusicBrainzAlbumID       Name = "musicbrainz_albumid"
+)
+
+var mappings = sync.OnceValue(func() map[string][]string {
+	mappingsFile, err := resources.FS().Open("mappings.yaml")
+	if err != nil {
+		log.Error("Error opening mappings.yaml", err)
+	}
+	decoder := yaml.NewDecoder(mappingsFile)
+	var mappings map[string][]string
+	err = decoder.Decode(&mappings)
+	if err != nil {
+		log.Error("Error decoding mappings.yaml", err)
+	}
+	normalized := map[string][]string{}
+	for k, v := range mappings {
+		k = strings.ToLower(k)
+		for _, val := range v {
+			normalized[k] = append(normalized[k], strings.ToLower(val))
+		}
+	}
+	return normalized
+})
