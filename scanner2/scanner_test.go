880ab2520799596fa259de3072a36bf280eac8c2
Deluan <deluan@navidrome.org>
2024-05-16 22:55:31 -0400

    WIP

---
+++ scanner2/scanner_test.go
@@ -0,0 +1,56 @@
+package scanner2
+
+import (
+	"context"
+	"testing/fstest"
+
+	"github.com/navidrome/navidrome/log"
+	"github.com/navidrome/navidrome/model"
+	"github.com/navidrome/navidrome/scanner"
+	"github.com/navidrome/navidrome/tests"
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("Scanner", func() {
+	var fs FakeFS
+	var files fstest.MapFS
+	var ctx context.Context
+	var libRepo tests.MockLibraryRepo
+	var ds tests.MockDataStore
+	var s scanner.Scanner
+	var lib model.Library
+
+	BeforeEach(func() {
+		log.SetLevel(log.LevelTrace)
+		ctx = context.Background()
+		files = fstest.MapFS{}
+		libRepo = tests.MockLibraryRepo{}
+		ds.MockedLibrary = &libRepo
+		s = GetInstance(ctx, &ds)
+		RegisterFakeExtractor(fs)
+	})
+
+	JustBeforeEach(func() {
+		libRepo.SetData(model.Libraries{lib})
+		fs = FakeFS{MapFS: files}
+	})
+
+	Describe("Scan", func() {
+		BeforeEach(func() {
+			lib = model.Library{Name: "The Beatles", Path: "/music/The%20Beatles"}
+			sgtPeppers := template(_tag{"albumartist": "The Beatles", "album": "Sgt. Pepper's Lonely Hearts Club Band", "year": 1967})
+			files = fstest.MapFS{
+				"The Beatles/1967 - Sgt. Pepper's Lonely Hearts Club Band/01 - Sgt. Pepper's Lonely Hearts Club Band.mp3": sgtPeppers(track(1, "Sgt. Pepper's Lonely Hearts Club Band")),
+				"The Beatles/1967 - Sgt. Pepper's Lonely Hearts Club Band/02 - With a Little Help from My Friends.mp3":    sgtPeppers(track(2, "With a Little Help from My Friends")),
+				"The Beatles/1967 - Sgt. Pepper's Lonely Hearts Club Band/03 - Lucy in the Sky with Diamonds.mp3":         sgtPeppers(track(3, "Lucy in the Sky with Diamonds")),
+				"The Beatles/1967 - Sgt. Pepper's Lonely Hearts Club Band/04 - Getting Better.mp3":                        sgtPeppers(track(4, "Getting Better")),
+			}
+		})
+
+		It("should scan all files", func() {
+			err := s.RescanAll(context.Background(), true)
+			Expect(err).ToNot(HaveOccurred())
+		})
+	})
+})
