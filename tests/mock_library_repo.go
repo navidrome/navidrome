c37d1600eec6a36a3920cc4c5bdf73a4b1b3d4dd
Deluan <deluan@navidrome.org>
2024-05-16 22:55:31 -0400

    WIP

---
+++ tests/mock_library_repo.go
@@ -0,0 +1,28 @@
+package tests
+
+import (
+	"github.com/navidrome/navidrome/model"
+	"golang.org/x/exp/maps"
+)
+
+type MockLibraryRepo struct {
+	model.LibraryRepository
+	data map[int]model.Library
+	Err  error
+}
+
+func (m *MockLibraryRepo) SetData(data model.Libraries) {
+	m.data = make(map[int]model.Library)
+	for _, d := range data {
+		m.data[d.ID] = d
+	}
+}
+
+func (m *MockLibraryRepo) GetAll(...model.QueryOptions) (model.Libraries, error) {
+	if m.Err != nil {
+		return nil, m.Err
+	}
+	return maps.Values(m.data), nil
+}
+
+var _ model.LibraryRepository = &MockLibraryRepo{}
