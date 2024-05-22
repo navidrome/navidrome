5bbcd71f2a4ccf9b35e364b25b45d257b9f56f4d
Deluan <deluan@navidrome.org>
2024-05-22 07:57:54 -0400

    WIP - Import Albums

--- db/migrations/20240520141836_add_folders_and_tags.go
+++ db/migrations/20240520141836_add_folders_and_tags.go
@@ -32,6 +32,8 @@ alter table media_file
     add column pid varchar default id not null;
 alter table media_file 
     add column album_pid varchar default album_id not null;
+alter table media_file
+	add column available boolean default true not null;
 
 create index if not exists media_file_folder_id_ix
  	on media_file (folder_id);
@@ -39,6 +41,16 @@ create unique index if not exists media_file_pid_ix
 	on media_file (pid);
 create index if not exists media_file_album_pid_ix
 	on media_file (album_pid);
+
+alter table album
+	add column folder_id varchar default "" not null;
+alter table album
+	add column pid varchar default id not null;
+
+create index if not exists album_folder_id_ix
+	on album (folder_id);
+create unique index if not exists album_pid_ix
+	on album (pid);
 
 -- FIXME Needs to process current media_file.paths, creating folders as needed
 
--- scanner2/persist_changes.go
+++ scanner2/persist_changes.go
@@ -12,11 +12,27 @@ import (
 func persistChanges(ctx context.Context) pipeline.StageFn[*folderEntry] {
 	return func(entry *folderEntry) (*folderEntry, error) {
 		err := entry.job.ds.WithTx(func(tx model.DataStore) error {
+			// Save folder to DB
+			err := tx.Folder(ctx).Put(entry.job.lib, entry.path)
+			if err != nil {
+				log.Error(ctx, "Scanner: Error persisting folder to DB", "folder", entry.path, err)
+				return err
+			}
+
+			// Save all albums to DB
+			for i := range entry.albums {
+				err = tx.Album(ctx).Put(&entry.albums[i])
+				if err != nil {
+					log.Error(ctx, "Scanner: Error persisting album to DB", "folder", entry.path, "album", entry.albums[i], err)
+					return err
+				}
+			}
+
 			// Save all tags to DB
-			err := slice.RangeByChunks(entry.tags, 100, func(chunk []model.Tag) error {
+			err = slice.RangeByChunks(entry.tags, 100, func(chunk []model.Tag) error {
 				err := tx.Tag(ctx).Add(chunk...)
 				if err != nil {
-					log.Error(ctx, "Scanner: Error adding tags to DB", "folder", entry.path, err)
+					log.Error(ctx, "Scanner: Error persisting tags to DB", "folder", entry.path, err)
 					return err
 				}
 				return nil
@@ -26,28 +42,14 @@ func persistChanges(ctx context.Context) pipeline.StageFn[*folderEntry] {
 			}
 
 			// Save all tracks to DB
-			err = slice.RangeByChunks(entry.tracks, 100, func(chunk []model.MediaFile) error {
-				for i := range chunk {
-					track := chunk[i]
-					err = tx.MediaFile(ctx).Put(&track)
-					if err != nil {
-						log.Error(ctx, "Scanner: Error adding/updating mediafile to DB", "folder", entry.path, "track", track, err)
-						return err
-					}
+			for i := range entry.tracks {
+				err = tx.MediaFile(ctx).Put(&entry.tracks[i])
+				if err != nil {
+					log.Error(ctx, "Scanner: Error persisting mediafile to DB", "folder", entry.path, "track", entry.tracks[i], err)
+					return err
 				}
-				return nil
-			})
-			if err != nil {
-				return err
 			}
-
-			// Save folder to DB
-			err = tx.Folder(ctx).Put(entry.job.lib, entry.path)
-			if err != nil {
-				log.Error(ctx, "Scanner: Error adding/updating folder to DB", "folder", entry.path, err)
-				return err
-			}
-			return err
+			return nil
 		})
 		if err != nil {
 			log.Error(ctx, "Scanner: Error persisting changes to DB", "folder", entry.path, err)
