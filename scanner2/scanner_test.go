1dba78457966423d98683e119f97a14875cfd3dc
Deluan <deluan@navidrome.org>
2024-05-22 07:57:54 -0400

    WIP - Import Albums

--- scanner2/scanner_test.go
+++ scanner2/scanner_test.go
@@ -5,8 +5,8 @@ import (
 	"testing/fstest"
 
 	"github.com/navidrome/navidrome/conf"
+	"github.com/navidrome/navidrome/core/storage/storagetest"
 	"github.com/navidrome/navidrome/db"
-	"github.com/navidrome/navidrome/log"
 	"github.com/navidrome/navidrome/model"
 	"github.com/navidrome/navidrome/persistence"
 	"github.com/navidrome/navidrome/scanner"
@@ -16,8 +16,14 @@ import (
 	. "github.com/onsi/gomega"
 )
 
+// Easy aliases for the storagetest package
+type _t = map[string]any
+
+var template = storagetest.Template
+var track = storagetest.Track
+
 var _ = Describe("Scanner", func() {
-	var fs *FakeFS
+	var fs storagetest.FakeFS
 	var files fstest.MapFS
 	var ctx context.Context
 	var libRepo model.LibraryRepository
@@ -26,16 +32,18 @@ var _ = Describe("Scanner", func() {
 	var lib model.Library
 
 	BeforeEach(func() {
-		log.SetLevel(log.LevelTrace)
+		ctx = context.Background()
+
+		//log.SetLevel(log.LevelTrace)
 		//os.Remove("./test-123.db")
 		//conf.Server.DbPath = "./test-123.db"
 		conf.Server.DbPath = "file::memory:?cache=shared"
 		db.Init()
-
-		ctx = context.Background()
 		ds = persistence.New(db.Db())
+
 		files = fstest.MapFS{}
 		s = scanner2.GetInstance(ctx, ds)
+		storagetest.Register(&fs)
 	})
 
 	AfterEach(func() {
@@ -55,46 +63,64 @@ var _ = Describe("Scanner", func() {
 		libRepo = ds.Library(ctx)
 		Expect(libRepo.Put(&lib)).To(Succeed())
 
-		fs = &FakeFS{MapFS: files}
-		RegisterFakeStorage(fs)
+		fs.MapFS = files
 	})
 
 	Describe("Scanner", func() {
-		BeforeEach(func() {
-			revolver := template(_t{"albumartist": "The Beatles", "album": "Revolver", "year": 1966})
-			help := template(_t{"albumartist": "The Beatles", "album": "Help!", "year": 1965})
-			files = fstest.MapFS{
-				"The Beatles/Revolver/01 - Taxman.mp3":                         revolver(track(1, "Taxman")),
-				"The Beatles/Revolver/02 - Eleanor Rigby.mp3":                  revolver(track(2, "Eleanor Rigby")),
-				"The Beatles/Revolver/03 - I'm Only Sleeping.mp3":              revolver(track(3, "I'm Only Sleeping")),
-				"The Beatles/Help!/01 - Help!.mp3":                             help(track(1, "Help!")),
-				"The Beatles/Help!/02 - The Night Before.mp3":                  help(track(2, "The Night Before")),
-				"The Beatles/Help!/03 - You've Got to Hide Your Love Away.mp3": help(track(3, "You've Got to Hide Your Love Away")),
-			}
-		})
+		Context("Simple library, 'artis/album/track - title.mp3'", func() {
+			BeforeEach(func() {
+				revolver := template(_t{"albumartist": "The Beatles", "album": "Revolver", "year": 1966})
+				help := template(_t{"albumartist": "The Beatles", "album": "Help!", "year": 1965})
+				files = fstest.MapFS{
+					"The Beatles/Revolver/01 - Taxman.mp3":                         revolver(track(1, "Taxman")),
+					"The Beatles/Revolver/02 - Eleanor Rigby.mp3":                  revolver(track(2, "Eleanor Rigby")),
+					"The Beatles/Revolver/03 - I'm Only Sleeping.mp3":              revolver(track(3, "I'm Only Sleeping")),
+					"The Beatles/Revolver/04 - Love You To.mp3":                    revolver(track(4, "Love You To")),
+					"The Beatles/Help!/01 - Help!.mp3":                             help(track(1, "Help!")),
+					"The Beatles/Help!/02 - The Night Before.mp3":                  help(track(2, "The Night Before")),
+					"The Beatles/Help!/03 - You've Got to Hide Your Love Away.mp3": help(track(3, "You've Got to Hide Your Love Away")),
+				}
+			})
+			When("First Scan", func() {
+				It("should import all folders", func() {
+					Expect(s.RescanAll(ctx, true)).To(Succeed())
 
-		It("should import all folders", func() {
-			Expect(s.RescanAll(ctx, true)).To(Succeed())
+					folders, _ := ds.Folder(ctx).GetAll(lib)
+					paths := slice.Map(folders, func(f model.Folder) string { return f.Name })
+					Expect(paths).To(SatisfyAll(
+						HaveLen(4),
+						ContainElements(".", "The Beatles", "Revolver", "Help!"),
+					))
+				})
+				It("should import all mediafiles", func() {
+					Expect(s.RescanAll(ctx, true)).To(Succeed())
 
-			folders, _ := ds.Folder(ctx).GetAll(lib)
-			paths := slice.Map(folders, func(f model.Folder) string { return f.Name })
-			Expect(paths).To(SatisfyAll(
-				HaveLen(4),
-				ContainElements(".", "The Beatles", "Revolver", "Help!"),
-			))
-		})
-		It("should import all mediafiles", func() {
-			Expect(s.RescanAll(ctx, true)).To(Succeed())
+					mfs, _ := ds.MediaFile(ctx).GetAll()
+					paths := slice.Map(mfs, func(f model.MediaFile) string { return f.Title })
+					Expect(paths).To(SatisfyAll(
+						HaveLen(7),
+						ContainElements(
+							"Taxman", "Eleanor Rigby", "I'm Only Sleeping", "Love You To",
+							"Help!", "The Night Before", "You've Got to Hide Your Love Away",
+						),
+					))
+				})
 
-			mfs, _ := ds.MediaFile(ctx).GetAll()
-			paths := slice.Map(mfs, func(f model.MediaFile) string { return f.Title })
-			Expect(paths).To(SatisfyAll(
-				HaveLen(6),
-				ContainElements(
-					"Taxman", "Eleanor Rigby", "I'm Only Sleeping",
-					"Help!", "The Night Before", "You've Got to Hide Your Love Away",
-				),
-			))
+				It("should import all albums", func() {
+					Expect(s.RescanAll(ctx, true)).To(Succeed())
+
+					albums, _ := ds.Album(ctx).GetAll(model.QueryOptions{Sort: "name"})
+					Expect(albums).To(HaveLen(2))
+					Expect(albums[0]).To(SatisfyAll(
+						HaveField("Name", Equal("Help!")),
+						HaveField("SongCount", Equal(3)),
+					))
+					Expect(albums[1]).To(SatisfyAll(
+						HaveField("Name", Equal("Revolver")),
+						HaveField("SongCount", Equal(4)),
+					))
+				})
+			})
 		})
 	})
 })
