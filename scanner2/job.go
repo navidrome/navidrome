d206b23558b492f7603bc75a90ff472e8d351589
Deluan <deluan@navidrome.org>
2024-05-22 19:38:04 -0400

    WIP - Import Albums

--- scanner2/job.go
+++ scanner2/job.go
@@ -7,6 +7,7 @@ import (
 	"sync/atomic"
 	"time"
 
+	"github.com/google/uuid"
 	"github.com/navidrome/navidrome/core/storage"
 	"github.com/navidrome/navidrome/log"
 	"github.com/navidrome/navidrome/model"
@@ -18,7 +19,9 @@ type scanJob struct {
 	ds          model.DataStore
 	startTime   time.Time
 	lastUpdates map[string]time.Time
-	lock        sync.RWMutex
+	folderLock  sync.RWMutex
+	albumPIDs   map[string]string
+	albumLock   sync.RWMutex
 	fullRescan  bool
 	numFolders  atomic.Int64
 }
@@ -38,23 +41,41 @@ func newScanJob(ctx context.Context, ds model.DataStore, lib model.Library, full
 		log.Error(ctx, "Error getting fs for library", "library", lib.Name, "path", lib.Path, err)
 		return nil, fmt.Errorf("error getting fs for library: %w", err)
 	}
+	albumPIDs, err := ds.Album(ctx).GetPIDs(lib.ID)
+	if err != nil {
+		log.Error(ctx, "Error getting album PIDs for library", "library", lib.Name, "path", lib.Path, err)
+		return nil, fmt.Errorf("error getting album PIDs for library: %w", err)
+	}
 	return &scanJob{
 		lib:         lib,
 		fs:          fsys,
 		ds:          ds,
 		startTime:   time.Now(),
 		lastUpdates: lastUpdates,
+		albumPIDs:   albumPIDs,
 		fullRescan:  fullRescan,
 	}, nil
 }
 
 func (s *scanJob) getLastUpdatedInDB(folderId string) time.Time {
-	s.lock.RLock()
-	defer s.lock.RUnlock()
+	s.folderLock.RLock()
+	defer s.folderLock.RUnlock()
 
 	t, ok := s.lastUpdates[folderId]
 	if !ok {
 		return time.Time{}
 	}
 	return t
+}
+
+func (s *scanJob) getAlbumID(albumPID string) string {
+	// TODO: Use RLock or sync.Map
+	s.albumLock.Lock()
+	defer s.albumLock.Unlock()
+	if id, ok := s.albumPIDs[albumPID]; ok {
+		return id
+	}
+	id := uuid.NewString()
+	s.albumPIDs[albumPID] = id
+	return id
 }
