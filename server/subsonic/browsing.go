932f0147c7641f7129d5dcb55fb9dcd1ae9ba382
Deluan <deluan@navidrome.org>
2024-06-01 12:13:31 -0400

    WIP

--- server/subsonic/browsing.go
+++ server/subsonic/browsing.go
@@ -223,7 +223,9 @@ func (api *Router) GetSong(r *http.Request) (*responses.Subsonic, error) {
 
 func (api *Router) GetGenres(r *http.Request) (*responses.Subsonic, error) {
 	ctx := r.Context()
-	genres, err := api.ds.Genre(ctx).GetAll(model.QueryOptions{Sort: "song_count, album_count, name desc", Order: "desc"})
+	// TODO Put back when album_count is available
+	//genres, err := api.ds.Genre(ctx).GetAll(model.QueryOptions{Sort: "song_count, album_count, name desc", Order: "desc"})
+	genres, err := api.ds.Genre(ctx).GetAll(model.QueryOptions{Sort: "song_count, name desc", Order: "desc"})
 	if err != nil {
 		log.Error(r, err)
 		return nil, err
