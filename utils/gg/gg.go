b4ef1b1e38eec7e42c1bc56e87b5420197cb1f66
Deluan <deluan@navidrome.org>
2024-06-05 22:48:00 -0400

    Replace gg.If with cmp.Or

--- utils/gg/gg.go
+++ utils/gg/gg.go
@@ -1,18 +1,6 @@
 // Package gg implements simple "extensions" to Go language. Based on https://github.com/icza/gog
 package gg
 
-// If returns v if it is a non-zero value, orElse otherwise.
-//
-// This is similar to elvis operator (?:) in Groovy and other languages.
-// Note: Different from the real elvis operator, the orElse expression will always get evaluated.
-func If[T comparable](v T, orElse T) T {
-	var zero T
-	if v != zero {
-		return v
-	}
-	return orElse
-}
-
 // P returns a pointer to the input value
 func P[T any](v T) *T {
 	return &v
