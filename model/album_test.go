749f3dd3c4754a5784530d7b4b0e43f8e5cb0b5b
Deluan <deluan@navidrome.org>
2024-06-09 19:33:21 -0400

    Add participants to albums

--- model/album_test.go
+++ model/album_test.go
@@ -1,6 +1,8 @@
 package model_test
 
 import (
+	"encoding/json"
+
 	. "github.com/navidrome/navidrome/model"
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
@@ -82,6 +84,26 @@ var _ = Describe("Albums", func() {
 					artist := albums.ToAlbumArtist()
 					Expect(artist.MbzArtistID).To(Equal("id1"))
 				})
+			})
+		})
+	})
+
+	Context("JSON Marshalling", func() {
+		When("we have a valid Albums object", func() {
+			BeforeEach(func() {
+				albums = Albums{
+					{ID: "1", AlbumArtist: "Artist", AlbumArtistID: "11", SortAlbumArtistName: "SortAlbumArtistName", OrderAlbumArtistName: "OrderAlbumArtistName"},
+					{ID: "2", AlbumArtist: "Artist", AlbumArtistID: "11", SortAlbumArtistName: "SortAlbumArtistName", OrderAlbumArtistName: "OrderAlbumArtistName"},
+				}
+			})
+			It("marshals correctly", func() {
+				data, err := json.Marshal(albums)
+				Expect(err).To(BeNil())
+
+				var albums2 Albums
+				err = json.Unmarshal(data, &albums2)
+				Expect(err).To(BeNil())
+				Expect(albums2).To(Equal(albums))
 			})
 		})
 	})
