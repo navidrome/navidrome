2977dd3f55fda2cfe5084c9065ea8bb02299467c
Deluan <deluan@navidrome.org>
2024-06-09 18:21:45 -0400

    Save participations to DB

--- model/participations.go
+++ model/participations.go
@@ -4,6 +4,10 @@ type Role struct {
 	role string
 }
 
+func (r Role) String() string {
+	return r.role
+}
+
 var (
 	RoleArtist      = Role{"artist"}
 	RoleAlbumArtist = Role{"album_artist"}
@@ -19,6 +23,31 @@ var (
 	RoleDJMixer     = Role{"djmixer"}
 	RolePerformer   = Role{"performer"}
 )
+
+var AllRoles = []Role{
+	RoleArtist,
+	RoleAlbumArtist,
+	RoleComposer,
+	RoleConductor,
+	RoleLyricist,
+	RoleArranger,
+	RoleProducer,
+	RoleDirector,
+	RoleEngineer,
+	RoleMixer,
+	RoleRemixer,
+	RoleDJMixer,
+	RolePerformer,
+}
+
+func RoleFromString(role string) Role {
+	for _, r := range AllRoles {
+		if r.String() == role {
+			return r
+		}
+	}
+	return Role{}
+}
 
 type Participations map[Role][]Artist
 
