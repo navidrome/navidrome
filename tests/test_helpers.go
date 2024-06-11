83c925014c5c1e23cd6348038ce3c391408c9af7
Deluan <deluan@navidrome.org>
2024-06-10 20:37:59 -0400

    Fix tests

--- tests/test_helpers.go
+++ tests/test_helpers.go
@@ -1,11 +1,13 @@
 package tests
 
 import (
+	"context"
 	"io/fs"
 	"os"
 	"path/filepath"
 
 	"github.com/google/uuid"
+	"github.com/navidrome/navidrome/db"
 )
 
 type testingT interface {
@@ -20,4 +22,15 @@ func TempFile(t testingT, prefix, suffix string) (fs.File, string, error) {
 	name := TempFileName(t, prefix, suffix)
 	f, err := os.Create(name)
 	return f, name, err
+}
+
+func ClearDB() error {
+	_, err := db.Db().WriteDB().ExecContext(context.Background(), `
+			PRAGMA writable_schema = 1;
+			DELETE FROM sqlite_master;
+			PRAGMA writable_schema = 0;
+			VACUUM;
+			PRAGMA integrity_check;
+		`)
+	return err
 }
