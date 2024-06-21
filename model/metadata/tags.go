b27219d75ed7fda647fd039314f1be4083ea1e42
Deluan <deluan@navidrome.org>
2024-06-21 18:38:42 -0400

    Sanitize tags when importing

--- model/metadata/tags.go
+++ model/metadata/tags.go
@@ -78,23 +78,40 @@ const (
 	MusicBrainzAlbumID       TagName = "musicbrainz_albumid"
 )
 
-var mappings = sync.OnceValue(func() map[string][]string {
+type tagMapping struct {
+	Aliases   []string `yaml:"aliases"`
+	Type      TagType  `yaml:"type"`
+	MaxLength int      `yaml:"maxLength"`
+}
+
+type TagType string
+
+const (
+	TagTypeInteger TagType = "integer"
+	TagTypeFloat   TagType = "float"
+	TagTypeDate    TagType = "date"
+	TagTypeUUID    TagType = "uuid"
+)
+
+var mappings = sync.OnceValue(func() map[string]tagMapping {
 	mappingsFile, err := resources.FS().Open("mappings.yaml")
 	if err != nil {
 		log.Error("Error opening mappings.yaml", err)
 	}
 	decoder := yaml.NewDecoder(mappingsFile)
-	var mappings map[string][]string
+	var mappings map[string]tagMapping
 	err = decoder.Decode(&mappings)
 	if err != nil {
 		log.Error("Error decoding mappings.yaml", err)
 	}
-	normalized := map[string][]string{}
+	normalized := map[string]tagMapping{}
 	for k, v := range mappings {
 		k = strings.ToLower(k)
-		for _, val := range v {
-			normalized[k] = append(normalized[k], strings.ToLower(val))
+		var aliases []string
+		for _, val := range v.Aliases {
+			aliases = append(aliases, strings.ToLower(val))
 		}
+		normalized[k] = tagMapping{Aliases: aliases, Type: v.Type, MaxLength: v.MaxLength}
 	}
 	return normalized
 })
