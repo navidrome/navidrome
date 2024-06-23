b3f190d2480625d283c9fd593d473c7c51f743ff
Deluan <deluan@navidrome.org>
2024-06-22 22:41:49 -0400

    Refactor and tests

--- model/participations_test.go
+++ model/participations_test.go
@@ -84,20 +84,60 @@ var _ = Describe("Participations", func() {
 		var participations1, participations2 Participations
 		BeforeEach(func() {
 			participations1 = Participations{
-				RoleArtist:      []Artist{{ID: "1", Name: "Artist1"}, {ID: "2", Name: "Artist2"}},
+				RoleArtist:      []Artist{{ID: "1", Name: "Artist1"}, {ID: "2", Name: "Duplicated Artist"}},
 				RoleAlbumArtist: []Artist{{ID: "3", Name: "AlbumArtist1"}, {ID: "4", Name: "AlbumArtist2"}},
 			}
 			participations2 = Participations{
-				RoleArtist:      []Artist{{ID: "5", Name: "Artist3"}, {ID: "6", Name: "Artist4"}},
+				RoleArtist:      []Artist{{ID: "5", Name: "Artist3"}, {ID: "6", Name: "Artist4"}, {ID: "2", Name: "Duplicated Artist"}},
 				RoleAlbumArtist: []Artist{{ID: "7", Name: "AlbumArtist3"}, {ID: "8", Name: "AlbumArtist4"}},
 			}
 		})
-		It("merges correctly", func() {
+		It("merges correctly, skipping duplicated artists", func() {
 			participations1.Merge(participations2)
 			Expect(participations1).To(Equal(Participations{
-				RoleArtist:      []Artist{{ID: "1", Name: "Artist1"}, {ID: "2", Name: "Artist2"}, {ID: "5", Name: "Artist3"}, {ID: "6", Name: "Artist4"}},
+				RoleArtist:      []Artist{{ID: "1", Name: "Artist1"}, {ID: "2", Name: "Duplicated Artist"}, {ID: "5", Name: "Artist3"}, {ID: "6", Name: "Artist4"}},
 				RoleAlbumArtist: []Artist{{ID: "3", Name: "AlbumArtist1"}, {ID: "4", Name: "AlbumArtist2"}, {ID: "7", Name: "AlbumArtist3"}, {ID: "8", Name: "AlbumArtist4"}},
 			}))
+		})
+	})
+
+	Describe("All", func() {
+		var participations Participations
+		BeforeEach(func() {
+			participations = Participations{
+				RoleArtist:      []Artist{{ID: "1", Name: "Artist1"}, {ID: "2", Name: "Artist2"}},
+				RoleAlbumArtist: []Artist{{ID: "3", Name: "AlbumArtist1"}, {ID: "4", Name: "AlbumArtist2"}},
+				RoleProducer:    []Artist{{ID: "5", Name: "Producer", SortArtistName: "SortProducerName"}},
+				RoleComposer:    []Artist{{ID: "1", Name: "Artist1"}},
+			}
+		})
+
+		Describe("All", func() {
+			It("returns all artists found in the Participations", func() {
+				artists := participations.All()
+				Expect(artists).To(ConsistOf(
+					Artist{ID: "1", Name: "Artist1"},
+					Artist{ID: "2", Name: "Artist2"},
+					Artist{ID: "3", Name: "AlbumArtist1"},
+					Artist{ID: "4", Name: "AlbumArtist2"},
+					Artist{ID: "5", Name: "Producer", SortArtistName: "SortProducerName"},
+				))
+			})
+		})
+
+		Describe("AllIDs", func() {
+			It("returns all artist IDs found in the Participations", func() {
+				ids := participations.AllIDs()
+				Expect(ids).To(ConsistOf("1", "2", "3", "4", "5"))
+			})
+		})
+
+		Describe("AllNames", func() {
+			It("returns all artist names found in the Participations", func() {
+				names := participations.AllNames()
+				Expect(names).To(ConsistOf("Artist1", "Artist2", "AlbumArtist1", "AlbumArtist2",
+					"Producer", "SortProducerName"))
+			})
 		})
 	})
 })
