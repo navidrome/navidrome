5aa8af82ed9ddae01fd08883b3c34059143c995a
Rob Emery <git@mintsoft.net>
2024-08-19 18:00:54 +0100

    Fixing " non-constant format string in call to fmt.Errorf (govet)"

--- core/playback/queue.go
+++ core/playback/queue.go
@@ -104,7 +104,7 @@ func (pd *Queue) Shuffle() {
 	var err error
 	pd.Index, err = pd.getMediaFileIndexByID(backupID)
 	if err != nil {
-		log.Error("Could not find ID while shuffling: " + backupID)
+		log.Error("Could not find ID while shuffling: %d", backupID)
 	}
 }
 
@@ -114,7 +114,7 @@ func (pd *Queue) getMediaFileIndexByID(id string) (int, error) {
 			return idx, nil
 		}
 	}
-	return -1, fmt.Errorf("ID not found in playlist: " + id)
+	return -1, fmt.Errorf("ID not found in playlist: %d", id)
 }
 
 // Sets the index to a new, valid value inside the Items. Values lower than zero are going to be zero,
