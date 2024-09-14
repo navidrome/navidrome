f758c0fb93843abc06cc450194c90a295c5136df
Deluan <deluan@navidrome.org>
2024-09-14 19:18:14 -0400

    Format code

--- persistence/mediafile_repository.go
+++ persistence/mediafile_repository.go
@@ -70,9 +70,9 @@ func NewMediaFileRepository(ctx context.Context, db dbx.Builder) model.MediaFile
 	r.db = db
 	r.tableName = "media_file"
 	r.registerModel(&model.MediaFile{}, map[string]filterFunc{
-		"id":      idFilter(r.tableName),
+		"id":       idFilter(r.tableName),
 		"title":    fullTextFilter(r.tableName),
-		"starred": booleanFilter,
+		"starred":  booleanFilter,
 		"genre_id": tagIDFilter,
 	})
 	r.setSortMappings(map[string]string{
