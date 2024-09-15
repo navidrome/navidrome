d11462a042c8aa2b241fd771f779f2def07cf724
Caio Cotts <caio@cotts.com.br>
2024-09-15 13:26:50 -0400

    Add ShareURL configuration option

--- conf/configuration.go
+++ conf/configuration.go
@@ -69,6 +69,7 @@ type configOptions struct {
 	EnableStarRating                bool
 	EnableUserEditing               bool
 	EnableSharing                   bool
+	ShareURL                        string
 	DefaultDownloadableShare        bool
 	DefaultTheme                    string
 	DefaultLanguage                 string
@@ -389,6 +390,7 @@ func init() {
 	viper.SetDefault("devautologinusername", "")
 	viper.SetDefault("devactivitypanel", true)
 	viper.SetDefault("enablesharing", false)
+	viper.SetDefault("shareurl", "")
 	viper.SetDefault("defaultdownloadableshare", false)
 	viper.SetDefault("devenablebufferedscrobble", true)
 	viper.SetDefault("devsidebarplaylists", true)
