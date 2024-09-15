3b401e15d0a65b8a8154626f2105b527e840c666
Caio Cotts <caio@cotts.com.br>
2024-09-15 17:09:11 -0400

    Fix wrong placement of When in test

--- server/public/public_test.go
+++ server/public/public_test.go
@@ -32,25 +32,25 @@ var _ = Describe("publicURL", func() {
 			Expect(actual).To(Equal("http://share.myotherserver.com/share/img/123?size=300"))
 
 		})
-		When("ShareURL is not set", func() {
-			BeforeEach(func() {
-				conf.Server.ShareURL = ""
-			})
-			It("uses AbsoluteURL", func() {
-				r, _ := http.NewRequest("GET", "https://myserver.com/share/123", nil)
-				uri := path.Join(consts.URLPathPublic, "123")
-				actual := publicURL(r, uri, nil)
-				Expect(actual).To(Equal("https://myserver.com/share/123"))
-			})
-			It("concatenates params if provided", func() {
-				r, _ := http.NewRequest("GET", "https://myserver.com/share/123", nil)
-				uri := path.Join(consts.URLPathPublicImages, "123")
-				params := url.Values{
-					"size": []string{"300"},
-				}
-				actual := publicURL(r, uri, params)
-				Expect(actual).To(Equal("https://myserver.com/share/img/123?size=300"))
-			})
+	})
+	When("ShareURL is not set", func() {
+		BeforeEach(func() {
+			conf.Server.ShareURL = ""
+		})
+		It("uses AbsoluteURL", func() {
+			r, _ := http.NewRequest("GET", "https://myserver.com/share/123", nil)
+			uri := path.Join(consts.URLPathPublic, "123")
+			actual := publicURL(r, uri, nil)
+			Expect(actual).To(Equal("https://myserver.com/share/123"))
+		})
+		It("concatenates params if provided", func() {
+			r, _ := http.NewRequest("GET", "https://myserver.com/share/123", nil)
+			uri := path.Join(consts.URLPathPublicImages, "123")
+			params := url.Values{
+				"size": []string{"300"},
+			}
+			actual := publicURL(r, uri, params)
+			Expect(actual).To(Equal("https://myserver.com/share/img/123?size=300"))
 		})
 	})
 })
