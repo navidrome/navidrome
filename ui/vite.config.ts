05756beb309b71fa84e8a9f405db50285f611572
Deluan <deluan@navidrome.org>
2024-09-23 20:57:09 -0400

    feat: `make dev` working!

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -2,13 +2,23 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import eslintPlugin from '@nabla/vite-plugin-eslint'
 
+let frontendPort = 4533
+let backendPort = 4633
+if (process.env.PORT !== undefined) {
+  frontendPort = parseInt(process.env.PORT)
+  backendPort = frontendPort + 100
+}
+
 // https://vitejs.dev/config/
 export default defineConfig({
   // plugins: [react(), eslintPlugin({ formatter: 'stylish' })],
   plugins: [react()],
   server: {
     host: true,
-    port: parseInt(process.env.PORT) || 3000, // Use the environment variable or default to 3000
+    port: frontendPort,
+    proxy: {
+      '^/(auth|api|rest|backgrounds)/.*': 'http://localhost:' + backendPort,
+    },
   },
   base: './',
   build: {
