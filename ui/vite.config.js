597740475ce3bc9e2db76dd1ac157947e2c608e1
Deluan <deluan@navidrome.org>
2024-09-24 20:18:59 -0400

    fix: pwa manifest

--- ui/vite.config.js
+++ ui/vite.config.js
@@ -2,6 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import { VitePWA } from 'vite-plugin-pwa'
 import eslintPlugin from '@nabla/vite-plugin-eslint'
+import fs from 'fs'
+import path from 'path'
 
 let frontendPort = 4533
 let backendPort = 4633
@@ -10,6 +12,10 @@ if (process.env.PORT !== undefined) {
   backendPort = frontendPort + 100
 }
 
+// Load manifest file
+const jsonFilePath = path.resolve(__dirname, './public/manifest.webmanifest')
+const manifest = JSON.parse(fs.readFileSync(jsonFilePath, 'utf-8'))
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
@@ -17,6 +23,7 @@ export default defineConfig({
     eslintPlugin({ formatter: 'stylish' }),
     VitePWA({
       registerType: 'autoUpdate',
+      manifest,
       workbox: {
         // Workbox options
       },
