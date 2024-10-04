1eb31edcb10ec89b50f0a1b6d89b1c43b4ad82de
Kendall Garner <17521368+kgarner7@users.noreply.github.com>
2024-10-04 14:18:24 -0700

    don't forget that newline

--- contrib/postinstall.sh
+++ contrib/postinstall.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 if [ ! -f /etc/navidrome/navidrome.toml ]; then
-    printf "No navidrome.toml detected, creating in postinstall"
-    printf "DataFolder = \"/var/lib/navidrome\"" > /etc/navidrome/navidrome.toml
-    printf "MusicFolder = \"/opt/navidrome/music\"" >> /etc/navidrome/navidrome.toml
+    printf "No navidrome.toml detected, creating in postinstall\n"
+    printf "DataFolder = \"/var/lib/navidrome\"\n" > /etc/navidrome/navidrome.toml
+    printf "MusicFolder = \"/opt/navidrome/music\"\n" >> /etc/navidrome/navidrome.toml
 fi
 
 if [ ! -f /etc/systemd/system/navidrome.service ]; then
