440ecf0d83deffd3c6a8300cad5a30d68d380446
Kendall Garner <17521368+kgarner7@users.noreply.github.com>
2024-10-04 14:08:22 -0700

    bash -> sh, create toml manually if it doesn't exist (thanks debian)

--- contrib/postinstall.sh
+++ contrib/postinstall.sh
@@ -1,4 +1,10 @@
-#!/bin/bash
+#!/bin/sh
+
+if [ ! -f /etc/navidrome/navidrome.toml ]; then
+    printf "No navidrome.toml detected, creating in postinstall"
+    printf "DataFolder = \"/var/lib/navidrome\"" > /etc/navidrome/navidrome.toml
+    printf "MusicFolder = \"/opt/navidrome/music\"" >> /etc/navidrome/navidrome.toml
+fi
 
 if [ ! -f /etc/systemd/system/navidrome.service ]; then
     navidrome service install --user navidrome --working-directory /var/lib/navidrome --configfile /etc/navidrome/navidrome.toml
--- contrib/preinstall.sh
+++ contrib/preinstall.sh
@@ -1,5 +1,6 @@
-#!/bin/bash
+#!/bin/sh
 
-if ! getent passwd navidrome &> /dev/null; then
+if ! getent passwd navidrome > /dev/null 2>&1; then
+    printf "Creating default Navidrome user"
     useradd --home-dir /var/lib/navidrome --create-home --system --user-group navidrome
 fi
