5f8464107dc62353e72b311b935b93b85cdd1ae3
Kendall Garner <17521368+kgarner7@users.noreply.github.com>
2024-10-04 14:36:54 -0700

    postrm

---
+++ contrib/postremove.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+printf "Stopping, disabling, and removing Navidrome service\n"
+systemctl disable --now navidrome ||:
+rm -rf /etc/systemd/system/navidrome.service
+
+printf "The following may still be present:\n"
+printf "1. /etc/navidrome/navidrome.toml (configuration file)\n"
+printf "2. /var/lib/navidrome (database/cache)\n"
+printf "3. /opt/navidrome (default location for music)\n"
+printf "4. The Navidrome user (user name navidrome)\n"
