df7f2ddcaa8c3a5cd11da53772b18b1e986d4ea1
Deluan <deluan@navidrome.org>
2024-10-17 17:16:39 -0400

    fix(msi): build msi locally

--- Makefile
+++ Makefile
@@ -120,7 +120,7 @@ docker-build: ##@Cross_Compilation Cross-compile for any supported platform (che
 		--build-arg GIT_TAG=${GIT_TAG} \
 		--build-arg GIT_SHA=${GIT_SHA} \
 		--build-arg CROSS_TAGLIB_VERSION=${CROSS_TAGLIB_VERSION} \
-		--output "./dist" --target binary .
+		--output "./binaries" --target binary .
 .PHONY: docker-build
 
 docker-image: ##@Cross_Compilation Build Docker image, tagged as `deluan/navidrome:develop`, override with DOCKER_TAG var. Use IMAGE_PLATFORMS to specify target platforms
@@ -136,8 +136,9 @@ docker-image: ##@Cross_Compilation Build Docker image, tagged as `deluan/navidro
 .PHONY: docker-image
 
 msi: ##@Cross_Compilation Build MSI installer for Windows 64 bits
+	make docker-build PLATFORMS=windows/amd64
 	DOCKER_CLI_HINTS=false docker build -q -t navidrome-msi-builder -f wix/msitools.dockerfile .
-	docker run -it --rm -v $(PWD):/workspace navidrome-msi-builder wix/build_msi.sh /workspace amd64
+	docker run -it --rm -v $(PWD):/workspace -e GIT_TAG=${GIT_TAG} navidrome-msi-builder wix/build_msi.sh /workspace amd64
 .PHONY: msi
 
 get-music: ##@Development Download some free music from Navidrome's demo instance
--- wix/build_msi.sh
+++ wix/build_msi.sh
@@ -1,24 +1,29 @@
 #!/bin/sh
+
+#Exit if GIT_TAG is not set
+if [ -z "$GIT_TAG" ]; then
+  echo "GIT_TAG is not set, exiting..."
+  exit 1
+fi
+
 set -e
 
 WORKSPACE=$1
 ARCH=$2
-NAVIDROME_BUILD_VERSION=$(jq -r '.version' < "$WORKSPACE"/dist/metadata.json | sed -e 's/^v//' -e 's/-SNAPSHOT/.1/' )
+NAVIDROME_BUILD_VERSION=$(echo "$GIT_TAG" | sed -e 's/^v//' -e 's/-SNAPSHOT/.1/')
 
 echo
 echo "Building MSI package for $ARCH, version $NAVIDROME_BUILD_VERSION"
 
-MSI_OUTPUT_DIR=$WORKSPACE/dist/msi
+MSI_OUTPUT_DIR=$WORKSPACE/binaries/msi
 mkdir -p "$MSI_OUTPUT_DIR"
-BINARY_DIR=$WORKSPACE/dist/navidrome_windows_${ARCH}_windows_${ARCH}
+BINARY_DIR=$WORKSPACE/binaries/windows_${ARCH}
 
 if [ "$ARCH" = "386" ]; then
   PLATFORM="x86"
 else
   PLATFORM="x64"
-  BINARY_DIR=${BINARY_DIR}_v1
 fi
-
 
 BINARY=$BINARY_DIR/navidrome.exe
 if [ ! -f "$BINARY" ]; then
@@ -28,7 +33,7 @@ if [ ! -f "$BINARY" ]; then
   exit 1
 fi
 
-cp "$WORKSPACE"/LICENSE $WORKSPACE/README.md "$MSI_OUTPUT_DIR"
+cp "$WORKSPACE"/LICENSE "$WORKSPACE"/README.md "$MSI_OUTPUT_DIR"
 cp "$BINARY" "$MSI_OUTPUT_DIR"
 
 # workaround for wixl WixVariable not working to override bmp locations
@@ -37,6 +42,7 @@ cp "$WORKSPACE"/wix/bmp/dialogue.bmp /usr/share/wixl-*/ext/ui/bitmaps/dlgbmp.bmp
 
 cd "$MSI_OUTPUT_DIR"
 rm -f "$MSI_OUTPUT_DIR"/*.msi
-wixl "$WORKSPACE"/wix/navidrome.wxs -D Version="$NAVIDROME_BUILD_VERSION" -D Platform=$PLATFORM --arch $PLATFORM --ext ui --output "$MSI_OUTPUT_DIR"/navidrome_${ARCH}.msi
+wixl "$WORKSPACE"/wix/navidrome.wxs -D Version="$NAVIDROME_BUILD_VERSION" -D Platform=$PLATFORM --arch $PLATFORM \
+    --ext ui --output "$MSI_OUTPUT_DIR"/navidrome_"${ARCH}".msi
 du -h "$MSI_OUTPUT_DIR"/*.msi
 
