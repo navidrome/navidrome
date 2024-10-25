2b7c3a89827d7f549e710e60a9f1f8858144f95f
Kendall Garner <17521368+kgarner7@users.noreply.github.com>
2024-10-24 22:24:22 -0700

    better preremove

--- release/linux/preremove.sh
+++ release/linux/preremove.sh
@@ -1,15 +1,30 @@
 #!/bin/sh
 
-postinstall_flag="/var/lib/navidrome/.installed"
+action=$1
 
-if [ -f "$postinstall_flag" ]; then
-    # If this fails, ignore it
-    navidrome service uninstall || :
-    rm "$postinstall_flag"
+remove() {
+    postinstall_flag="/var/lib/navidrome/.installed"
 
-    printf "The following may still be present (especially if you have not done a purge):\n"
-    printf "1. /etc/navidrome/navidrome.toml (configuration file)\n"
-    printf "2. /var/lib/navidrome (database/cache)\n"
-    printf "3. /opt/navidrome (default location for music)\n"
-    printf "4. The Navidrome user (user name navidrome)\n"
-fi
+    if [ -f "$postinstall_flag" ]; then
+        # If this fails, ignore it
+        navidrome service uninstall || :
+        rm "$postinstall_flag"
+
+        printf "The following may still be present (especially if you have not done a purge):\n"
+        printf "1. /etc/navidrome/navidrome.toml (configuration file)\n"
+        printf "2. /var/lib/navidrome (database/cache)\n"
+        printf "3. /opt/navidrome (default location for music)\n"
+        printf "4. The Navidrome user (user name navidrome)\n"
+    fi
+}
+
+case "$action" in 
+    "1" | "upgrade")
+        # For an upgrade, do nothing
+        # Leave the service file untouched
+        # This is relevant for RPM/DEB-based installs
+        ;;
+    *)
+        remove
+        ;;
+esac
