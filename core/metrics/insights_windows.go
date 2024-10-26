3df8d6bfd7804aaf034ef8b5f05f030185c805e1
Deluan <deluan@navidrome.org>
2024-10-25 22:46:13 -0400

    feat(insights): add fs info

--- core/metrics/insights_windows.go
+++ core/metrics/insights_windows.go
@@ -3,6 +3,8 @@ package metrics
 import (
 	"os/exec"
 	"regexp"
+
+	"golang.org/x/sys/windows"
 )
 
 // Ex: Microsoft Windows [Version 10.0.26100.1742]
@@ -18,7 +20,34 @@ func getOSVersion() (version string, _ string) {
 
 	matches := winVerRegex.FindStringSubmatch(string(output))
 	if len(matches) != 2 {
-		return output, ""
+		return string(output), ""
 	}
 	return matches[1], ""
+}
+
+func getFilesystemType(path string) (string, error) {
+	pathPtr, err := windows.UTF16PtrFromString(path)
+	if err != nil {
+		return "", err
+	}
+
+	var volumeName, filesystemName [windows.MAX_PATH + 1]uint16
+	var serialNumber uint32
+	var maxComponentLen, filesystemFlags uint32
+
+	err = windows.GetVolumeInformation(
+		pathPtr,
+		&volumeName[0],
+		windows.MAX_PATH,
+		&serialNumber,
+		&maxComponentLen,
+		&filesystemFlags,
+		&filesystemName[0],
+		windows.MAX_PATH)
+
+	if err != nil {
+		return "", err
+	}
+
+	return windows.UTF16ToString(filesystemName[:]), nil
 }
