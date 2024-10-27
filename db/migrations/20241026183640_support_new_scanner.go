1ddfe10a70f86248bccbf0343bc9fcb999adf873
Deluan <deluan@navidrome.org>
2024-10-27 12:18:48 -0400

    fix: migration

--- db/migrations/20241026183640_support_new_scanner.go
+++ db/migrations/20241026183640_support_new_scanner.go
@@ -257,20 +257,6 @@ create index if not exists media_file_pid_ix
 create index if not exists media_file_missing_ix
 	on media_file (missing);
 `)
-	if err != nil {
-		return err
-	}
-
-	// Fix sort tags index to be case insensitive
-	_, err = tx.ExecContext(ctx, `
-drop index if exists media_file_sort_title;
-drop index if exists album_sort_name;
-drop index if exists artist_sort_name;
-
-create index if not exists media_file_sort_title on media_file(coalesce(nullif(sort_title,''),order_title) collate nocase);
-create index if not exists album_sort_name on album(coalesce(nullif(sort_album_name,''),order_album_name) collate nocase);
-create index if not exists artist_sort_name on artist(coalesce(nullif(sort_artist_name,''),order_artist_name) collate nocase);
-`)
 
 	return err
 }
