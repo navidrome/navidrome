5b23a0a13b41cb720bf591fed919c516eb154e97
Deluan <deluan@navidrome.org>
2024-11-01 18:51:10 -0400

    fix: lint - remove deprecation from artist totals
    Signed-off-by: Deluan <deluan@navidrome.org>

--- model/artist.go
+++ model/artist.go
@@ -5,25 +5,22 @@ import "time"
 type Artist struct {
 	Annotations `structs:"-"`
 
-	ID              string               `structs:"id" json:"id"`
-	Name            string               `structs:"name" json:"name"`
-	SortArtistName  string               `structs:"sort_artist_name" json:"sortArtistName,omitempty"`
-	OrderArtistName string               `structs:"order_artist_name" json:"orderArtistName,omitempty"`
-	MbzArtistID     string               `structs:"mbz_artist_id" json:"mbzArtistId,omitempty"`
-	Stats           map[Role]ArtistStats `structs:"-" json:"stats,omitempty"`
-	// Deprecated: Use Stats instead
-	Size int64 `structs:"-" json:"size,omitempty"`
-	// Deprecated: Use Stats instead
-	AlbumCount int `structs:"-" json:"albumCount,omitempty"`
-	// Deprecated: Use Stats instead
-	SongCount             int        `structs:"-" json:"songCount,omitempty"`
-	Biography             string     `structs:"biography" json:"biography,omitempty"`
-	SmallImageUrl         string     `structs:"small_image_url" json:"smallImageUrl,omitempty"`
-	MediumImageUrl        string     `structs:"medium_image_url" json:"mediumImageUrl,omitempty"`
-	LargeImageUrl         string     `structs:"large_image_url" json:"largeImageUrl,omitempty"`
-	ExternalUrl           string     `structs:"external_url" json:"externalUrl,omitempty"`
-	SimilarArtists        Artists    `structs:"similar_artists"  json:"-"`
-	ExternalInfoUpdatedAt *time.Time `structs:"external_info_updated_at" json:"externalInfoUpdatedAt,omitempty"`
+	ID                    string               `structs:"id" json:"id"`
+	Name                  string               `structs:"name" json:"name"`
+	SortArtistName        string               `structs:"sort_artist_name" json:"sortArtistName,omitempty"`
+	OrderArtistName       string               `structs:"order_artist_name" json:"orderArtistName,omitempty"`
+	MbzArtistID           string               `structs:"mbz_artist_id" json:"mbzArtistId,omitempty"`
+	Stats                 map[Role]ArtistStats `structs:"-" json:"stats,omitempty"`
+	Size                  int64                `structs:"-" json:"size,omitempty"`
+	AlbumCount            int                  `structs:"-" json:"albumCount,omitempty"`
+	SongCount             int                  `structs:"-" json:"songCount,omitempty"`
+	Biography             string               `structs:"biography" json:"biography,omitempty"`
+	SmallImageUrl         string               `structs:"small_image_url" json:"smallImageUrl,omitempty"`
+	MediumImageUrl        string               `structs:"medium_image_url" json:"mediumImageUrl,omitempty"`
+	LargeImageUrl         string               `structs:"large_image_url" json:"largeImageUrl,omitempty"`
+	ExternalUrl           string               `structs:"external_url" json:"externalUrl,omitempty"`
+	SimilarArtists        Artists              `structs:"similar_artists"  json:"-"`
+	ExternalInfoUpdatedAt *time.Time           `structs:"external_info_updated_at" json:"externalInfoUpdatedAt,omitempty"`
 
 	CreatedAt time.Time `structs:"created_at" json:"createdAt,omitempty"` // Oldest CreatedAt for all songs for this artist
 	UpdatedAt time.Time `structs:"updated_at" json:"updatedAt,omitempty"` // Newest UpdatedAt for all songs for this artist
