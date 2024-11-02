785302d82a2505c4a665e5ac78371b2e1cb5a5c2
Deluan <deluan@navidrome.org>
2024-11-02 19:28:41 -0400

    fix: migration `addColumn` hack
    Signed-off-by: Deluan <deluan@navidrome.org>

--- db/migrations/migration.go
+++ db/migrations/migration.go
@@ -4,9 +4,12 @@ import (
 	"context"
 	"database/sql"
 	"fmt"
+	"strings"
 	"sync"
 
+	"github.com/navidrome/navidrome/conf"
 	"github.com/navidrome/navidrome/consts"
+	"github.com/navidrome/navidrome/log"
 )
 
 // Use this in migrations that need to communicate something important (breaking changes, forced reindexes, etc...)
@@ -58,9 +61,24 @@ func checkErr(err error) {
 	}
 }
 
+// Hack way to add a column with a default value to a table. This is needed because SQLite does not support
+// adding a column with a default value to a table that already has data, and there is no ALTER TABLE MODIFY COLUMN.
+//
+// Based on https://stackoverflow.com/a/25917323
+//
+// Add a new column to a table, setting the initial value for existing rows based on a SQL expression.
+// It is done in 3 steps:
+//  1. Add the column as nullable. Due the way SQLite manipulates the DDL in memory,
+//     (we need to add extra padding to the default value to avoid truncating it when changing the column to not null)
+//  2. Update the column with the initial value
+//  3. Change the column to not null with the default value
 func addColumn(ctx context.Context, tx *sql.Tx, tableName, columnName, columnType, defaultValue, initialValue string) error {
+	log.Error(conf.Server.DbPath)
+	// Format the `default null` value to have the same length as the final defaultValue
+	finalLen := len(fmt.Sprintf(`%s not`, defaultValue))
+	tempDefault := fmt.Sprintf(`default %s null`, strings.Repeat(" ", finalLen))
 	_, err := tx.ExecContext(ctx, fmt.Sprintf(`
-alter table %s add column %s %s default null /* REPLACE ME */;`, tableName, columnName, columnType))
+alter table %s add column %s %s %s;`, tableName, columnName, columnType, tempDefault))
 	if err != nil {
 		return err
 	}
@@ -72,10 +90,18 @@ update %s set %s = %s where %[2]s is null;`, tableName, columnName, initialValue
 	_, err = tx.ExecContext(ctx, fmt.Sprintf(`
 PRAGMA writable_schema = on;
 UPDATE sqlite_master
-SET sql = replace(sql, 'default null /* REPLACE ME */', 'default %s not null')
+SET sql = replace(sql, '%[1]s %[2]s %[5]s', '%[1]s %[2]s default %[3]s not null')
 WHERE type = 'table'
-  AND name = '%s';
+  AND name = '%[4]s';
 PRAGMA writable_schema = off;
-`, defaultValue, tableName))
+`, columnName, columnType, defaultValue, tableName, tempDefault))
+	if err != nil {
+		return err
+	}
+	return err
+}
+
+func execute(ctx context.Context, tx *sql.Tx, stmt string) error {
+	_, err := tx.ExecContext(ctx, stmt)
 	return err
 }
