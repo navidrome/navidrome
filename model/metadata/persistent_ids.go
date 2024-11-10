886fe78ca00ce2ddd79ba5d7adc4ec89ce8e7aca
Deluan <deluan@navidrome.org>
2024-11-09 20:24:49 -0500

    feat: persistent_ids (wip)
    Signed-off-by: Deluan <deluan@navidrome.org>

--- model/metadata/persistent_ids.go
+++ model/metadata/persistent_ids.go
@@ -2,7 +2,6 @@ package metadata
 
 import (
 	"cmp"
-	"fmt"
 	"path/filepath"
 	"strings"
 
@@ -14,35 +13,44 @@ import (
 	"github.com/navidrome/navidrome/utils/str"
 )
 
+// BFR Must be configurable
 const (
 	albumPIDSpec = "musicbrainz_albumid|albumartistid,album,version,releasedate"
+	trackPIDSpec = "musicbrainz_trackid|albumid,discnumber,tracknumber,title"
 )
 
-type hashFunc = func(string) string
+type hashFunc = func(...string) string
 
 // getPID returns the persistent ID for a given spec, getting the referenced values from the metadata
 // The spec is a pipe-separated list of fields, where each field is a comma-separated list of attributes
+// Attributes can be either tags or some processed values like folder, albumid, albumartistid, etc.
 // For each field, it gets all its attributes values and concatenates them, then hashes the result.
 // If a field is empty, it is skipped and the function looks for the next field.
-func createGetPID(hash hashFunc) func(md Metadata, spec string) string {
-	var getPID func(md Metadata, spec string, hash hashFunc) string
-	getAttr := func(md Metadata, attr string) string {
+func createGetPID(hash hashFunc) func(mf model.MediaFile, md Metadata, spec string) string {
+	var getPID func(mf model.MediaFile, md Metadata, spec string, hash hashFunc) string
+	getAttr := func(mf model.MediaFile, md Metadata, attr string) string {
 		switch attr {
 		case "albumid":
-			return getPID(md, albumPIDSpec, hash)
+			return getPID(mf, md, albumPIDSpec, hash)
 		case "folder":
-			return filepath.Dir(md.FilePath())
+			return filepath.Dir(mf.Path)
+		case "albumartistid":
+			return hash(str.Clear(strings.ToLower(mf.AlbumArtist)))
+		case "title":
+			return mf.Title
+		case "album":
+			return str.Clear(strings.ToLower(md.String(model.TagAlbum)))
 		}
 		return md.String(model.TagName(attr))
 	}
-	getPID = func(md Metadata, spec string, hash hashFunc) string {
+	getPID = func(mf model.MediaFile, md Metadata, spec string, hash hashFunc) string {
 		pid := ""
 		fields := strings.Split(spec, "|")
 		for _, field := range fields {
 			attributes := strings.Split(field, ",")
 			hasValue := false
 			values := slice.Map(attributes, func(attr string) string {
-				v := getAttr(md, attr)
+				v := getAttr(mf, md, attr)
 				if v != "" {
 					hasValue = true
 				}
@@ -56,44 +64,23 @@ func createGetPID(hash hashFunc) func(md Metadata, spec string) string {
 		return hash(pid)
 	}
 
-	return func(md Metadata, spec string) string {
-		return getPID(md, spec, hash)
+	return func(mf model.MediaFile, md Metadata, spec string) string {
+		return getPID(mf, md, spec, hash)
 	}
 }
 
-// BFR Must be configurable
 func (md Metadata) trackPID(mf model.MediaFile) string {
-	value := cmp.Or(
-		mf.MbzReleaseTrackID,
-		fmt.Sprintf("%s\\%02d\\%02d%s",
-			md.albumID(mf),
-			mf.DiscNumber,
-			mf.TrackNumber,
-			md.mapTrackTitle(),
-		),
-	)
-
-	return id.NewHash(str.Clear(strings.ToLower(value)))
+	return createGetPID(id.NewHash)(mf, md, trackPIDSpec)
 }
 
-// BFR Must be configurable
-// BFR Tag mapper for PIDs need to use functions, to be able to extract attributes or processed values
 func (md Metadata) albumID(mf model.MediaFile) string {
-	value := cmp.Or(
-		mf.MbzAlbumID,
-		fmt.Sprintf("%s\\%s\\%s\\%s",
-			md.artistID(mf.AlbumArtist),
-			strings.ToLower(md.mapAlbumName()),
-			strings.ToLower(md.String(model.TagAlbumVersion)),
-			md.String(model.TagReleaseDate),
-		),
-	)
-	return id.NewHash(str.Clear(strings.ToLower(value)))
+	return createGetPID(id.NewHash)(mf, md, albumPIDSpec)
 }
 
 // BFR Must be configurable?
 func (md Metadata) artistID(name string) string {
-	return id.NewHash(str.Clear(strings.ToLower(name)))
+	mf := model.MediaFile{AlbumArtist: name}
+	return createGetPID(id.NewHash)(mf, md, "albumartistid")
 }
 
 func (md Metadata) mapTrackTitle() string {
--- model/metadata/persistent_ids_test.go
+++ model/metadata/persistent_ids_test.go
@@ -1,49 +1,45 @@
 package metadata
 
 import (
-	"testing"
+	"strings"
 
 	"github.com/navidrome/navidrome/model"
+	"github.com/navidrome/navidrome/model/id"
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 )
 
-func TestMetadata(t *testing.T) {
-	RegisterFailHandler(Fail)
-	RunSpecs(t, "Metadata Suite")
-}
-
 var _ = Describe("getPID", func() {
 	var (
 		md     Metadata
+		mf     model.MediaFile
 		sum    hashFunc
-		getPID func(md Metadata, spec string) string
+		getPID func(mf model.MediaFile, md Metadata, spec string) string
 	)
 
 	BeforeEach(func() {
-		sum = func(s string) string { return s }
+		sum = func(s ...string) string { return "(" + strings.Join(s, ",") + ")" }
 		getPID = createGetPID(sum)
 	})
 
 	Context("attributes are tags", func() {
-		spec := "musicbrainz_trackid|album,discnumber,tracknumber,title"
+		spec := "musicbrainz_trackid|album,discnumber,tracknumber"
 		When("no attributes were present", func() {
 			It("should return empty pid", func() {
 				md.tags = map[model.TagName][]string{}
-				pid := getPID(md, spec)
-				Expect(pid).To(BeEmpty())
+				pid := getPID(mf, md, spec)
+				Expect(pid).To(Equal("()"))
 			})
 		})
 		When("all fields are present", func() {
 			It("should return the pid", func() {
 				md.tags = map[model.TagName][]string{
 					"musicbrainz_trackid": {"mbtrackid"},
-					"album":               {"albumname"},
+					"album":               {"album name"},
 					"discnumber":          {"1"},
 					"tracknumber":         {"1"},
-					"title":               {"song title"},
 				}
-				Expect(getPID(md, spec)).To(Equal("mbtrackid"))
+				Expect(getPID(mf, md, spec)).To(Equal("(mbtrackid)"))
 			})
 		})
 		When("only first field is present", func() {
@@ -51,27 +47,35 @@ var _ = Describe("getPID", func() {
 				md.tags = map[model.TagName][]string{
 					"musicbrainz_trackid": {"mbtrackid"},
 				}
-				Expect(getPID(md, spec)).To(Equal("mbtrackid"))
+				Expect(getPID(mf, md, spec)).To(Equal("(mbtrackid)"))
 			})
 		})
 		When("first is empty, but second field is present", func() {
 			It("should return the pid", func() {
 				md.tags = map[model.TagName][]string{
-					"album":      {"albumname"},
+					"album":      {"album name"},
 					"discnumber": {"1"},
-					"title":      {"song title"},
 				}
-				Expect(getPID(md, spec)).To(Equal("albumname\\1\\\\song title"))
+				Expect(getPID(mf, md, spec)).To(Equal("(album name\\1\\)"))
 			})
 		})
 	})
 	Context("calculated attributes", func() {
+		When("field is title", func() {
+			It("should return the pid", func() {
+				spec := "title|folder"
+				md.tags = map[model.TagName][]string{"title": {"title"}}
+				md.filePath = "/path/to/file.mp3"
+				mf.Title = "Title"
+				Expect(getPID(mf, md, spec)).To(Equal("(Title)"))
+			})
+		})
 		When("field is folder", func() {
 			It("should return the pid", func() {
 				spec := "folder|title"
 				md.tags = map[model.TagName][]string{"title": {"title"}}
-				md.filePath = "/path/to/file.mp3"
-				Expect(getPID(md, spec)).To(Equal("/path/to"))
+				mf.Path = "/path/to/file.mp3"
+				Expect(getPID(mf, md, spec)).To(Equal("(/path/to)"))
 			})
 		})
 		When("field is albumid", func() {
@@ -79,12 +83,54 @@ var _ = Describe("getPID", func() {
 				spec := "albumid|title"
 				md.tags = map[model.TagName][]string{
 					"title":       {"title"},
-					"album":       {"albumname"},
+					"album":       {"album name"},
 					"version":     {"version"},
 					"releasedate": {"2021-01-01"},
 				}
-				Expect(getPID(md, spec)).To(Equal("\\albumname\\version\\2021-01-01"))
+				mf.AlbumArtist = "Album Artist"
+				Expect(getPID(mf, md, spec)).To(Equal("(((album artist)\\album name\\version\\2021-01-01))"))
 			})
 		})
+		When("field is albumartistid", func() {
+			It("should return the pid", func() {
+				spec := "musicbrainz_albumartistid|albumartistid"
+				md.tags = map[model.TagName][]string{
+					"albumartist": {"Album Artist"},
+				}
+				mf.AlbumArtist = "Album Artist"
+				Expect(getPID(mf, md, spec)).To(Equal("((album artist))"))
+			})
+		})
+		When("field is album", func() {
+			It("should return the pid", func() {
+				spec := "album|title"
+				md.tags = map[model.TagName][]string{"album": {"Album Name"}}
+				Expect(getPID(mf, md, spec)).To(Equal("(album name)"))
+			})
+		})
+	})
+	It("trackPID with MBID", func() {
+		md.tags = map[model.TagName][]string{
+			"musicbrainz_trackid": {"mbtrackid"},
+		}
+		mf.MbzReleaseTrackID = "mbtrackid"
+		getPID := createGetPID(id.NewHash)
+		Expect(md.trackPID(mf)).To(Equal(getPID(mf, md, trackPIDSpec)))
+	})
+	It("trackPID without MBID", func() {
+		md.tags = map[model.TagName][]string{
+			"albumartist": {"Album Artist"},
+			"album":       {"Album Name"},
+			"discnumber":  {"02"},
+			"tracknumber": {"05"},
+			"title":       {"Song Title"},
+		}
+		mf.AlbumArtist = "Album Artist"
+		mf.Title = "Song Title"
+		mf.DiscNumber = 2
+		mf.TrackNumber = 5
+		mf.Album = "Album Name"
+		getPID := createGetPID(id.NewHash)
+		Expect(md.trackPID(mf)).To(Equal(getPID(mf, md, trackPIDSpec)))
 	})
 })
