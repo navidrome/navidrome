ded0583c4092a95030ea9f512ff247205a578ce7
Kendall Garner <17521368+kgarner7@users.noreply.github.com>
2025-01-09 07:55:46 -0800

    feat(cli): support getting playlists via cli

--- cmd/pls.go
+++ cmd/pls.go
@@ -2,7 +2,9 @@ package cmd
 
 import (
 	"context"
+	"encoding/json"
 	"errors"
+	"fmt"
 	"os"
 
 	"github.com/Masterminds/squirrel"
@@ -15,25 +17,50 @@ import (
 )
 
 var (
-	playlistID string
-	outputFile string
+	playlistID   string
+	outputFile   string
+	userID       string
+	outputFormat string
 )
+
+type DisplayPlaylist struct {
+	Id        string `json:"id"`
+	Name      string `json:"name"`
+	OwnerName string `json:"ownerName"`
+	OwnerId   string `json:"ownerId"`
+}
+
+type DisplayPlaylists []DisplayPlaylist
 
 func init() {
 	plsCmd.Flags().StringVarP(&playlistID, "playlist", "p", "", "playlist name or ID")
 	plsCmd.Flags().StringVarP(&outputFile, "output", "o", "", "output file (default stdout)")
 	_ = plsCmd.MarkFlagRequired("playlist")
 	rootCmd.AddCommand(plsCmd)
+
+	listCommand.Flags().StringVarP(&userID, "user", "u", "", "user name or ID")
+	listCommand.Flags().StringVarP(&outputFormat, "format", "f", "plain", "output format [supported values: json, plain]")
+	plsCmd.AddCommand(listCommand)
 }
 
-var plsCmd = &cobra.Command{
-	Use:   "pls",
-	Short: "Export playlists",
-	Long:  "Export Navidrome playlists to M3U files",
-	Run: func(cmd *cobra.Command, args []string) {
-		runExporter()
-	},
-}
+var (
+	plsCmd = &cobra.Command{
+		Use:   "pls",
+		Short: "Export playlists",
+		Long:  "Export Navidrome playlists to M3U files",
+		Run: func(cmd *cobra.Command, args []string) {
+			runExporter()
+		},
+	}
+
+	listCommand = &cobra.Command{
+		Use:   "list",
+		Short: "List playlists",
+		Run: func(cmd *cobra.Command, args []string) {
+			runList()
+		},
+	}
+)
 
 func runExporter() {
 	sqlDB := db.Db()
@@ -67,5 +94,57 @@ func runExporter() {
 	err = os.WriteFile(outputFile, []byte(pls), 0600)
 	if err != nil {
 		log.Fatal("Error writing to the output file", "file", outputFile, err)
+	}
+}
+
+func runList() {
+	if outputFormat != "plain" && outputFormat != "json" {
+		log.Fatal("Invalid output format. Must be one of plain, json", "format", outputFormat)
+	}
+
+	sqlDB := db.Db()
+	ds := persistence.New(sqlDB)
+	ctx := auth.WithAdminUser(context.Background(), ds)
+
+	options := model.QueryOptions{Sort: "owner_name"}
+
+	if userID != "" {
+		user, err := ds.User(ctx).FindByUsername(userID)
+
+		if err != nil && !errors.Is(err, model.ErrNotFound) {
+			log.Fatal("Error retrieving user by name", "name", userID, err)
+		}
+
+		if errors.Is(err, model.ErrNotFound) {
+			user, err = ds.User(ctx).Get(userID)
+			if err != nil {
+				log.Fatal("Error retrieving user by id", "id", userID, err)
+			}
+		}
+
+		options.Filters = squirrel.Eq{"owner_id": user.ID}
+	}
+
+	playlists, err := ds.Playlist(ctx).GetAll(options)
+	if err != nil {
+		log.Fatal(ctx, "Failed to retrieve playlists", err)
+	}
+
+	if outputFormat == "plain" {
+		println("playlist name|playlist id|owner name|owner id")
+		for _, playlist := range playlists {
+			fmt.Printf("%s|%s|%s|%s\n", playlist.Name, playlist.ID, playlist.OwnerName, playlist.OwnerID)
+		}
+	} else {
+		display := make(DisplayPlaylists, len(playlists))
+		for idx, playlist := range playlists {
+			display[idx].Id = playlist.ID
+			display[idx].Name = playlist.Name
+			display[idx].OwnerId = playlist.OwnerID
+			display[idx].OwnerName = playlist.OwnerName
+		}
+
+		j, _ := json.Marshal(display)
+		fmt.Printf("%s\n", j)
 	}
 }
