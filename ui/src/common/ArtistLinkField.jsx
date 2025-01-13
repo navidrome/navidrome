3573c771ef419071a53d0e056d1352aaec0251d6
Deluan <deluan@navidrome.org>
2025-01-12 20:55:04 -0500

    feat: use (display) albumArtist and add links to each artist
    Signed-off-by: Deluan <deluan@navidrome.org>

--- ui/src/common/ArtistLinkField.jsx
+++ ui/src/common/ArtistLinkField.jsx
@@ -3,14 +3,53 @@ import PropTypes from 'prop-types'
 import { Link } from 'react-admin'
 import { withWidth } from '@material-ui/core'
 import { useGetHandleArtistClick } from './useGetHandleArtistClick'
+import { intersperse } from '../utils/index.js'
 
-export const ArtistLinkField = withWidth()(({
-  record,
-  className,
-  width,
-  source,
-}) => {
+const ALink = withWidth()((props) => {
+  const { artist, width, ...rest } = props
   const artistLink = useGetHandleArtistClick(width)
+  return (
+    <Link
+      key={artist.id}
+      to={artistLink(artist.id)}
+      onClick={(e) => e.stopPropagation()}
+      {...rest}
+    >
+      {artist.name}
+    </Link>
+  )
+})
+
+const parseAndReplaceArtists = (
+  displayAlbumArtist,
+  albumArtists,
+  className,
+) => {
+  let result = []
+  let lastIndex = 0
+
+  albumArtists.forEach((artist) => {
+    const index = displayAlbumArtist.indexOf(artist.name, lastIndex)
+    if (index !== -1) {
+      // Add text before the artist name
+      if (index > lastIndex) {
+        result.push(displayAlbumArtist.slice(lastIndex, index))
+      }
+      // Add the artist link
+      result.push(<ALink artist={artist} className={className}></ALink>)
+      lastIndex = index + artist.name.length
+    }
+  })
+
+  // Add any remaining text after the last artist name
+  if (lastIndex < displayAlbumArtist.length) {
+    result.push(displayAlbumArtist.slice(lastIndex))
+  }
+
+  return result
+}
+
+export const ArtistLinkField = ({ record, className, source }) => {
   const role = source.toLowerCase()
   const artists = record['participants']
     ? record['participants'][role]
@@ -27,6 +66,14 @@ export const ArtistLinkField = withWidth()(({
     })
   }
 
+  if (role === 'albumartist') {
+    return (
+      <div className={className}>
+        {parseAndReplaceArtists(record[source], artists, className)}
+      </div>
+    )
+  }
+
   // Dedupe artists, only shows the first 3
   const seen = new Set()
   const dedupedArtists = []
@@ -37,26 +84,12 @@ export const ArtistLinkField = withWidth()(({
     }
   })
 
-  return (
-    <>
-      {dedupedArtists.map((artist, index) => {
-        const id = artist.id
-        return (
-          <>
-            <Link
-              to={artistLink(id)}
-              onClick={(e) => e.stopPropagation()}
-              className={className}
-            >
-              {artist.name}
-            </Link>
-            {index < dedupedArtists.length - 1 && ' • '}
-          </>
-        )
-      })}
-    </>
-  )
-})
+  const artistsList = dedupedArtists.map((artist) => (
+    <ALink artist={artist} className={className} key={artist?.id} />
+  ))
+
+  return <>{intersperse(artistsList, ' • ')}</>
+}
 
 ArtistLinkField.propTypes = {
   record: PropTypes.object,
