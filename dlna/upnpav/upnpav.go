ac84e1ca5a55ca6a57dff77b312e06bf4a50fbd2
Rob Emery <git@mintsoft.net>
2025-01-19 18:23:11 +0000

    Some metadata returned

--- dlna/upnpav/upnpav.go
+++ dlna/upnpav/upnpav.go
@@ -50,9 +50,9 @@ type Object struct {
 	Album       string    `xml:"upnp:album,omitempty"`
 	Genre       string    `xml:"upnp:genre,omitempty"`
 	AlbumArtURI string    `xml:"upnp:albumArtURI,omitempty"`
-	Searchable  int       `xml:"searchable,attr"`
+	Searchable  int       `xml:"searchable,attr"`	//
+	OriginalTrackNumber int `xml:"originalTrackNumber,omitempty"`
 }
-
 // Timestamp wraps time.Time for formatting purposes
 type Timestamp struct {
 	time.Time
