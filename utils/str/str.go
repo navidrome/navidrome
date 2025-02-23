217678af2de2297ab340ad80caa632af15768bef
tony tan <zhiyuan.tan@ooma.com>
2025-02-23 14:57:02 -0800

    Better support on Chinese metadata, reduce duplication caused by Traditional/Simplified Chinese variants

--- utils/str/str.go
+++ utils/str/str.go
@@ -2,7 +2,21 @@ package str
 
 import (
 	"strings"
+	"unicode"
+
+	"github.com/liuzl/gocc"
+	"golang.org/x/text/unicode/norm"
 )
+
+func init() {
+	var err error
+	opencc, err = gocc.New("t2s")
+	if err != nil {
+		panic(err)
+	}
+}
+
+var opencc *gocc.OpenCC
 
 var utf8ToAscii = func() *strings.Replacer {
 	var utf8Map = map[string]string{
@@ -38,4 +52,33 @@ func LongestCommonPrefix(list []string) string {
 		}
 	}
 	return list[0]
+}
+
+// NormalizeText performs normalization on the given text
+// This includes
+// - converts input to Unicode NFC
+// - converts all Chinese character to simplified
+func NormalizeText(s string) string {
+	transformFuncs := []func(s string) string{
+		norm.NFC.String,
+		ToSimplifiedChinese,
+	}
+
+	for _, f := range transformFuncs {
+		s = f(s)
+	}
+
+	return s
+}
+
+// ToSimplifiedChinese converts the given string from Traditional Chinese to Simplified
+// Original string is returned if it contains no Chinese character
+func ToSimplifiedChinese(s string) string {
+	for _, r := range s {
+		if unicode.Is(unicode.Han, r) {
+			s, _ = opencc.Convert(s)
+			break
+		}
+	}
+	return s
 }
