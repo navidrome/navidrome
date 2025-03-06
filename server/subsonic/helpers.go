5869f7caaf6c2ac22800de6152d031730949a98c
Kendall Garner <17521368+kgarner7@users.noreply.github.com>
2025-03-06 03:52:15 +0000

    feat(subsonic): set sortName for OS AlbumList (#3776)
    * feat(subsonic): Set SortName for OS AlbumList, test to JSON/XML
    
    * albumlist2, star2 updated properly
    
    * fix(subsonic): add sort or order name based on config
    
    Signed-off-by: Deluan <deluan@navidrome.org>
    
    ---------
    
    Signed-off-by: Deluan <deluan@navidrome.org>
    Co-authored-by: Deluan <deluan@navidrome.org>

--- server/subsonic/helpers.go
+++ server/subsonic/helpers.go
@@ -317,6 +317,7 @@ func osChildFromAlbum(ctx context.Context, al model.Album) *responses.OpenSubson
 	child.DisplayAlbumArtist = al.AlbumArtist
 	child.AlbumArtists = artistRefs(al.Participants[model.RoleAlbumArtist])
 	child.ExplicitStatus = mapExplicitStatus(al.ExplicitStatus)
+	child.SortName = sortName(al.SortAlbumName, al.OrderAlbumName)
 	return &child
 }
 
