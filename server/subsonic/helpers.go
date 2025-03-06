a750af5a83489fa47b49b2a223dc6bbe9bcfb88a
Deluan <deluan@navidrome.org>
2025-03-05 22:50:33 -0500

    fix(subsonic): add sort or order name based on config
    Signed-off-by: Deluan <deluan@navidrome.org>

--- server/subsonic/helpers.go
+++ server/subsonic/helpers.go
@@ -317,7 +317,7 @@ func osChildFromAlbum(ctx context.Context, al model.Album) *responses.OpenSubson
 	child.DisplayAlbumArtist = al.AlbumArtist
 	child.AlbumArtists = artistRefs(al.Participants[model.RoleAlbumArtist])
 	child.ExplicitStatus = mapExplicitStatus(al.ExplicitStatus)
-	child.SortName = al.SortAlbumName
+	child.SortName = sortName(al.SortAlbumName, al.OrderAlbumName)
 	return &child
 }
 
