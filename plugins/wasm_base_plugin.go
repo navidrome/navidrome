3386f25d3e885a6e47c8d4c1f2bb58906eb263da
Deluan <deluan@navidrome.org>
2025-04-26 17:51:09 -0400

    feat(plugins): implement instance pooling for wasmBasePlugin to improve performance - WIP
    Signed-off-by: Deluan <deluan@navidrome.org>

--- plugins/wasm_base_plugin.go
+++ plugins/wasm_base_plugin.go
@@ -2,9 +2,10 @@ package plugins
 
 import (
 	"context"
+	"fmt"
+	"sync"
 	"time"
 
-	gonanoid "github.com/matoous/go-nanoid/v2"
 	"github.com/navidrome/navidrome/log"
 )
 
@@ -23,6 +24,7 @@ type LoaderFunc[S any, P any] func(ctx context.Context, loader P, path string) (
 // that return non-exported interface types (like Scrobbler), while our code works
 // with the exported interfaces (like Scrobbler). The loadFunc bridges this gap.
 type wasmBasePlugin[S any, P any] struct {
+	pool     sync.Pool
 	wasmPath string
 	name     string
 	loader   P
@@ -40,27 +42,45 @@ func (w *wasmBasePlugin[S, P]) GetInstance(ctx context.Context) (any, func(), er
 	}, err
 }
 
+func (w *wasmBasePlugin[S, P]) initPool(ctx context.Context) {
+	if w.pool.New != nil {
+		return
+	}
+	w.pool.New = func() any {
+		inst, err := w.loadFunc(ctx, w.loader, w.wasmPath)
+		if err != nil {
+			log.Error(ctx, "wasmBasePlugin: failed to create instance for pool", "plugin", w.name, err)
+			return nil
+		}
+		instanceID := fmt.Sprintf("%p", inst)
+		log.Trace(ctx, "wasmBasePlugin: created new instance for pool", "plugin", w.name, "instanceID", instanceID)
+		return inst
+	}
+}
+
 // getInstance returns a new plugin instance, a cleanup function, and error
 func (w *wasmBasePlugin[S, P]) getInstance(ctx context.Context, methodName string) (S, func(error), error) {
-	var zero S
-	instanceID, _ := gonanoid.New(10)
-	inst, err := w.loadFunc(ctx, w.loader, w.wasmPath)
-	if err != nil {
-		log.Error(ctx, "wasmBasePlugin: failed to create instance", "plugin", w.name, "instanceID", instanceID, "method", methodName, "err", err)
-		return zero, nil, err
-	}
-	log.Trace(ctx, "wasmBasePlugin: created new instance", "plugin", w.name, "instanceID", instanceID, "method", methodName)
-	start := time.Now()
-	closeFn := func(opErr error) {
+	w.initPool(ctx)
+
+	inst := w.pool.Get().(S)
+	instanceID := fmt.Sprintf("%p", inst)
+	log.Trace(ctx, "wasmBasePlugin: got instance from pool", "plugin", w.name, "instanceID", instanceID, "method", methodName)
+	return inst, func(opErr error) {
+		if opErr == nil {
+			log.Trace(ctx, "wasmBasePlugin: returning instance to pool", "plugin", w.name, "instanceID", instanceID, "method", methodName)
+			w.pool.Put(inst)
+			return
+		}
+		log.Error(ctx, "wasmBasePlugin: error in method, closing instance", "plugin", w.name, "instanceID", instanceID, "method", methodName, opErr)
+		start := time.Now()
 		if closer, ok := any(inst).(interface{ Close(context.Context) error }); ok {
 			if closeErr := closer.Close(ctx); closeErr != nil {
-				log.Error(ctx, "wasmBasePlugin: error closing instance", "plugin", w.name, "instanceID", instanceID, "method", methodName, "elapsed", time.Since(start), "closeErr", closeErr, "opErr", opErr)
+				log.Error(ctx, "wasmBasePlugin: error closing instance", "plugin", w.name, "instanceID", instanceID, "method", methodName, "elapsed", time.Since(start), "closeErr", closeErr)
 			} else {
-				log.Trace(ctx, "wasmBasePlugin: closed instance", "plugin", w.name, "instanceID", instanceID, "method", methodName, "elapsed", time.Since(start), "opErr", opErr)
+				log.Trace(ctx, "wasmBasePlugin: closed instance", "plugin", w.name, "instanceID", instanceID, "method", methodName, "elapsed", time.Since(start), "closeErr", closeErr)
 			}
 		}
-	}
-	return inst, closeFn, nil
+	}, nil
 }
 
 func callMethod[S any, R any](ctx context.Context, w wasmPlugin[S], methodName string, fn func(inst S) (R, error)) (R, error) {
