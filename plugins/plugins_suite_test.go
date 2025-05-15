37f90a31a03b2dd55e0fc71f979627aa83ba404a
Deluan <deluan@navidrome.org>
2025-05-15 09:11:08 -0400

    feat(tests): add build step for test plugins in the test suite
    Signed-off-by: Deluan <deluan@navidrome.org>

--- plugins/plugins_suite_test.go
+++ plugins/plugins_suite_test.go
@@ -1,6 +1,7 @@
 package plugins
 
 import (
+	"os/exec"
 	"testing"
 
 	"github.com/navidrome/navidrome/log"
@@ -11,7 +12,19 @@ import (
 
 func TestPlugins(t *testing.T) {
 	tests.Init(t, false)
+	buildTestPlugins(t, "plugins/testdata")
 	log.SetLevel(log.LevelFatal)
 	RegisterFailHandler(Fail)
 	RunSpecs(t, "Plugins Suite")
+}
+
+func buildTestPlugins(t *testing.T, path string) {
+	t.Helper()
+	t.Logf("[BeforeSuite] Current working directory: %s", path)
+	cmd := exec.Command("make", "-C", path)
+	out, err := cmd.CombinedOutput()
+	t.Logf("[BeforeSuite] Make output: %s", string(out))
+	if err != nil {
+		t.Fatalf("Failed to build test plugins: %v", err)
+	}
 }
