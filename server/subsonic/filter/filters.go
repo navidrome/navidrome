886bf247adfb464abb3284adc1e464f9149a0fe6
Deluan <deluan@navidrome.org>
2025-05-21 09:33:37 -0400

    refactor(filters): rename function to better reflect its purpose
    Signed-off-by: Deluan <deluan@navidrome.org>

--- server/subsonic/filter/filters.go
+++ server/subsonic/filter/filters.go
@@ -108,7 +108,7 @@ func SongsByRandom(genre string, fromYear, toYear int) Options {
 	return addDefaultFilters(options)
 }
 
-func SongWithArtistTitle(artist, title string) Options {
+func SongWithLyrics(artist, title string) Options {
 	return addDefaultFilters(Options{
 		Sort:  "updated_at",
 		Order: "desc",
