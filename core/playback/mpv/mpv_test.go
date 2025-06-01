7f6d0a9245b4c3c9d1d127bc1bee932ab4b2eaac
Deluan <deluan@navidrome.org>
2025-05-31 21:55:19 -0400

    test(mpv): add test suite for MPV command functionality
    Signed-off-by: Deluan <deluan@navidrome.org>

--- core/playback/mpv/mpv_test.go
+++ core/playback/mpv/mpv_test.go
@@ -9,23 +9,13 @@ import (
 	"runtime"
 	"strings"
 	"sync"
-	"testing"
 	"time"
 
 	"github.com/navidrome/navidrome/conf"
 	"github.com/navidrome/navidrome/conf/configtest"
-	"github.com/navidrome/navidrome/log"
-	"github.com/navidrome/navidrome/tests"
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 )
-
-func TestMPV(t *testing.T) {
-	tests.Init(t, false)
-	log.SetLevel(log.LevelFatal)
-	RegisterFailHandler(Fail)
-	RunSpecs(t, "MPV Suite")
-}
 
 var _ = Describe("MPV", func() {
 	var (
