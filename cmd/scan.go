f82ee7cf32ba756e04d1728765bcfdaf304cf923
Kendall Garner <17521368+kgarner7@users.noreply.github.com>
2025-06-01 18:19:37 -0700

    Real Fix: move `WriteAfterScanMetrics` outside gofunc

--- cmd/scan.go
+++ cmd/scan.go
@@ -7,7 +7,6 @@ import (
 
 	"github.com/navidrome/navidrome/core"
 	"github.com/navidrome/navidrome/core/artwork"
-	"github.com/navidrome/navidrome/core/metrics"
 	"github.com/navidrome/navidrome/db"
 	"github.com/navidrome/navidrome/log"
 	"github.com/navidrome/navidrome/persistence"
@@ -70,7 +69,7 @@ func runScanner(ctx context.Context) {
 	ds := persistence.New(sqlDB)
 	pls := core.NewPlaylists(ds)
 
-	progress, err := scanner.CallScan(ctx, ds, artwork.NoopCacheWarmer(), pls, metrics.NewPrometheusInstance(ds), fullScan)
+	progress, err := scanner.CallScan(ctx, ds, artwork.NoopCacheWarmer(), pls, fullScan)
 	if err != nil {
 		log.Fatal(ctx, "Failed to scan", err)
 	}
