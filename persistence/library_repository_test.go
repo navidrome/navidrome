0b262cb0039a3c43af48a21d27b60354e5e04068
Deluan Quintão <github@deluan.com>
2025-06-13 21:20:08 -0400

    test: verify full library stats

--- persistence/library_repository_test.go
+++ persistence/library_repository_test.go
@@ -29,6 +29,24 @@ var _ = Describe("LibraryRepository", func() {
 		libAfter, err := repo.Get(1)
 		Expect(err).ToNot(HaveOccurred())
 		Expect(libAfter.UpdatedAt).To(BeTemporally(">", libBefore.UpdatedAt))
-		Expect(libAfter.TotalMissingFiles).To(BeZero())
+
+		var songsRes, albumsRes, artistsRes, foldersRes, filesRes, missingRes struct{ Count int64 }
+		var sizeRes struct{ Sum int64 }
+
+		Expect(conn.NewQuery("select count(*) as count from media_file where library_id = {:id} and missing = 0").Bind(dbx.Params{"id": 1}).One(&songsRes)).To(Succeed())
+		Expect(conn.NewQuery("select count(*) as count from album where library_id = {:id}").Bind(dbx.Params{"id": 1}).One(&albumsRes)).To(Succeed())
+		Expect(conn.NewQuery("select count(*) as count from library_artist where library_id = {:id}").Bind(dbx.Params{"id": 1}).One(&artistsRes)).To(Succeed())
+		Expect(conn.NewQuery("select count(*) as count from folder where library_id = {:id} and missing = 0").Bind(dbx.Params{"id": 1}).One(&foldersRes)).To(Succeed())
+		Expect(conn.NewQuery("select ifnull(sum(num_audio_files + num_playlists + json_array_length(image_files)),0) as count from folder where library_id = {:id} and missing = 0").Bind(dbx.Params{"id": 1}).One(&filesRes)).To(Succeed())
+		Expect(conn.NewQuery("select count(*) as count from media_file where library_id = {:id} and missing = 1").Bind(dbx.Params{"id": 1}).One(&missingRes)).To(Succeed())
+		Expect(conn.NewQuery("select ifnull(sum(size),0) as sum from album where library_id = {:id}").Bind(dbx.Params{"id": 1}).One(&sizeRes)).To(Succeed())
+
+		Expect(libAfter.TotalSongs).To(Equal(int(songsRes.Count)))
+		Expect(libAfter.TotalAlbums).To(Equal(int(albumsRes.Count)))
+		Expect(libAfter.TotalArtists).To(Equal(int(artistsRes.Count)))
+		Expect(libAfter.TotalFolders).To(Equal(int(foldersRes.Count)))
+		Expect(libAfter.TotalFiles).To(Equal(int(filesRes.Count)))
+		Expect(libAfter.TotalMissingFiles).To(Equal(int(missingRes.Count)))
+		Expect(libAfter.TotalSize).To(Equal(sizeRes.Sum))
 	})
 })
