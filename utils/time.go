43bcd14795d1c28b93dcfe421cb833fdfe75292a
Sajjad Servatjoo <S.Servatjoo@tejaratshayan.com>
2025-08-14 17:14:53 +0330

    refactor(utils): improve TimeNewest function

--- utils/time.go
+++ utils/time.go
@@ -2,9 +2,27 @@ package utils
 
 import "time"
 
+// TimeNewest returns the most recent (latest) time from a list of provided times.
+//
+// Usage:
+//
+//	t1 := time.Date(2025, 8, 14, 10, 0, 0, 0, time.UTC)
+//	t2 := time.Date(2025, 8, 14, 12, 0, 0, 0, time.UTC)
+//	newest := TimeNewest(t1, t2) // returns t2
+//
+// If no times are provided, the function returns the zero value of time.Time.
+// The zero value represents January 1, year 1, 00:00:00 UTC.
+//
+// Note:
+//   - The function compares all times using the After method.
+//   - Times with the same value are handled correctly; the first occurrence is returned.
 func TimeNewest(times ...time.Time) time.Time {
-	newest := time.Time{}
-	for _, t := range times {
+	if len(times) == 0 {
+		return time.Time{}
+	}
+
+	newest := times[0]
+	for _, t := range times[1:] {
 		if t.After(newest) {
 			newest = t
 		}
--- utils/time_test.go
+++ utils/time_test.go
@@ -9,20 +9,33 @@ import (
 )
 
 var _ = Describe("TimeNewest", func() {
+	var (
+		t1, t2, t3 time.Time
+	)
+
+	BeforeEach(func() {
+		t1 = time.Date(2020, 1, 1, 0, 0, 0, 0, time.UTC)
+		t2 = time.Date(2021, 1, 1, 0, 0, 0, 0, time.UTC)
+		t3 = time.Date(2019, 1, 1, 0, 0, 0, 0, time.UTC)
+	})
+
 	It("returns zero time when no times are provided", func() {
 		Expect(utils.TimeNewest()).To(Equal(time.Time{}))
 	})
 
-	It("returns the time when only one time is provided", func() {
-		t1 := time.Date(2020, 1, 1, 0, 0, 0, 0, time.UTC)
+	It("returns the same time when only one time is provided", func() {
 		Expect(utils.TimeNewest(t1)).To(Equal(t1))
 	})
 
 	It("returns the newest time when multiple times are provided", func() {
-		t1 := time.Date(2020, 1, 1, 0, 0, 0, 0, time.UTC)
-		t2 := time.Date(2021, 1, 1, 0, 0, 0, 0, time.UTC)
-		t3 := time.Date(2019, 1, 1, 0, 0, 0, 0, time.UTC)
-
 		Expect(utils.TimeNewest(t1, t2, t3)).To(Equal(t2))
+	})
+
+	It("returns the newest time even if the newest is first", func() {
+		Expect(utils.TimeNewest(t2, t1, t3)).To(Equal(t2))
+	})
+
+	It("returns the newest time even if there are duplicates", func() {
+		Expect(utils.TimeNewest(t1, t2, t2, t3)).To(Equal(t2))
 	})
 })
