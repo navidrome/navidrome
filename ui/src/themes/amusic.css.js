9dde22766cbcebb00196db44de62c34edb377f30
Stephan Wahlen <44159957+metalheim@users.noreply.github.com>
2025-11-21 19:16:49 +0100

    more styling to player
    - fix some appearances of green in queue
    - match queue styling to rest of theme
    - round albumart in player and prevent rotation

--- ui/src/themes/amusic.css.js
+++ ui/src/themes/amusic.css.js
@@ -7,18 +7,24 @@ const stylesheet = `
     background-color: #ff4e6b
 }
 .react-jinke-music-player-main ::-webkit-scrollbar-thumb {
-    background-color: #ff4e6b;
+    background-color: #ff4e6b
 }
 .react-jinke-music-player-main .music-player-panel .panel-content .rc-slider-handle:active {
     box-shadow: 0 0 2px #ff4e6b
 }
-.react-jinke-music-player-main .audio-item.playing svg {
+.audio-lists-panel-content .audio-item.playing,
+.react-jinke-music-player-main .audio-item.playing svg,
+.react-jinke-music-player-main .group player-delete {
     color: #ff4e6b
+}
+.audio-lists-panel-content .audio-item:hover,{
+    color: #ff0436
 }
 .react-jinke-music-player-main .audio-item.playing .player-singer {
     color: #ff4e6b !important
 }
-.react-jinke-music-player-main .lyric-btn {
+.react-jinke-music-player-main .lyric-btn,
+.react-jinke-music-player-main .lyric-btn-active svg{
     color: #ff4e6b !important
 }
 .react-jinke-music-player-main .lyric-btn-active {
@@ -31,10 +37,19 @@ const stylesheet = `
     color: #ff4e6b !important;
 	text-shadow: -1px -1px 0 #000, 1px -1px 0 #000, -1px 1px 0 #000, 1px 1px 0 #000
 }
-.react-jinke-music-player-main .music-player-panel{
-    background-color: #1f1f1f
+.react-jinke-music-player-main .music-player-panel,
+.react-jinke-music-player-main .audio-lists-panel{
+    background-color: #1f1f1f;
+	border: 1px solid rgba(255, 255, 255, 0.12);
 }
-.react-jinke-music-player-main *{
+.react-jinke-music-player-main .audio-lists-panel{
+	border-radius: 6px 6px 0 0;
+}
+.react-jinke-music-player-main .music-player-panel .panel-content div.img-rotate{
+    border-radius: 6px;
+	animation-duration: 0s !important
+}
+.react-jinke-music-player-main .songTitle{
     color: #ddd
 }
 `
