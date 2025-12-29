e51fe1cbd15f48307b2bf835e5392a0112d44571
Deluan <deluan@navidrome.org>
2025-12-29 09:19:10 -0500

    feat(plugins): implement KVStore service for persistent key-value storage
    Signed-off-by: Deluan <deluan@navidrome.org>

--- plugins/manifest.go
+++ plugins/manifest.go
@@ -1,6 +1,6 @@
 package plugins
 
-//go:generate go tool go-jsonschema -p plugins --struct-name-from-title -o manifest_gen.go manifest.json
+//go:generate go tool go-jsonschema -p plugins --struct-name-from-title -o manifest_gen.go manifest-schema.json
 
 // AllowedHosts returns a list of allowed hosts for HTTP requests.
 // Returns the hosts directly from the manifest's permissions.
