23bf256a66f47713d225269546bc542bad1c9e40
Valeri Sokolov <ulfurinn@ulfurinn.net>
2026-02-23 04:05:59 +0100

    feat: make album and artist annotations available to smart playlists (#4927)
    * feat(criteria): make album ratings available to smart playlist queries
    
    Expose an "albumrating" field mapping to album annotations.
    
    Signed-off-by: Valeri Sokolov <ulfurinn@ulfurinn.net>
    
    * fix(criteria): use query parameters
    
    Signed-off-by: Valeri Sokolov <ulfurinn@ulfurinn.net>
    
    * feat: add album and artist annotation fields to smart playlists
    
    Extend smart playlists to filter songs by album or artist annotations
    (rating, loved, play count, last played, date loved, date rated). This
    adds 12 new fields (6 album, 6 artist) with conditional JOINs that are
    only added when the criteria or sort references them, avoiding
    unnecessary query overhead. The album table JOIN is also removed since
    media_file.album_id can be used directly.
    
    ---------
    
    Signed-off-by: Valeri Sokolov <ulfurinn@ulfurinn.net>
    Co-authored-by: Deluan <deluan@navidrome.org>

--- persistence/playlist_repository.go
+++ persistence/playlist_repository.go
@@ -241,10 +241,25 @@ func (r *playlistRepository) refreshSmartPlaylist(pls *model.Playlist) bool {
 	}
 
 	sq := Select("row_number() over (order by "+rules.OrderBy()+") as id", "'"+pls.ID+"' as playlist_id", "media_file.id as media_file_id").
-		From("media_file").LeftJoin("annotation on (" +
-		"annotation.item_id = media_file.id" +
-		" AND annotation.item_type = 'media_file'" +
-		" AND annotation.user_id = '" + usr.ID + "')")
+		From("media_file").LeftJoin("annotation on ("+
+		"annotation.item_id = media_file.id"+
+		" AND annotation.item_type = 'media_file'"+
+		" AND annotation.user_id = ?)", usr.ID)
+
+	// Conditionally join album/artist annotation tables only when referenced by criteria or sort
+	requiredJoins := rules.RequiredJoins()
+	if requiredJoins.Has(criteria.JoinAlbumAnnotation) {
+		sq = sq.LeftJoin("annotation AS album_annotation ON ("+
+			"album_annotation.item_id = media_file.album_id"+
+			" AND album_annotation.item_type = 'album'"+
+			" AND album_annotation.user_id = ?)", usr.ID)
+	}
+	if requiredJoins.Has(criteria.JoinArtistAnnotation) {
+		sq = sq.LeftJoin("annotation AS artist_annotation ON ("+
+			"artist_annotation.item_id = media_file.artist_id"+
+			" AND artist_annotation.item_type = 'artist'"+
+			" AND artist_annotation.user_id = ?)", usr.ID)
+	}
 
 	// Only include media files from libraries the user has access to
 	sq = r.applyLibraryFilter(sq, "media_file")
