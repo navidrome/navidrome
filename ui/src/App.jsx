d109f678a7bc06da2d45cac9fe787dad4de24116
Deluan <deluan@navidrome.org>
2026-03-08 11:49:26 -0400

    feat(ui): add Redux transcoding reducer for browser profile state
    Store the detected browser codec profile in Redux so it's available
    globally. The profile is set once at startup and used by the decision
    service when calling getTranscodeDecision.

--- ui/src/App.jsx
+++ ui/src/App.jsx
@@ -33,6 +33,7 @@ import {
   replayGainReducer,
   downloadMenuDialogReducer,
   shareDialogReducer,
+  transcodingReducer,
 } from './reducers'
 import createAdminStore from './store/createAdminStore'
 import { i18nProvider } from './i18n'
@@ -72,6 +73,7 @@ const adminStore = createAdminStore({
     activity: activityReducer,
     settings: settingsReducer,
     replayGain: replayGainReducer,
+    transcoding: transcodingReducer,
   },
 })
 
