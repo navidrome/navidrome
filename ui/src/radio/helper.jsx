2e21842f0358248e93b4064b5206621052c0a0de
Deluan <deluan@navidrome.org>
2026-03-18 14:32:35 -0400

    feat(ui): prefer artwork URL in radio player helper

--- ui/src/radio/helper.jsx
+++ ui/src/radio/helper.jsx
@@ -1,16 +1,22 @@
+import subsonic from '../subsonic'
+
 export async function songFromRadio(radio) {
   if (!radio) {
     return undefined
   }
 
-  let cover = 'internet-radio-icon.svg'
-  try {
-    const url = new URL(radio.homePageUrl ?? radio.streamUrl)
-    url.pathname = '/favicon.ico'
-    await resourceExists(url)
-    cover = url.toString()
-  } catch {
-    // ignore
+  let cover = subsonic.getCoverArtUrl(radio, 300)
+
+  // If no uploaded image, try favicon as fallback
+  if (!radio.uploadedImage) {
+    try {
+      const url = new URL(radio.homePageUrl ?? radio.streamUrl)
+      url.pathname = '/favicon.ico'
+      await resourceExists(url)
+      cover = url.toString()
+    } catch {
+      // Use artwork URL (will show placeholder)
+    }
   }
 
   return {
