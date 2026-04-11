f385fc8d171d697f87fbdaec0439500dc3cca348
RockWolf <git@rockwolf.eu>
2026-04-11 17:46:37 +0200

    feat(persistence): implement playlist permission repository

--- model/playlist.go
+++ model/playlist.go
@@ -131,6 +131,7 @@ type PlaylistRepository interface {
 	Delete(id string) error
 	Tracks(playlistId string, refreshSmartPlaylist bool) PlaylistTrackRepository
 	GetPlaylists(mediaFileId string) (Playlists, error)
+	Permissions(playlistId string) PlaylistPermissionRepository
 }
 
 type PlaylistTrack struct {
