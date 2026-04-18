6a0fff30d1787688cb74ece8360774298e1d5c38
Deluan <deluan@navidrome.org>
2026-04-18 19:49:24 -0400

    test(plugins): run platform-neutral specs on Windows, drop blanket Skip

--- plugins/plugins_suite_windows_test.go
+++ plugins/plugins_suite_windows_test.go
@@ -11,15 +11,13 @@ import (
 	. "github.com/onsi/gomega"
 )
 
+// Runs the subset of plugin specs compiled on Windows (files without the
+// //go:build !windows tag): capabilities, manager_cache, manager_plugin,
+// manifest, package. WASM-runtime-dependent specs live in !windows-tagged
+// files and aren't reached here.
 func TestPlugins(t *testing.T) {
 	tests.Init(t, false)
 	log.SetLevel(log.LevelFatal)
 	RegisterFailHandler(Fail)
 	RunSpecs(t, "Plugins Suite")
 }
-
-var _ = BeforeEach(func() {
-	if true {
-		Skip("not supported on Windows: plugin runtime (#TBD-plugins-windows)")
-	}
-})
