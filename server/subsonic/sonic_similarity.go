498b1ff4ccef51bdf1b94bfed80b40d0606a30db
Deluan <deluan@navidrome.org>
2026-04-26 22:18:24 -0400

    feat(subsonic): add getSonicSimilarTracks and findSonicPath handlers
    Add two new Subsonic API handlers for the sonicSimilarity OpenSubsonic extension: GetSonicSimilarTracks returns similarity-scored tracks similar to a given song, and FindSonicPath returns a path of tracks connecting two songs. Both handlers delegate to the sonic core service and map results to SonicMatch response types.

---
+++ server/subsonic/sonic_similarity.go
@@ -0,0 +1,62 @@
+package subsonic
+
+import (
+	"net/http"
+
+	"github.com/navidrome/navidrome/server/subsonic/responses"
+	"github.com/navidrome/navidrome/utils/req"
+)
+
+func (api *Router) GetSonicSimilarTracks(r *http.Request) (*responses.Subsonic, error) {
+	ctx := r.Context()
+	p := req.Params(r)
+	id, err := p.String("id")
+	if err != nil {
+		return nil, err
+	}
+	count := p.IntOr("count", 10)
+
+	matches, err := api.sonic.GetSonicSimilarTracks(ctx, id, count)
+	if err != nil {
+		return nil, err
+	}
+
+	response := newResponse()
+	response.SonicMatches = make([]responses.SonicMatch, len(matches))
+	for i, m := range matches {
+		response.SonicMatches[i] = responses.SonicMatch{
+			Entry:      childFromMediaFile(ctx, m.MediaFile),
+			Similarity: m.Similarity,
+		}
+	}
+	return response, nil
+}
+
+func (api *Router) FindSonicPath(r *http.Request) (*responses.Subsonic, error) {
+	ctx := r.Context()
+	p := req.Params(r)
+	startSongID, err := p.String("startSongId")
+	if err != nil {
+		return nil, err
+	}
+	endSongID, err := p.String("endSongId")
+	if err != nil {
+		return nil, err
+	}
+	count := p.IntOr("count", 25)
+
+	matches, err := api.sonic.FindSonicPath(ctx, startSongID, endSongID, count)
+	if err != nil {
+		return nil, err
+	}
+
+	response := newResponse()
+	response.SonicMatches = make([]responses.SonicMatch, len(matches))
+	for i, m := range matches {
+		response.SonicMatches[i] = responses.SonicMatch{
+			Entry:      childFromMediaFile(ctx, m.MediaFile),
+			Similarity: m.Similarity,
+		}
+	}
+	return response, nil
+}
