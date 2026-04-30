0465441fe0e0fd275089dff1c3849534a40b192d
Deluan <deluan@navidrome.org>
2026-04-29 21:38:20 -0400

    feat(subsonic): register playbackReport OpenSubsonic extension

--- server/subsonic/opensubsonic.go
+++ server/subsonic/opensubsonic.go
@@ -14,6 +14,7 @@ func (api *Router) GetOpenSubsonicExtensions(_ *http.Request) (*responses.Subson
 		{Name: "songLyrics", Versions: []int32{1}},
 		{Name: "indexBasedQueue", Versions: []int32{1}},
 		{Name: "transcoding", Versions: []int32{1}},
+		{Name: "playbackReport", Versions: []int32{1}},
 	}
 	if api.sonic != nil && api.sonic.HasProvider() {
 		extensions = append(extensions, responses.OpenSubsonicExtension{
