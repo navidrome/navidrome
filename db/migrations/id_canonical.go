e8183fdfe0f65c7e2ddf8bf3932f3e701411def4
Deluan <deluan@navidrome.org>
2026-07-19 21:48:42 -0400

    feat(db): add id canonicalization transform for the uniform-ids migration

---
+++ db/migrations/id_canonical.go
@@ -0,0 +1,39 @@
+package migrations
+
+import (
+	"crypto/md5"
+	"encoding/hex"
+	"math/big"
+
+	"github.com/navidrome/navidrome/model/id"
+)
+
+// canonicalID maps any historical Navidrome id shape to the canonical 22-char base62 encoding
+// of a 128-bit value; unrecognized shapes (including empty and share ids) pass through unchanged.
+func canonicalID(s string) string {
+	switch len(s) {
+	case 22:
+		v, ok := new(big.Int).SetString(s, 62)
+		if !ok || v.Sign() < 0 || v.BitLen() <= 128 {
+			return s
+		}
+		sum := md5.Sum([]byte(s))
+		return id.Encode128(sum[:])
+	case 32:
+		b, err := hex.DecodeString(s)
+		if err != nil {
+			return s
+		}
+		return id.Encode128(b)
+	case 36:
+		if s[8] != '-' || s[13] != '-' || s[18] != '-' || s[23] != '-' {
+			return s
+		}
+		b, err := hex.DecodeString(s[:8] + s[9:13] + s[14:18] + s[19:23] + s[24:])
+		if err != nil {
+			return s
+		}
+		return id.Encode128(b)
+	}
+	return s
+}
