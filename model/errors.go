2dab4b40483a6feb3be2007afa4a1ac83c2d0504
Deluan <deluan@navidrome.org>
2026-08-19 08:49:15 -0400

    chore: remove redundant comment
    Signed-off-by: Deluan <deluan@navidrome.org>

--- model/errors.go
+++ model/errors.go
@@ -3,13 +3,11 @@ package model
 import "errors"
 
 var (
-	ErrNotFound      = errors.New("data not found")
-	ErrInvalidAuth   = errors.New("invalid authentication")
-	ErrNotAuthorized = errors.New("not authorized")
-	ErrExpired       = errors.New("access expired")
-	ErrNotAvailable  = errors.New("functionality not available")
-	ErrValidation    = errors.New("validation error")
-
-	// ErrPlaylistNotEditable: tracks are server-managed, so nobody can edit them (not an ACL failure).
+	ErrNotFound            = errors.New("data not found")
+	ErrInvalidAuth         = errors.New("invalid authentication")
+	ErrNotAuthorized       = errors.New("not authorized")
+	ErrExpired             = errors.New("access expired")
+	ErrNotAvailable        = errors.New("functionality not available")
+	ErrValidation          = errors.New("validation error")
 	ErrPlaylistNotEditable = errors.New("playlist tracks are not editable")
 )
